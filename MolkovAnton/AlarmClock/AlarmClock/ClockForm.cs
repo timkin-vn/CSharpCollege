d10be789f3252e2364b434628873568935476557
MolkovAnton <molkov374@gmail.com>
2024-10-13 13:28:05 +0300

    Начал работу над проектом

---
+++ MolkovAnton/AlarmClock/AlarmClock/ClockForm.cs
@@ -0,0 +1,58 @@
+﻿using AlarmClock.Forms;
+using AlarmClock.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClock
+{
+    public partial class ClockForm : Form
+    {
+        private AlarmSettings _settings = new AlarmSettings();
+
+        private AwakeForm _awakeForm = null;
+
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DateTime.Now.ToLongTimeString();
+            if (_settings.IsAlarmActive && DateTime.Now.TimeOfDay >= _settings.AlarmTime.TimeOfDay)
+            {
+                if (_awakeForm == null || _awakeForm.IsDisposed)
+                {
+                    _awakeForm = new AwakeForm();
+                    _awakeForm.Settings = _settings;
+                }
+
+                _awakeForm.Show();
+            }
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            new AboutForm().ShowDialog();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void SettingsButton_Click(object sender, EventArgs e)
+        {
+            var form = new SettingsForm();
+            form.Settings = _settings;
+            form.ShowDialog();
+        }
+    }
+}
