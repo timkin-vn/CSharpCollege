8d474fe41419fa19cda115376bc0800daff118ff
Zaklzzzy <zaletin123@gmail.com>
2024-10-25 15:24:51 +0300

    Add Modify Alarm Clock

---
+++ ZaletinArtem/AlarmClock/AlarmClock/ClockForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AlarmClock
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClockForm));
+            this.DisplayLabel = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.AlarmTimer = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.About = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DisplayLabel
+            // 
+            this.DisplayLabel.BackColor = System.Drawing.Color.Black;
+            this.DisplayLabel.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DisplayLabel.ForeColor = System.Drawing.Color.White;
+            this.DisplayLabel.Location = new System.Drawing.Point(18, 14);
+            this.DisplayLabel.Name = "DisplayLabel";
+            this.DisplayLabel.Size = new System.Drawing.Size(181, 52);
+            this.DisplayLabel.TabIndex = 0;
+            this.DisplayLabel.Text = "00:00:00";
+            this.DisplayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.Color.Black;
+            this.Add.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Add.ForeColor = System.Drawing.Color.White;
+            this.Add.Location = new System.Drawing.Point(247, 14);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(120, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = false;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.AccessibleDescription = "";
+            this.ExitButton.BackColor = System.Drawing.Color.Black;
+            this.ExitButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitButton.ForeColor = System.Drawing.Color.White;
+            this.ExitButton.Location = new System.Drawing.Point(247, 72);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(120, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // AlarmTimer
+            // 
+            this.AlarmTimer.Enabled = true;
+            this.AlarmTimer.Interval = 1000;
+            this.AlarmTimer.Tick += new System.EventHandler(this.AlarmTimer_Tick);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 101);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(378, 348);
+            this.flowLayoutPanel1.TabIndex = 4;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // About
+            // 
+            this.About.AccessibleDescription = "";
+            this.About.BackColor = System.Drawing.Color.Black;
+            this.About.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.About.ForeColor = System.Drawing.Color.White;
+            this.About.Location = new System.Drawing.Point(247, 44);
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(120, 23);
+            this.About.TabIndex = 5;
+            this.About.Text = "О программе";
+            this.About.UseVisualStyleBackColor = false;
+            this.About.Click += new System.EventHandler(this.About_Click);
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(389, 450);
+            this.Controls.Add(this.About);
+            this.Controls.Add(this.DisplayLabel);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.ExitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ClockForm";
+            this.Text = "Будильник";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DisplayLabel;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Timer AlarmTimer;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button About;
+    }
+}
+
---
+++ ZaletinArtem/AlarmClock/AlarmClock/Forms/SettingsForm.cs
@@ -0,0 +1,34 @@
+﻿using AlarmClock.Models;
+using System;
+using System.Windows.Forms;
+
+namespace AlarmClock.Forms
+{
+    public partial class SettingsForm : Form
+    {
+        public AlarmSettings Settings { get; set; }
+
+        public SettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            if (!DateTime.TryParse(hourInput.Text + ":" + minuteInput.Text + ":00.0", out var alarmTime))
+            {
+                MessageBox.Show("Неверно задано время срабатывания", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Settings.AlarmTime = alarmTime;
+
+            DialogResult = DialogResult.OK;
+        }
+
+        private void SettingsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
