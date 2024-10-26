9c56cec038de5e2f6b9eeb6008ea49bd93a00dbf
KurymiCh <164912181+SempaiCh@users.noreply.github.com>
2024-10-26 11:05:46 +0300

    Калькулятор времени (не архив)

---
+++ Vladisval/AlarmClock/ClockForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace AlarmClock
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.DisplayLabel = new System.Windows.Forms.Label();
+            this.AboutButton = new System.Windows.Forms.Button();
+            this.SettingsButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ClockTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.time1 = new System.Windows.Forms.TextBox();
+            this.time2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resault = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DisplayLabel
+            // 
+            this.DisplayLabel.BackColor = System.Drawing.Color.Black;
+            this.DisplayLabel.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DisplayLabel.ForeColor = System.Drawing.Color.GreenYellow;
+            this.DisplayLabel.Location = new System.Drawing.Point(17, 16);
+            this.DisplayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DisplayLabel.Name = "DisplayLabel";
+            this.DisplayLabel.Size = new System.Drawing.Size(391, 102);
+            this.DisplayLabel.TabIndex = 0;
+            this.DisplayLabel.Text = "00:00:00";
+            this.DisplayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.Location = new System.Drawing.Point(416, 15);
+            this.AboutButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(156, 28);
+            this.AboutButton.TabIndex = 1;
+            this.AboutButton.Text = "О программе";
+            this.AboutButton.UseVisualStyleBackColor = true;
+            this.AboutButton.Click += new System.EventHandler(this.AboutButton_Click);
+            // 
+            // SettingsButton
+            // 
+            this.SettingsButton.Location = new System.Drawing.Point(416, 52);
+            this.SettingsButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SettingsButton.Name = "SettingsButton";
+            this.SettingsButton.Size = new System.Drawing.Size(156, 28);
+            this.SettingsButton.TabIndex = 2;
+            this.SettingsButton.Text = "Настройки";
+            this.SettingsButton.UseVisualStyleBackColor = true;
+            this.SettingsButton.Click += new System.EventHandler(this.SettingsButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(416, 89);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(156, 28);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ClockTimer
+            // 
+            this.ClockTimer.Enabled = true;
+            this.ClockTimer.Interval = 1000;
+            this.ClockTimer.Tick += new System.EventHandler(this.ClockTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Конечная точка времени";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(322, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Начальная точка";
+            // 
+            // time1
+            // 
+            this.time1.Location = new System.Drawing.Point(34, 172);
+            this.time1.Name = "time1";
+            this.time1.Size = new System.Drawing.Size(160, 22);
+            this.time1.TabIndex = 6;
+            this.time1.Text = "00:00:00";
+            this.time1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // time2
+            // 
+            this.time2.Location = new System.Drawing.Point(328, 172);
+            this.time2.Name = "time2";
+            this.time2.Size = new System.Drawing.Size(160, 22);
+            this.time2.TabIndex = 7;
+            this.time2.Text = "00:00:00";
+            this.time2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(248, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 39);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "-";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(454, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Рассчитать ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resault
+            // 
+            this.resault.AutoSize = true;
+            this.resault.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.resault.Location = new System.Drawing.Point(527, 164);
+            this.resault.Name = "resault";
+            this.resault.Size = new System.Drawing.Size(0, 34);
+            this.resault.TabIndex = 10;
+            this.resault.Click += new System.EventHandler(this.resault_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(495, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "=";
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 320);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.resault);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.time2);
+            this.Controls.Add(this.time1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.SettingsButton);
+            this.Controls.Add(this.AboutButton);
+            this.Controls.Add(this.DisplayLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ClockForm";
+            this.Text = "Будильник";
+            this.Load += new System.EventHandler(this.ClockForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DisplayLabel;
+        private System.Windows.Forms.Button AboutButton;
+        private System.Windows.Forms.Button SettingsButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Timer ClockTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox time1;
+        private System.Windows.Forms.TextBox time2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resault;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Vladisval/AlarmClock/ClockForm.cs
@@ -0,0 +1,138 @@
+﻿using AlarmClock.Forms;
+using AlarmClock.Models;
+using AlarmClock.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AlarmClock
+{
+    public partial class ClockForm : Form
+    {
+        private AlarmSettings _settings = new AlarmSettings();
+
+        private AwakeForm _awakeForm = null;
+        private readonly int totalSeconds;
+
+        public AlarmSettings Settings { get; set; }
+
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DateTime.Now.ToLongTimeString();
+            if (_settings.IsAlarmActive && DateTime.Now.TimeOfDay >= _settings.AlarmTime.TimeOfDay)
+            {
+                if (_awakeForm == null || _awakeForm.IsDisposed)
+                {
+                    _awakeForm = new AwakeForm();
+                    _awakeForm.Settings = _settings;
+                }
+
+                _awakeForm.Show();
+
+                if (_settings.IsSoundActive)
+                {
+                    SystemSounds.Beep.Play();
+                }
+            }
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            new AboutForm().ShowDialog();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void SettingsButton_Click(object sender, EventArgs e)
+        {
+            var form = new SettingsForm();
+            form.Settings = _settings;
+            form.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            string timeString = time1.Text;
+            string timeString2 = time2.Text;
+            try
+            {
+              
+                string[] timeParts = timeString.Split(':');
+
+                int hours = int.Parse(timeParts[0]);
+                int minutes = int.Parse(timeParts[1]);
+                int seconds = int.Parse(timeParts[2]);
+                int totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+
+                string[] timeParts2 = timeString2.Split(':');
+
+                int hours2 = int.Parse(timeParts2[0]);
+                int minutes2 = int.Parse(timeParts2[1]);
+                int seconds2 = int.Parse(timeParts2[2]);
+                int totalSeconds2 = (hours2 * 3600) + (minutes2 * 60) + seconds2;
+                if(totalSeconds> totalSeconds2)
+                {
+                    int res = totalSeconds - totalSeconds2;
+
+                    int hoursResult = res / 3600;
+                    int minutesResult = (res % 3600) / 60;
+                    int secondsResult = res % 60;
+
+                    resault.Text = $"{hoursResult:D2}:{minutesResult:D2}:{secondsResult:D2}";
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный формат ввода! попробуйте точки поменять местами. (Ожидалось конечная точка > начальной точки!)", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+               
+            }
+            catch { };
+
+
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {}
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {}
+
+        private void resault_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClockForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
