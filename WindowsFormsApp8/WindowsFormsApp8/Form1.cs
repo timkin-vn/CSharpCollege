ffafe257027d378a5ac45355b767429d6499828d
MarkProMaster229 <164912605+MarkProMaster229@users.noreply.github.com>
2024-11-08 00:47:38 +0300

    izmenen

--- WindowsFormsApp8/WindowsFormsApp8/Form1.cs
+++ WindowsFormsApp8/WindowsFormsApp8/Form1.cs
@@ -6,44 +6,165 @@ namespace WindowsFormsApp8
 {
     public partial class MainForm : Form
     {
+        private const int OriginalWidth = 300;
+        private const int OriginalHeight = 300;
+
+
         public MainForm()
         {
-            InitializeComponent();//такого размера будет окно при открытии 
-            this.Width = 300;  
-            this.Height = 300; 
+            InitializeComponent();
+            this.Width = OriginalWidth;
+            this.Height = OriginalHeight;
+            this.ResizeRedraw = true;
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            base.OnPaint(e);//не обязательно но желательно 
+            base.OnPaint(e);
             var graphics = e.Graphics;
 
-            // ручки для рисования 
-            var pen = new Pen(Color.Black, 3);
-            var brush = new SolidBrush(Color.Green);
+            //работа масштабирования -
+            float scaleX = (float)Width / OriginalWidth;
+            float scaleY = (float)Height / OriginalHeight;
 
-            // Рисование квадрата дома
-            graphics.FillRectangle(brush, new Rectangle(50, 100, 100, 100));
+            
+            using (var pen = new Pen(Color.Black, 3 / Math.Max(scaleX, scaleY)))
+            using (var brush = new SolidBrush(Color.Green))
+            {
+               
+                graphics.FillRectangle(brush, new Rectangle(
+                    (int)(50 * scaleX),
+                    (int)(100 * scaleY),
+                    (int)(100 * scaleX),
+                    (int)(100 * scaleY)
+                ));
 
-            // для крыши 
-            graphics.DrawLine(pen, 50, 100, 100, 50);
-            graphics.DrawLine(pen, 100, 50, 150, 100);
-            graphics.DrawLine(pen, 50, 100, 150, 100);
+                //крыша
+                graphics.DrawLine(pen, 50 * scaleX, 100 * scaleY, 100 * scaleX, 50 * scaleY);
+                graphics.DrawLine(pen, 100 * scaleX, 50 * scaleY, 150 * scaleX, 100 * scaleY);
+                graphics.DrawLine(pen, 50 * scaleX, 100 * scaleY, 150 * scaleX, 100 * scaleY);
 
-            // для окна
-            graphics.FillRectangle(new SolidBrush(Color.LightBlue), new Rectangle(70, 130, 40, 40)); 
-            graphics.DrawRectangle(new Pen(Color.Black, 2), new Rectangle(70, 130, 40, 40)); 
+                //окно
+                using (var windowBrush = new SolidBrush(Color.LightBlue))
+                {
+                    graphics.FillRectangle(windowBrush, new Rectangle(
+                        (int)(70 * scaleX),
+                        (int)(130 * scaleY),
+                        (int)(40 * scaleX),
+                        (int)(40 * scaleY)
+                    ));
+                }
 
-            graphics.DrawLine(new Pen(Color.Black, 2), 70, 150, 110, 150); 
-            graphics.DrawLine(new Pen(Color.Black, 2), 90, 130, 90, 170); 
+                using (var windowPen = new Pen(Color.Black, 2 / Math.Max(scaleX, scaleY)))
+                {
+                    graphics.DrawRectangle(windowPen, new Rectangle(
+                        (int)(70 * scaleX),
+                        (int)(130 * scaleY),
+                        (int)(40 * scaleX),
+                        (int)(40 * scaleY)
+                    ));
+                    graphics.DrawLine(windowPen, 70 * scaleX, 150 * scaleY, 110 * scaleX, 150 * scaleY);
+                    graphics.DrawLine(windowPen, 90 * scaleX, 130 * scaleY, 90 * scaleX, 170 * scaleY);
+                }
+
+            }
+
+            olimpickiecolca(e);
+            son(e);
+            ptichi(e);
+
+
+
         }
+
+        int diameter = 40;
+
+        public void olimpickiecolca(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            var graphics = e.Graphics;
+
+            
+            float scaleX = (float)Width / OriginalWidth;
+            float scaleY = (float)Height / OriginalHeight;
+
+
+
+            using (var penBlue = new Pen(Color.Blue, 3 / Math.Max(scaleX, scaleY)))
+            {
+                graphics.DrawEllipse(penBlue, 243 * scaleX, 200 * scaleY, diameter * scaleX, diameter * scaleY);
+            }
+
+            using (var penYellow = new Pen(Color.Yellow, 3 / Math.Max(scaleX, scaleY)))
+            {
+                graphics.DrawEllipse(penYellow, 210 * scaleX, 200 * scaleY, diameter * scaleX, diameter * scaleY); 
+            }
+
+            using (var penBlack = new Pen(Color.Black, 3 / Math.Max(scaleX, scaleY)))
+            {
+                graphics.DrawEllipse(penBlack, 176 * scaleX, 200 * scaleY, diameter * scaleX, diameter * scaleY); 
+            }
+
+            using (var penGreen = new Pen(Color.Green, 3 / Math.Max(scaleX, scaleY)))
+            {
+                graphics.DrawEllipse(penGreen, 193 * scaleX, 225 * scaleY, diameter * scaleX, diameter * scaleY); 
+            }
+
+            using (var penRed = new Pen(Color.Red, 3 / Math.Max(scaleX, scaleY)))
+            {
+                graphics.DrawEllipse(penRed, 225 * scaleX, 225 * scaleY, diameter * scaleX, diameter * scaleY); 
+            }
+        }
+
+        private void son(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            var graphics = e.Graphics;
+
+            
+            float scaleX = (float)Width / OriginalWidth;
+            float scaleY = (float)Height / OriginalHeight;
+
+            //заливка
+            using (var brushYellow = new SolidBrush(Color.Yellow))
+            {
+                
+                graphics.FillEllipse(brushYellow, 240 * scaleX, -10 * scaleY, 100 * scaleX, 60 * scaleY);
+            }
+        }
+
+        private void ptichi(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            var graphics = e.Graphics;
+
+            
+            float scaleX = (float)Width / OriginalWidth;
+            float scaleY = (float)Height / OriginalHeight;
+
+            var pen = new Pen(Color.Black, 2 / Math.Max(scaleX, scaleY));
+            var font = new Font("Arial", 20);
+            var brush = new SolidBrush(Color.Black);
+            {
+                // это птицы 
+                graphics.DrawString("✔", font, brush, 50 * scaleX, 20 * scaleY);
+                graphics.DrawString("✔", font, brush, 100 * scaleX, 20 * scaleY);
+                graphics.DrawString("✔", font, brush, 150 * scaleX, 20 * scaleY);
+
+                
+              
+            }
+        }
+
 
         [STAThread]
-        public static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            public static void Main()
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
         }
     }
-}
+
+
