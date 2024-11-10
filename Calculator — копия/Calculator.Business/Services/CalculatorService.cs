ee072e4af000c383adf3db2b09eb5ff4c79f749f
MarkProMaster229 <164912605+MarkProMaster229@users.noreply.github.com>
2024-11-10 23:18:02 +0300

    журнал

---
+++ Calculator — копия/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,72 @@
+﻿using Calculator.Business.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void InsertDigit(CalculatorState state, string digitText)
+        {
+            var digit = byte.Parse(digitText);
+            if (state.NeedClearX)
+            {
+                state.XRegister = 0;
+            }
+
+            state.XRegister = state.XRegister * 10 + digit;
+            state.NeedClearX = false;
+        }
+
+        public void Clear(CalculatorState state)
+        {
+            state.XRegister = 0;
+        }
+
+        public void InsertOperation(CalculatorState state, string opCode)
+        {
+            PerformOperation(state, state.OpCode);
+            MoveXToY(state);
+            state.OpCode = opCode;
+            state.NeedClearX = true;
+        }
+
+        private void MoveXToY(CalculatorState state)
+        {
+            state.YRegister = state.XRegister;
+        }
+
+        private void PerformOperation(CalculatorState state, string opCode)
+        {
+            switch (opCode)
+            {
+                case "+":
+                    state.XRegister += state.YRegister;
+                    break;
+
+                case "-":
+                    state.XRegister = state.YRegister - state.XRegister;
+                    break;
+
+                case "*":
+                    state.XRegister *= state.YRegister;
+                    break;
+
+                case "/":
+                    if (state.XRegister == 0)
+                    {
+                        state.XRegister = double.NaN;
+                    }
+                    else
+                    {
+                        state.XRegister = state.YRegister / state.XRegister;
+                    }
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Calculator — копия/Calculator/CalculatorForm.cs
@@ -0,0 +1,135 @@
+﻿using Calculator.Business.Entites;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private CalculatorService _service = new CalculatorService();
+
+        private CalculatorState _state = new CalculatorState();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, EventArgs e)
+        {
+            var senderButton = (Button)sender;
+            var digitText = senderButton.Text;
+
+            _service.InsertDigit(_state, digitText);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            _service.Clear(_state);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            var senderButton = (Button)sender;
+            var opCode = senderButton.Text;
+
+            _service.InsertOperation(_state, opCode);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Создаем и открываем окно
+            SettingsForm settingsForm = new SettingsForm();
+            settingsForm.Show();
+
+        }
+    }
+}
+
+
+//окно 
+public partial class SettingsForm : Form
+{
+    private ListView list;
+    private TextBox settingsTextBox;
+
+    public SettingsForm()
+    {
+        
+
+        this.Text = "журнал";
+
+        
+        Button applyButton = new Button();
+        applyButton.Text = "Применить";
+        applyButton.Location = new System.Drawing.Point(10, 30); 
+        applyButton.Click += ApplyButton_Click;
+        Controls.Add(applyButton);
+
+        
+        Button closeButton = new Button();
+        closeButton.Text = "Закрыть";
+        closeButton.Location = new System.Drawing.Point(200, 220); 
+        closeButton.Click += CloseButton_Click;
+        Controls.Add(closeButton);
+
+        
+        Label instructionsLabel = new Label();
+        instructionsLabel.Text = "Запишите сюда что бы не забыть - ";
+        instructionsLabel.Location = new System.Drawing.Point(5, 10); // Позиция
+        Controls.Add(instructionsLabel);
+
+        
+        list = new ListView();//в поле класса
+        list.Location = new System.Drawing.Point(40, 100);
+        Controls.Add(list);
+
+       
+        settingsTextBox = new TextBox();//в поле класса
+        settingsTextBox.Location = new System.Drawing.Point(40, 65);
+        settingsTextBox.Size = new System.Drawing.Size(200, 20);
+        settingsTextBox.Text = "Введите значение - ";
+        Controls.Add(settingsTextBox);
+    }
+
+    // Обработчик для кнопки 
+    private void ApplyButton_Click(object sender, EventArgs e)
+    {
+        string inputText = settingsTextBox.Text; // Получаем 
+
+
+        ListViewItem item = new ListViewItem(inputText);//создает новый элемент для списка 
+        list.Items.Add(item);//добавляет только что созданный элемент
+
+
+        if (list.Items.Count > 3)
+        {
+            list.Items.RemoveAt(0); 
+        }
+
+        
+        settingsTextBox.Clear();
+    }
+
+   
+    private void CloseButton_Click(object sender, EventArgs e)
+    {
+        this.Close();
+    }
+}
+
+
+
