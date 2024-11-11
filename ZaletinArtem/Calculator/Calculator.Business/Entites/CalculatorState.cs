407cb9d9eb1b8e9f4309701642726eaf46f4f22a
Zaklzzzy <zaletin123@gmail.com>
2024-11-11 09:10:58 +0300

    Add Calculator

---
+++ ZaletinArtem/Calculator/Calculator.Business/Entites/CalculatorState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Entites
+{
+    public class CalculatorState
+    {
+        public double XRegister { get; set; }
+        public double YRegister { get; set; }
+        public double Result { get; set; }
+
+        public string OpCode { get; set; }
+
+        public bool NeedClearX { get; set; }
+
+        public bool IsDecimalMode { get; set; }
+        public double DecimalFactor { get; set; } = 1;
+
+        public string DisplayedOperation { get; set; }
+    }
+}
