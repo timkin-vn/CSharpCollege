dd7d45410470efcebe4ce4a0efbf72958e614918
NektoPlaton <164889351+NektoPlaton@users.noreply.github.com>
2024-11-11 10:10:20 +0300

    Add files via upload
    добавил калькулятор, саму папку

---
+++ Calculator/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,115 @@
+﻿using Calculator.Business.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void InsertDigit(CalculatorState state, string digitText)
+        {
+            var digit = byte.Parse(digitText);
+            if (state.NeedClearX)
+            {
+                state.XRegister = 0;
+            }
+
+            state.XRegister = state.XRegister * 10 + digit;
+            state.NeedClearX = false;
+        }
+
+        public void Clear(CalculatorState state)
+        {
+            state.XRegister = 0;
+        }
+
+        public void InsertOperation(CalculatorState state, string opCode)
+        {
+            PerformOperation(state, state.OpCode);
+            MoveXToY(state);
+            state.OpCode = opCode;
+            state.NeedClearX = true;
+
+         
+        }
+
+        private void CalculateInverse(CalculatorState state)
+        {
+            if (state.XRegister != 0) // Проверка на деление на ноль
+            {
+                state.XRegister = 1.0 / state.XRegister;
+            }
+            else
+            {
+                throw new DivideByZeroException("Деление на ноль невозможно.");
+            }
+        }
+
+        
+
+        private void MoveXToY(CalculatorState state)
+        {
+            state.YRegister = state.XRegister;
+        }
+
+        private void PerformOperation(CalculatorState state, string opCode)
+        {
+            switch (opCode)
+            {
+                case "+":
+                    state.XRegister += state.YRegister;
+                    break;
+
+                case "-":
+                    state.XRegister = state.YRegister - state.XRegister;
+                    break;
+
+                case "*":
+                    state.XRegister *= state.YRegister;
+                    break;
+
+                case "/":
+                    if (state.XRegister == 0)
+                    {
+                        state.XRegister = double.NaN;
+                    }
+                    else
+                    {
+                        state.XRegister = state.YRegister / state.XRegister;
+                    }
+
+                    break;
+                case "1/x":
+                    if (state.XRegister != 0)
+                    {
+                        state.XRegister = 1 / state.XRegister;
+                    }
+                    else
+                    {
+                        state.XRegister = double.NaN; // Обработка деления на ноль
+                    }
+                    break;
+
+                case "x^2":
+                    state.XRegister = Math.Pow(state.XRegister, 2);
+                    break;
+
+                case "√x":
+                    if (state.XRegister >= 0)
+                    {
+                        state.XRegister = Math.Sqrt(state.XRegister);
+                    }
+                    else
+                    {
+                        state.XRegister = double.NaN; // Обработка извлечения корня из отрицательного числа
+                    }
+                    break;
+            }
+            
+
+        }
+    }
+}
---
+++ Calculator/Calculator/CalculatorForm.cs
@@ -0,0 +1,83 @@
+﻿using Calculator.Business.Entites;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private CalculatorService _service = new CalculatorService();
+
+        private CalculatorState _state = new CalculatorState();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+    
+
+    private void DigitButton_Click(object sender, EventArgs e)
+        {
+            var senderButton = (Button)sender;
+            var digitText = senderButton.Text;
+
+            _service.InsertDigit(_state, digitText);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            _service.Clear(_state);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            var senderButton = (Button)sender;
+            var opCode = senderButton.Text;
+
+            /*_service.InsertOperation(_state, opCode);
+            DisplayLabel.Text = _state.XRegister.ToString();*/
+
+            if (opCode == "x^2")
+            {
+                _state.XRegister = Math.Pow(_state.XRegister, 2);
+            }
+            else if (opCode == "√x")
+            {
+                if (_state.XRegister >= 0)
+                {
+                    _state.XRegister = Math.Sqrt(_state.XRegister);
+                }
+                else
+                {
+                    _state.XRegister = double.NaN; // Ошибка: корень из отрицательного числа
+                }
+            }
+            else if (opCode == "1/x")
+            {
+                if (_state.XRegister != 0)
+                {
+                    _state.XRegister = 1 / _state.XRegister;
+                }
+                else
+                {
+                    _state.XRegister = double.NaN; // Обработка деления на ноль
+                }
+            }
+                
+                DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+    }
+}
