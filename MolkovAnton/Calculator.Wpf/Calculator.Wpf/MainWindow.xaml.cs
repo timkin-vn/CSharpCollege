39c24dd7a5fbcf2ce24ddd84cf5b4574e397854a
Molkov Anton <molkov374@gmail.com>
2024-11-21 17:37:38 +0300

    Проект Calculator.Wpf

---
+++ MolkovAnton/Calculator.Wpf/Calculator.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Calculator.Business.Services;
+using Calculator.Wpf.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal CalculatorViewModel ViewModel => (CalculatorViewModel)DataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Content is string digit)
+            {
+                ViewModel.InsertDigit(digit);
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Clear();
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Content is string opCode)
+            {
+                ViewModel.InsertOperation(opCode);
+            }
+        }
+
+        private void ClearOperationLog_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.ClearOperationLog();
+
+        }
+
+        private void OnePartNumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            string ErrorMessage = ViewModel.ZeroCheck();
+            if (ErrorMessage!= null)
+            {
+                ViewModel.DisplayOutput = ErrorMessage;
+            }
+            else
+            {
+                ViewModel.OnePart();
+            }
+            
+        }
+
+        private void DegreeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.RaisetoDegree();
+           
+        }
+        private void SquareButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Square();
+        }
+
+        private void LogButton_Click(object sender, RoutedEventArgs e)
+        {
+            string ErrorMessage = ViewModel.ZeroCheck();
+            if (ErrorMessage != null)
+            {
+                ViewModel.DisplayOutput = ErrorMessage;
+            }
+            else
+            {
+                ViewModel.Ln();
+            }
+
+        }
+    }
+}
---
+++ MolkovAnton/Calculator.Wpf/Calculator.Wpf/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,99 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Wpf.ViewModel
+{
+    internal class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private CalculatorService _service = new CalculatorService();
+
+        private CalculatorState _state = new CalculatorState();
+
+        public string DisplayOutput { get; set; } = "0";
+
+        public ObservableCollection<string> OperationLog { get; set; } = new ObservableCollection<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void InsertDigit(string digit)
+        {
+            _service.InsertDigit(_state, digit);
+            DisplayResult();
+        }
+
+        public void Clear()
+        {
+            _service.Clear(_state);
+            DisplayResult();
+        }
+
+        public void InsertOperation(string opCode)
+        {
+            _service.PressOperation(_state, opCode);
+            DisplayResult();
+
+            if (!string.IsNullOrEmpty(_state.OperationLog))
+            {
+                OperationLog.Add(_state.OperationLog);
+            }
+        }
+
+        public void ClearOperationLog()
+        {
+            OperationLog.Clear();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void DisplayResult()
+        {
+            DisplayOutput = _state.XRegister.ToString();
+            OnPropertyChanged(nameof(DisplayOutput));
+        }
+
+        public void RaisetoDegree()
+        {
+            _service.RaiseToDegree(_state);
+            DisplayResult();
+        }
+
+        public void Square()
+        {
+            _service.CalculateRoot(_state);
+            DisplayResult();
+        }
+         
+        public void Ln()
+        {
+            _service.CalculateLn(_state);
+            DisplayResult();
+        }
+
+        public void OnePart()
+        {
+            _service.OnePartNumber(_state);
+            DisplayResult();
+        }
+
+        public string ZeroCheck()
+        {
+            if (_state.XRegister == 0) 
+            {
+                return "Деление на ноль невозможно";
+            }
+            return null;
+        }
+
+    }
+}
