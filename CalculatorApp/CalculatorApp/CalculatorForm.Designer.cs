f3045d3bfa4e5579f9b90b2e6abec987fac99fe8
Lugo2132 <artimaleksandrov2567@gmail.com>
2024-11-23 02:07:14 +0300

    CalculatorApp
    Завершена работа над проектом калькулятор (добавлены кнопка умножения и деления)

---
+++ CalculatorApp/CalculatorApp/CalculatorForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CalculatorApp
+{
+    partial class CalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.XTextBox = new System.Windows.Forms.TextBox();
+            this.YTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SubtractButton = new System.Windows.Forms.Button();
+            this.MultiplyButton = new System.Windows.Forms.Button();
+            this.DivideButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // XTextBox
+            // 
+            this.XTextBox.Location = new System.Drawing.Point(16, 73);
+            this.XTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.XTextBox.Name = "XTextBox";
+            this.XTextBox.Size = new System.Drawing.Size(132, 22);
+            this.XTextBox.TabIndex = 0;
+            // 
+            // YTextBox
+            // 
+            this.YTextBox.Location = new System.Drawing.Point(265, 73);
+            this.YTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.YTextBox.Name = "YTextBox";
+            this.YTextBox.Size = new System.Drawing.Size(132, 22);
+            this.YTextBox.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(157, 12);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(4);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(100, 28);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "+";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(153, 164);
+            this.ResultLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 16);
+            this.ResultLabel.TabIndex = 3;
+            // 
+            // SubtractButton
+            // 
+            this.SubtractButton.Location = new System.Drawing.Point(157, 49);
+            this.SubtractButton.Margin = new System.Windows.Forms.Padding(4);
+            this.SubtractButton.Name = "SubtractButton";
+            this.SubtractButton.Size = new System.Drawing.Size(100, 28);
+            this.SubtractButton.TabIndex = 4;
+            this.SubtractButton.Text = "-";
+            this.SubtractButton.UseVisualStyleBackColor = true;
+            // 
+            // MultiplyButton
+            // 
+            this.MultiplyButton.Location = new System.Drawing.Point(277, 123);
+            this.MultiplyButton.Name = "MultiplyButton";
+            this.MultiplyButton.Size = new System.Drawing.Size(100, 23);
+            this.MultiplyButton.TabIndex = 5;
+            this.MultiplyButton.Text = "*";
+            this.MultiplyButton.UseVisualStyleBackColor = true;
+            this.MultiplyButton.Click += new System.EventHandler(this.MultiplyButton_Click_1);
+            // 
+            // DivideButton
+            // 
+            this.DivideButton.Location = new System.Drawing.Point(31, 123);
+            this.DivideButton.Name = "DivideButton";
+            this.DivideButton.Size = new System.Drawing.Size(100, 23);
+            this.DivideButton.TabIndex = 0;
+            this.DivideButton.Text = "/";
+            this.DivideButton.Click += new System.EventHandler(this.DivideButton_Click_1);
+            // 
+            // CalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 210);
+            this.Controls.Add(this.DivideButton);
+            this.Controls.Add(this.MultiplyButton);
+            this.Controls.Add(this.SubtractButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.YTextBox);
+            this.Controls.Add(this.XTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CalculatorForm";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox XTextBox;
+        private System.Windows.Forms.TextBox YTextBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button SubtractButton;
+        private System.Windows.Forms.Button MultiplyButton;
+        private System.Windows.Forms.Button DivideButton;
+    }
+}
+
---
+++ CalculatorApp/CalculatorApp/CalculatorForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class CalculatorForm : Form
+    {
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            var input = XTextBox.Text;
+            if (!double.TryParse(input, out var x))
+            {
+                MessageBox.Show("Неверно введено первое число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                XTextBox.Focus();
+                XTextBox.SelectAll();
+                return;
+            }
+
+            input = YTextBox.Text;
+            if (!double.TryParse(input, out var y))
+            {
+                MessageBox.Show("Неверно введено второе число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                YTextBox.Focus();
+                YTextBox.SelectAll();
+                return;
+            }
+
+            var result = x + y;
+            ResultLabel.Text = $"Сумма = {result}";
+        }
+
+        private void DivideButton_Click_1(object sender, EventArgs e)
+        {
+            var input = XTextBox.Text;
+            if (!double.TryParse(input, out var x))
+            {
+                MessageBox.Show("Неверно введено первое число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                XTextBox.Focus();
+                XTextBox.SelectAll();
+                return;
+            }
+
+            input = YTextBox.Text;
+            if (!double.TryParse(input, out var y))
+            {
+                MessageBox.Show("Неверно введено второе число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                YTextBox.Focus();
+                YTextBox.SelectAll();
+                return;
+            }
+
+            if (y == 0)
+            {
+                MessageBox.Show("Деление на ноль невозможно", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var result = x / y;
+            ResultLabel.Text = $"Частное = {result}";
+        }
+
+        private void MultiplyButton_Click_1(object sender, EventArgs e)
+        {
+            var input = XTextBox.Text;
+            if (!double.TryParse(input, out var x))
+            {
+                MessageBox.Show("Неверно введено первое число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                XTextBox.Focus();
+                XTextBox.SelectAll();
+                return;
+            }
+
+            input = YTextBox.Text;
+            if (!double.TryParse(input, out var y))
+            {
+                MessageBox.Show("Неверно введено второе число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                YTextBox.Focus();
+                YTextBox.SelectAll();
+                return;
+            }
+
+            var result = x * y;
+            ResultLabel.Text = $"Произведение = {result}";
+        }
+    }
+}
