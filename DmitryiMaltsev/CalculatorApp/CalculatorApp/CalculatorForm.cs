4759a2f8a792da2d6edce16138aff076ef827b51
Dmityi98 <malsevd2@gmail.com>
2024-11-22 10:53:58 +0300

    Сделан проект калькулятов на WinForm

---
+++ DmitryiMaltsev/CalculatorApp/CalculatorApp/CalculatorForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class CalculatorForm : Form
+    {
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            var input = XTextBox.Text;
+            if (!double.TryParse(input, out var x))
+            {
+                MessageBox.Show("Неверно введено первое число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                XTextBox.Focus();
+                XTextBox.SelectAll();
+                return;
+            }
+
+            input = YTextBox.Text;
+            if (!double.TryParse(input, out var y))
+            {
+                MessageBox.Show("Неверно введено второе число", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                YTextBox.Focus();
+                YTextBox.SelectAll();
+                return;
+            }
+
+            var result = x + y;
+
+            ResultLabel.Text = $"Сумма = {result}";
+        }
+    }
+}
---
+++ Samples/Calculator/Calculator/CalculatorForm.cs
@@ -0,0 +1,69 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private CalculatorState _state = new CalculatorState();
+
+        private CalculatorService _service = new CalculatorService();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var digit = button.Text;
+            _service.InsertDigit(_state, digit);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void CalculatorForm_Load(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            _service.Clear(_state);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var opCode = button.Text;
+            _service.PressOperation(_state, opCode);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void square_Button_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var digit = DisplayLabel.Text;
+            _service.SquareFun(_state, digit);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+
+        private void factorial_Button_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var digit = DisplayLabel.Text;
+            _service.FactorialFun(_state, digit);
+            DisplayLabel.Text = _state.XRegister.ToString();
+        }
+    }
+}
