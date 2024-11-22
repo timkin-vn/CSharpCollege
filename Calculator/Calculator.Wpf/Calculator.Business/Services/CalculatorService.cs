dd60bab6cd9f8b5947a4acdf276a5f8158567d35
DESKTOP-0V5QKEK\пан4лайнер <flik191chan@gmail.com>
2024-11-22 14:41:44 +0300

    Калькулятор Wpf

---
+++ Calculator/Calculator.Wpf/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,93 @@
+﻿using Calculator.Business.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void InsertDigit(CalculatorState state, string digitText)
+        {
+            var digit = byte.Parse(digitText);
+            if (state.NeedClearX)
+            {
+                state.XRegister = 0;
+            }
+
+            state.XRegister = state.XRegister * 10 + digit;
+            state.NeedClearX = false;
+        }
+
+        public void Clear(CalculatorState state)
+        {
+            state.XRegister = 0;
+        }
+
+        public void InsertOperation(CalculatorState state, string opCode)
+        {
+            PerformOperation(state, state.OpCode);
+            MoveXToY(state);
+            state.OpCode = opCode;
+            state.NeedClearX = true;
+        }
+
+        private void MoveXToY(CalculatorState state)
+        {
+            state.YRegister = state.XRegister;
+        }
+
+        public void Negativ_and_Positiv_Operation(CalculatorState state)
+        {
+            state.XRegister = -state.XRegister;
+
+            state.NeedClearX = true;
+        }
+
+        private void PerformOperation(CalculatorState state, string opCode)
+        {
+            var log = $"{state.YRegister} {opCode} {state.XRegister} = ";
+            state.OperationLog = null;
+
+            switch (opCode)
+            {
+                case "+":
+                    state.XRegister += state.YRegister;
+                    state.OperationLog = log + state.XRegister.ToString();
+                    break;
+
+                case "-":
+                    state.XRegister = state.YRegister - state.XRegister;
+                    state.OperationLog = log + state.XRegister.ToString();
+                    break;
+
+                case "*":
+                    state.XRegister *= state.YRegister;
+                    state.OperationLog = log + state.XRegister.ToString();
+                    break;
+
+                case "x²":
+                    state.XRegister = state.XRegister * state.XRegister;
+                    state.OperationLog = $"x² {state.XRegister}";
+                    break;
+
+                case "√":
+                    if (state.XRegister < 0)
+                    {
+                        state.XRegister = double.NaN;
+                        break;
+                    }
+                    state.XRegister = Math.Sqrt(state.XRegister);
+                    state.OperationLog = $"√ {state.XRegister}";
+                    break;
+
+                case "+/-":
+                    state.XRegister = -state.XRegister;
+                    state.OperationLog = $"+/- {state.XRegister}";
+                    break;
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator.Wpf/Calculator.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Calculator.Business.Entites;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Wpf.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private CalculatorService _service = new CalculatorService();
+
+        private CalculatorState _state = new CalculatorState();
+
+        public string OutputDisplay { get; set; } = "0";
+
+        public ObservableCollection<string> OperationLog { get; set; } = new ObservableCollection<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void InsertDigit(string digit)
+        {
+            _service.InsertDigit(_state, digit);
+            OutputDisplay = _state.XRegister.ToString();
+            OnPropertyChanged(nameof(OutputDisplay));
+        }
+
+        public void Clear()
+        {
+            _service.Clear(_state);
+            OutputDisplay = _state.XRegister.ToString();
+            OnPropertyChanged(nameof(OutputDisplay));
+        }
+
+        public void PressOperation(string opCode)
+        {
+            _service.InsertOperation(_state, opCode);
+            OutputDisplay = _state.XRegister.ToString();
+            OnPropertyChanged(nameof(OutputDisplay));
+            if (!string.IsNullOrEmpty(_state.OperationLog))
+            {
+                OperationLog.Add(_state.OperationLog);
+            }
+        }
+
+        public void ClearLog()
+        {
+            OperationLog.Clear();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
