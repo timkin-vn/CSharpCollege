da901389ed567bdec04cca376abfcac71eca7f18
Тимкин Владимир <timkin_vn@mail.ru>
2024-11-27 19:14:54 +0700

    Проект Картотека для группы 23

---
+++ Samples/CardFile.Wpf/CaedFile.DataAccess/Dtos/CardDto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.DataAccess.Dtos
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public decimal PaymentAmount { get; set; }
+
+        public int ChildrenCount { get; set; }
+
+        public CardDto Clone()
+        {
+            return new CardDto
+            {
+                Id = Id,
+                FirstName = FirstName,
+                MiddleName = MiddleName,
+                LastName = LastName,
+                BirthDate = BirthDate,
+                PaymentAmount = PaymentAmount,
+                ChildrenCount = ChildrenCount,
+            };
+        }
+
+        public void Update(CardDto newCard)
+        {
+            FirstName = newCard.FirstName;
+            MiddleName = newCard.MiddleName;
+            LastName = newCard.LastName;
+            BirthDate = newCard.BirthDate;
+            PaymentAmount = newCard.PaymentAmount;
+            ChildrenCount = newCard.ChildrenCount;
+        }
+    }
+}
---
+++ Samples/CardFile.Wpf/CardFile.Wpf/ViewModels/CardFileViewModel.cs
@@ -0,0 +1,127 @@
+﻿using CardFile.Business.Entities;
+using CardFile.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Wpf.ViewModels
+{
+    public class CardFileViewModel : INotifyPropertyChanged
+    {
+        private readonly CardFileService _service = new CardFileService();
+
+        private CardViewModel _selectedCard;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<CardViewModel> Cards { get; set; } = new ObservableCollection<CardViewModel>();
+
+        public CardViewModel SelectedCard
+        {
+            get => _selectedCard;
+            set
+            {
+                _selectedCard = value;
+                OnPropertyChanged(nameof(IsEditButtonEnabled));
+            }
+        }
+
+        public bool IsEditButtonEnabled => SelectedCard != null;
+
+        public CardFileViewModel()
+        {
+            ShowAll();
+        }
+
+        public CardViewModel GetNewCard()
+        {
+            return new CardViewModel();
+        }
+
+        public CardViewModel GetSelectedCard()
+        {
+            return SelectedCard;
+        }
+
+        public bool Save(CardViewModel cardViewModel)
+        {
+            var card = FromViewModel(cardViewModel);
+            card = _service.Save(card);
+            var result = card != null;
+            if (result)
+            {
+                ShowAll();
+                var selectedViewModel = Cards.FirstOrDefault(c => c.Id == card.Id);
+                if (selectedViewModel != null)
+                {
+                    SelectedCard = selectedViewModel;
+                    OnPropertyChanged(nameof(SelectedCard));
+                }
+            }
+
+            return result;
+        }
+
+        public bool DeleteSelected()
+        {
+            if (SelectedCard == null)
+            {
+                return false;
+            }
+
+            if (!_service.Delete(SelectedCard.Id))
+            {
+                return false;
+            }
+
+            ShowAll();
+            return true;
+        }
+
+        private CardViewModel ToViewModel(Card card)
+        {
+            return new CardViewModel
+            {
+                Id = card.Id,
+                FirstName = card.FirstName,
+                MiddleName = card.MiddleName,
+                LastName = card.LastName,
+                BirthDate = card.BirthDate,
+                PaymentAmount = card.PaymentAmount,
+                ChildrenCount = card.ChildrenCount,
+            };
+        }
+
+        private Card FromViewModel(CardViewModel card)
+        {
+            return new Card
+            {
+                Id = card.Id,
+                FirstName = card.FirstName,
+                MiddleName = card.MiddleName,
+                LastName = card.LastName,
+                BirthDate = card.BirthDate,
+                PaymentAmount = card.PaymentAmount,
+                ChildrenCount = card.ChildrenCount,
+            };
+        }
+
+        private void ShowAll()
+        {
+            Cards.Clear();
+            foreach (var card in _service.GetAll())
+            {
+                Cards.Add(ToViewModel(card));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
