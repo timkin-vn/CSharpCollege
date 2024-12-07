1b240c651db114da374ac12c3c7c75c6f328e4e0
Lugo2132 <artimaleksandrov2567@gmail.com>
2024-12-07 06:35:23 +0300

    доделан проект CardFile WPF

---
+++ CardFile WPF/CardFile.Wpf/ViewModels/CardViewModel.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Wpf.ViewModels
+{
+    public class CardViewModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _middleName;
+        private DateTime _birthDate = new DateTime(2000, 6, 15);
+        private decimal _paymentAmount;
+        private int _childrenCount;
+        private string _email;
+        private string _phoneNumber;
+        private string _address;
+        private string _position;
+        private string _notes;
+
+        public int Id { get; set; }
+
+        public string Fio => $"{LastName} {FirstName} {MiddleName}";
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(Fio));
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string MiddleName
+        {
+            get => _middleName;
+            set
+            {
+                _middleName = value;
+                OnPropertyChanged(nameof(Fio));
+                OnPropertyChanged(nameof(MiddleName));
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(Fio));
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                _address = value;
+                OnPropertyChanged(nameof(Address));
+            }
+        }
+
+        public string Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                OnPropertyChanged(nameof(Position));
+            }
+        }
+
+        public string Notes
+        {
+            get => _notes;
+            set
+            {
+                _notes = value;
+                OnPropertyChanged(nameof(Notes));
+            }
+        }
+        public DateTime BirthDate
+        {
+            get => _birthDate;
+            set
+            {
+                _birthDate = value;
+                OnPropertyChanged(nameof(BirthDate));
+            }
+        }
+
+        public string BirthDateText => BirthDate.ToShortDateString();
+
+        public decimal PaymentAmount
+        {
+            get => _paymentAmount;
+            set
+            {
+                _paymentAmount = value;
+                OnPropertyChanged(nameof(PaymentAmount));
+            }
+        }
+
+        public string PaymentAmountText => PaymentAmount.ToString("#,##0.00 р\\.");
+
+        public int ChildrenCount
+        {
+            get => _childrenCount;
+            set
+            {
+                _childrenCount = value;
+                OnPropertyChanged(nameof(ChildrenCount));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void CopyFrom(CardViewModel model)
+        {
+            Id = model.Id;
+            FirstName = model.FirstName;
+            MiddleName = model.MiddleName;
+            LastName = model.LastName;
+            BirthDate = model.BirthDate;
+            PaymentAmount = model.PaymentAmount;
+            ChildrenCount = model.ChildrenCount;
+            Email = model.Email; // Copy Email
+            PhoneNumber = model.PhoneNumber; // Copy Phone Number
+            Address = model.Address; // Copy Address
+            Position = model.Position; // Copy Position
+            Notes = model.Notes; // Copy Notes
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
