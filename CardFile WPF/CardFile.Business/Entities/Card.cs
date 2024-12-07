a8521e720ac0c15b28e278d949edade00d9ebdf9
Lugo2132 <artimaleksandrov2567@gmail.com>
2024-12-07 06:36:49 +0300

    доделан проект CardFile WPF

---
+++ CardFile WPF/CardFile.Business/Entities/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Business.Entities
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public decimal PaymentAmount { get; set; }
+
+        public int ChildrenCount { get; set; }
+
+        public string Email { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public string Position { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
