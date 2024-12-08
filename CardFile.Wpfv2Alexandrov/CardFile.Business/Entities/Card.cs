81129a23ca9fd9207ac5f39c4a735da392ef1446
Nilter-vulpera <rikudo.sanin861@mail.ru>
2024-12-08 21:05:54 +0300

    Картотека Кредитов банка

---
+++ CardFile.Wpfv2Alexandrov/CardFile.Business/Entities/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Business.Entities
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public decimal Credit { get; set; }
+
+        public DateTime DateOfEndCredit { get; set; }
+    }
+}
