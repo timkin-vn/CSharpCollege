33c36b7678ce28ed7389abd92791fc02602c4942
Zaklzzzy <zaletin123@gmail.com>
2024-12-08 17:34:25 +0300

    Modify Cards

---
+++ ZaletinArtem/CardFile.Wpf/CardFile.Business/Entities/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CardFile.Business.Entities
+{
+    public class Card
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; } 
+        public string Author { get; set; }
+        public DateTime PublicationDate { get; set; } 
+        public string Genre { get; set; } 
+        public int PageCount { get; set; } 
+        public decimal Price { get; set; }
+    }
+}
