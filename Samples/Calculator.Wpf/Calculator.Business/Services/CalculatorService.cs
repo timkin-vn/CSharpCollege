13b096540005addd7161d306bead1b6430d84e01
tep98 <Korotkovstepan95@gmail.com>
2024-12-12 02:09:40 +0300

    Make calculator wpf task

--- Samples/Calculator.Wpf/Calculator.Business/Services/CalculatorService.cs
+++ Samples/Calculator.Wpf/Calculator.Business/Services/CalculatorService.cs
@@ -40,6 +40,54 @@ namespace Calculator.Business.Services
             state.YRegister = state.XRegister;
         }
 
+        public void Backspace(CalculatorState state)
+        {
+            var xText = state.XRegister.ToString();
+            if (xText.Length > 1)
+            {
+                xText = xText.Substring(0, xText.Length - 1);
+                state.XRegister = double.Parse(xText);
+            }
+            else
+            {
+                state.XRegister = 0;
+            }
+        }
+
+        public void Square(CalculatorState state)
+        {
+            state.XRegister *= state.XRegister;
+        }
+
+        public void PrepareForExponentiation(CalculatorState state)
+        {
+            MoveXToY(state);
+            state.OpCodeRegister = "^";
+            state.NeedClear = true;
+        }
+
+        public void SquareRoot(CalculatorState state)
+        {
+            if (state.XRegister >= 0)
+            {
+                state.XRegister = Math.Sqrt(state.XRegister);
+            }
+            else
+            {
+                state.XRegister = double.NaN;
+            }
+        }
+
+
+        public void InsertDecimal(CalculatorState state)
+        {
+            if (!state.XRegister.ToString().Contains("."))
+            {
+                state.XRegister = double.Parse(state.XRegister.ToString() + ".");
+            }
+        }
+
+
         private void MakeOperation(CalculatorState state, string opCode)
         {
             var operationLog = $"{state.YRegister} {opCode} {state.XRegister} = ";
@@ -47,6 +95,9 @@ namespace Calculator.Business.Services
 
             switch (opCode)
             {
+                case "^":
+                    state.XRegister = Math.Pow(state.YRegister, state.XRegister);
+                    break;
                 case "+":
                     state.XRegister += state.YRegister;
                     state.OperationLog = operationLog + state.XRegister.ToString();
