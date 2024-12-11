22061da6ff74f1dfa686053d62aa7446bdbc78e2
tep98 <Korotkovstepan95@gmail.com>
2024-12-12 00:30:59 +0300

    make a calculator task

--- Dev22/Calculator/Calculator.Business/Services/CalculatorService.cs
+++ Dev22/Calculator/Calculator.Business/Services/CalculatorService.cs
@@ -39,6 +39,53 @@ namespace Calculator.Business.Services
             state.YRegister = state.XRegister;
         }
 
+        public void Backspace(CalculatorState state)
+        {
+            var xText = state.XRegister.ToString();
+            if (xText.Length > 1)
+            {
+                xText = xText.Substring(0, xText.Length - 1);
+                state.XRegister = double.Parse(xText);
+            }
+            else
+            {
+                state.XRegister = 0;
+            }
+        }
+
+        public void Square(CalculatorState state)
+        {
+            state.XRegister *= state.XRegister;
+        }
+
+        public void PrepareForExponentiation(CalculatorState state)
+        {
+            MoveXToY(state);
+            state.OpCode = "^";
+            state.NeedClearX = true;
+        }
+
+        public void SquareRoot(CalculatorState state)
+        {
+            if (state.XRegister >= 0)
+            {
+                state.XRegister = Math.Sqrt(state.XRegister);
+            }
+            else
+            {
+                state.XRegister = double.NaN; // Корень из отрицательного числа
+            }
+        }
+
+        public void InsertDecimal(CalculatorState state)
+        {
+            if (!state.XRegister.ToString().Contains("."))
+            {
+                state.XRegister = double.Parse(state.XRegister.ToString() + ".");
+            }
+        }
+
+
         private void PerformOperation(CalculatorState state, string opCode)
         {
             switch (opCode)
@@ -66,7 +113,12 @@ namespace Calculator.Business.Services
                     }
 
                     break;
+                case "^":
+                    state.XRegister = Math.Pow(state.YRegister, state.XRegister);
+                    break;
+
             }
         }
+
     }
 }
