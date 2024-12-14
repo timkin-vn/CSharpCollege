c7bdda2daef3ce2b876dc1886ccb45caa43caff6
Sinelnikov_Yurii <chillldragam9911@gmail.com>
2024-12-14 06:08:39 +0300

    no message

---
+++ Calculator.Wpf/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,127 @@
+﻿using Calculator.Business.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void InsertDigit(CalculatorState state, string digitText)
+        {
+            var digit = byte.Parse(digitText);
+            if (state.NeedClearX)
+            {
+                state.XRegister = 0;
+            }
+
+            state.XRegister = state.XRegister * 10 + digit;
+            state.NeedClearX = false;
+        }
+
+        public void Clear(CalculatorState state)
+        {
+            state.XRegister = 0;
+        }
+
+        public void InsertOperation(CalculatorState state, string opCode)
+        {
+            PerformOperation(state, state.OpCode);
+            MoveXToY(state);
+            state.OpCode = opCode;
+            state.NeedClearX = true;
+        }
+
+        public void InsertDegree(CalculatorState state)
+        {
+            PerformDegree2(state);
+            MoveXToY(state);
+            state.NeedClearX = true;
+            
+        }
+
+        public void Insert_1x(CalculatorState state)
+        {
+            Perform_1x(state);
+            MoveXToY(state);
+            state.NeedClearX = true;
+
+        }
+
+        private void MoveXToY(CalculatorState state)
+        {
+            state.YRegister = state.XRegister;
+        }
+
+        private void PerformOperation(CalculatorState state, string opCode)
+        {
+            var log = $"{state.YRegister} {opCode} {state.XRegister} = ";
+            state.OperationLog = null;
+
+            switch (opCode)
+            {
+                case "+":
+                    state.XRegister += state.YRegister;
+                    state.OperationLog = log + state.XRegister.ToString();
+                    break;
+
+                case "-":
+                    state.XRegister = state.YRegister - state.XRegister;
+                    state.OperationLog = log + state.XRegister.ToString();
+                    break;
+
+                case "x^y":
+
+                    log = $"{state.YRegister} ^ {state.XRegister} = ";
+                    state.XRegister = Math.Pow(state.YRegister, state.XRegister);
+                    state.OperationLog = log + state.XRegister.ToString();
+                    break;
+
+                case "*":
+                    state.XRegister *= state.YRegister;
+                    state.OperationLog = log + state.XRegister.ToString();
+                    break;
+
+                case "/":
+                    if (state.XRegister == 0)
+                    {
+                        state.XRegister = double.NaN;
+                    }
+                    else
+                    {
+                        state.XRegister = state.YRegister / state.XRegister;
+                        state.OperationLog = log + state.XRegister.ToString();
+                    }
+
+                    break;
+            }
+        }
+
+        private void PerformDegree2(CalculatorState state)
+        {
+
+            var log = $"{state.XRegister} ^ 2 = ";
+            state.XRegister *= state.XRegister;
+            state.OperationLog = log + state.XRegister.ToString();
+
+        }
+
+        private void Perform_1x(CalculatorState state)
+        {
+
+            var log = $"1 / {state.XRegister} = ";
+            state.XRegister = 1 / state.XRegister;
+            state.OperationLog = log + state.XRegister.ToString();
+
+        }
+
+
+
+    }
+
+
+
+}
---
+++ Calculator.Wpf/Calculator.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Calculator.Business.Entites;
+using Calculator.Wpf.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButoon_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Content is string digit)
+            {
+                ViewModel.InsertDigit(digit);
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Clear();
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Content is string opCode)
+            {
+                ViewModel.PressOperation(opCode);
+            }
+        }
+
+
+
+        private void Degree2click(object sender, RoutedEventArgs e)
+        {
+                ViewModel.PressDegree2();
+        }
+
+        private void Click_1x_click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Press_1x();
+        }
+
+        
+
+        private void ClearLogButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.ClearLog();
+        }
+
+
+        private void FULL_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.ClearLog();
+            ViewModel.Clear();
+        }
+
+    }
+}
