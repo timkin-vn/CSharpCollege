74a9ceb4b7f9a4c274869e165a1009ddc12a621e
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-01-19 03:02:10 +0300

    Добавил изменения контура и отделения фигуры
    Добавил изменения контура и отделения фигуры на один шаг .Пытался сделать сохранения цвета, но не получилось .Хорошего вам дня.

---
+++ GraphEditor_Yaroslav_Reksi/GraphEditor/ViewModels/RectangleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphEditor.ViewModels
+{
+    internal class RectangleViewModel
+    {
+        public Rectangle Rectangle { get; set; }
+
+        public Color FillColor { get; set; } = Color.Yellow;
+
+        public Color DrawColor { get; set; } = Color.Blue;
+    }
+}
