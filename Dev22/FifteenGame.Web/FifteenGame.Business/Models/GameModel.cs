e7c9661903eb57b11c691ad6c8e6f42c8044e9ca
Тимкин Владимир <timkin_vn@mail.ru>
2025-02-07 18:55:00 +0700

    Разработка веб-приложения

---
+++ Dev22/FifteenGame.Web/FifteenGame.Business/Models/GameModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Business.Models
+{
+    public class GameModel
+    {
+        public const int RowCount = 4;
+
+        public const int ColumnCount = 4;
+
+        public const int FreeCellValue = -1;
+
+        private int[,] _cells = new int[RowCount, ColumnCount];
+
+        public int this[int row, int column]
+        {
+            get => _cells[row, column];
+            internal set => _cells[row, column] = value;
+        }
+
+        public int FreeCellRow { get; internal set; }
+
+        public int FreeCellColumn { get; internal set; }
+    }
+}
