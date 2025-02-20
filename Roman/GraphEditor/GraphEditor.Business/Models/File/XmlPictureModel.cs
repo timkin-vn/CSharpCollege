23b31ee584c193d6c0dd99af0ffbca54cfbbb256
MarkProMaster229 <164912605+MarkProMaster229@users.noreply.github.com>
2025-02-20 15:26:32 +0300

    проект1

---
+++ Roman/GraphEditor/GraphEditor.Business/Models/File/XmlPictureModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace GraphEditor.Business.Models.File
+{
+    [Serializable]
+    [XmlRoot("Picture")]
+    public class XmlPictureModel
+    {
+        [XmlArray("Rectangles")]
+        [XmlArrayItem("Rectangle")]
+        public List<XmlRectangleModel> Rectangles { get; set; }
+    }
+}
---
+++ Roman/GraphEditor/GraphEditor/GraphEditorForm.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace GraphEditor
+{
+    partial class GraphEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GraphEditorForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.DeleteToolButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.FillToolMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.ForwardToolMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ColorDialog = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileOpenMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileSaveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileSaveAsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExportMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.OpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.CreateRectangleToolButton = new System.Windows.Forms.ToolStripButton();
+            this.fon = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateRectangleToolButton,
+            this.DeleteToolButton,
+            this.toolStripDropDownButton1,
+            this.toolStripSplitButton1,
+            this.fon});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // DeleteToolButton
+            // 
+            this.DeleteToolButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.DeleteToolButton.Enabled = false;
+            this.DeleteToolButton.Image = ((System.Drawing.Image)(resources.GetObject("DeleteToolButton.Image")));
+            this.DeleteToolButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DeleteToolButton.Name = "DeleteToolButton";
+            this.DeleteToolButton.Size = new System.Drawing.Size(55, 22);
+            this.DeleteToolButton.Text = "Удалить";
+            this.DeleteToolButton.Click += new System.EventHandler(this.DeleteToolButton_Click);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FillToolMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(46, 22);
+            this.toolStripDropDownButton1.Text = "Цвет";
+            // 
+            // FillToolMenuItem
+            // 
+            this.FillToolMenuItem.Name = "FillToolMenuItem";
+            this.FillToolMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.FillToolMenuItem.Text = "Заливка...";
+            this.FillToolMenuItem.Click += new System.EventHandler(this.FillToolMenuItem_Click);
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ForwardToolMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(71, 22);
+            this.toolStripSplitButton1.Text = "Порядок";
+            // 
+            // ForwardToolMenuItem
+            // 
+            this.ForwardToolMenuItem.Enabled = false;
+            this.ForwardToolMenuItem.Name = "ForwardToolMenuItem";
+            this.ForwardToolMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ForwardToolMenuItem.Text = "Ближе";
+            this.ForwardToolMenuItem.Click += new System.EventHandler(this.ForwardToolMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileOpenMenuItem,
+            this.FileSaveMenuItem,
+            this.FileSaveAsMenuItem,
+            this.ExportMenuItem,
+            this.toolStripMenuItem1,
+            this.ExitMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // FileOpenMenuItem
+            // 
+            this.FileOpenMenuItem.Name = "FileOpenMenuItem";
+            this.FileOpenMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileOpenMenuItem.Text = "Открыть...";
+            this.FileOpenMenuItem.Click += new System.EventHandler(this.FileOpenMenuItem_Click);
+            // 
+            // FileSaveMenuItem
+            // 
+            this.FileSaveMenuItem.Name = "FileSaveMenuItem";
+            this.FileSaveMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileSaveMenuItem.Text = "Сохранить";
+            this.FileSaveMenuItem.Click += new System.EventHandler(this.FileSaveMenuItem_Click);
+            // 
+            // FileSaveAsMenuItem
+            // 
+            this.FileSaveAsMenuItem.Name = "FileSaveAsMenuItem";
+            this.FileSaveAsMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileSaveAsMenuItem.Text = "Сохранить как...";
+            this.FileSaveAsMenuItem.Click += new System.EventHandler(this.FileSaveAsMenuItem_Click);
+            // 
+            // ExportMenuItem
+            // 
+            this.ExportMenuItem.Name = "ExportMenuItem";
+            this.ExportMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ExportMenuItem.Text = "Экспорт...";
+            this.ExportMenuItem.Click += new System.EventHandler(this.ExportMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // ExitMenuItem
+            // 
+            this.ExitMenuItem.Name = "ExitMenuItem";
+            this.ExitMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ExitMenuItem.Text = "Выход";
+            this.ExitMenuItem.Click += new System.EventHandler(this.ExitMenuItem_Click);
+            // 
+            // CreateRectangleToolButton
+            // 
+            this.CreateRectangleToolButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.CreateRectangleToolButton.Image = ((System.Drawing.Image)(resources.GetObject("CreateRectangleToolButton.Image")));
+            this.CreateRectangleToolButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CreateRectangleToolButton.Name = "CreateRectangleToolButton";
+            this.CreateRectangleToolButton.Size = new System.Drawing.Size(54, 22);
+            this.CreateRectangleToolButton.Text = "Создать";
+            this.CreateRectangleToolButton.Click += new System.EventHandler(this.CreateRectangleToolButton_Click);
+            // 
+            // fon
+            // 
+            this.fon.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fon.Image = ((System.Drawing.Image)(resources.GetObject("fon.Image")));
+            this.fon.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.fon.Name = "fon";
+            this.fon.Size = new System.Drawing.Size(98, 22);
+            this.fon.Text = "изменить фон";
+            this.fon.Click += new System.EventHandler(this.fon_Click);
+            // 
+            // GraphEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GraphEditorForm";
+            this.Text = "Графический редактор";
+            this.Load += new System.EventHandler(this.GraphEditorForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GraphEditorForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GraphEditorForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GraphEditorForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GraphEditorForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.GraphEditorForm_MouseUp);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton DeleteToolButton;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem FillToolMenuItem;
+        private System.Windows.Forms.ColorDialog ColorDialog;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem ForwardToolMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileOpenMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileSaveMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileSaveAsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExportMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ExitMenuItem;
+        private System.Windows.Forms.SaveFileDialog SaveDialog;
+        private System.Windows.Forms.OpenFileDialog OpenDialog;
+        private System.Windows.Forms.ToolStripButton CreateRectangleToolButton;
+        private System.Windows.Forms.ToolStripButton fon;
+    }
+}
+
