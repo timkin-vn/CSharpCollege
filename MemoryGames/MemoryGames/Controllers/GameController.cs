0ae0f5dc14019f84c711c6273844f9adb54e1a67
KurymiCh <164912181+SempaiCh@users.noreply.github.com>
2025-03-10 09:03:47 +0300

    Игра мемори asp веб (memory game asp)

---
+++ MemoryGames/MemoryGames/Controllers/GameController.cs
@@ -0,0 +1,52 @@
+﻿using MemoryGames.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc; 
+
+namespace MemoryGames.Controllers 
+{
+    public class GameController : Controller 
+    {
+        
+        private const string GameModelSessionKey = "GameModel";
+
+       
+        private GameModel GetGameModel()
+        {
+            GameModel gameModel = Session[GameModelSessionKey] as GameModel;
+            if (gameModel == null)
+            {
+                gameModel = new GameModel(3, 2);
+                Session[GameModelSessionKey] = gameModel;
+            }
+            return gameModel;
+        }
+
+        public ActionResult Index()
+        {
+            GameModel gameModel = GetGameModel();
+            return View(gameModel.Cards);
+        }
+
+        [HttpPost]
+        public JsonResult FlipCard(int id)
+        {
+
+            GameModel gameModel = GetGameModel();
+
+            gameModel.CardClicked(id);
+
+
+            if (gameModel.FirstFlippedCard == null)
+            {
+                
+                System.Threading.Thread.Sleep(1000); 
+                gameModel.FlipBackUnmatchedCards();
+            }
+
+
+            return Json(gameModel.Cards.Select(c => new { c.Id, c.ImagePath, c.IsFlipped, c.IsMatched }));
+        }
+    }
+}
---
+++ MemoryGames/MemoryGames/Models/GameModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//состояние игры
+namespace MemoryGames.Model
+{
+    public class GameModel
+    {
+        public List<Card> Cards { get; private set; }
+        public Card FirstFlippedCard { get; set; } 
+        public int Rows { get; }
+        public int Cols { get; }
+
+        public event EventHandler GameWon;
+        public event EventHandler CardsUpdated;
+
+        public GameModel(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            string[] imagePaths = {
+                "~/Content/images/image1.png",
+                "~/Content/images/image.png",
+                "~/Content/images/image3.png"
+            };
+
+            List<Card> allCards = new List<Card>();
+
+            
+            for (int i = 0; i < imagePaths.Length; i++)
+            {
+                allCards.Add(new Card(i * 2, imagePaths[i]));
+                allCards.Add(new Card(i * 2 + 1, imagePaths[i]));
+            }
+
+            
+            Random rng = new Random();
+            Cards = allCards.OrderBy(x => rng.Next()).ToList();
+
+            FirstFlippedCard = null;
+        }
+
+
+        public void CardClicked(int cardId)
+        {
+            var clickedCard = Cards.FirstOrDefault(c => c.Id == cardId);
+
+            if (clickedCard == null || clickedCard.IsMatched || clickedCard.IsFlipped)
+                return;
+
+            clickedCard.IsFlipped = true;
+
+            if (FirstFlippedCard == null)
+            {
+                FirstFlippedCard = clickedCard;
+            }
+            else
+            {
+                if (FirstFlippedCard.ImagePath == clickedCard.ImagePath)
+                {
+                    FirstFlippedCard.IsMatched = true;
+                    clickedCard.IsMatched = true;
+                }
+
+                FirstFlippedCard = null;
+
+                
+                CardsUpdated?.Invoke(this, EventArgs.Empty);
+
+                CheckForWin();
+            }
+
+            CardsUpdated?.Invoke(this, EventArgs.Empty); 
+
+        }
+
+        public void FlipBackUnmatchedCards()
+        {
+            foreach (var card in Cards)
+            {
+                if (!card.IsMatched)
+                    card.IsFlipped = false;
+            }
+
+            CardsUpdated?.Invoke(this, EventArgs.Empty); 
+        }
+
+        private void CheckForWin()
+        {
+            if (Cards.All(c => c.IsMatched))
+            {
+                GameWon?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
