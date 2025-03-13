b98aec81a66771bdb58b5c8a31bd4b22d182be7b
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-03-13 17:55:18 +0300

    Графика
    Сделанный графика

---
+++ GraphEditor_Romanov/GraphEditor/ViewModels/PictureViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GraphEditor.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphEditor.ViewModels
+{
+    internal class PictureViewModel
+    {
+        public IEnumerable<RectangleViewModel> Rectangles { get; set; }
+
+        public IEnumerable<MarkerViewModel> Markers { get; set; }
+        public bool IsNewFillColor { get; set; } = false;
+        public bool IsNewDrawColor { get; set; } = false;
+        public Color FillColor_New { get; set; } = Color.Orange;
+        public Color DrawColor_New { get; set; } = Color.Green;
+    }
+}
