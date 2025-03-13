6d2a9ee59bc7ed123e6c84acf2cf85c7aa9ed430
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-03-13 18:56:31 +0300

    Игра web

---
+++ FifteenGame.Web/FifteenGame.Business/Services/GameService.cs
@@ -0,0 +1,102 @@
+﻿using FifteenGame.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Business.Services
+{
+
+    public class GameService
+    {
+        public void Initialize(GameModel model)
+        {
+            string[] markCar = { "Ford", "Geely", "Lada", "BMW", "Tesla", "Audi", "Haval", "Skoda" };
+            Random random = new Random();
+
+
+            List<string> markCarPairs = new List<string>();
+
+            foreach (var color in markCar)
+            {
+                markCarPairs.Add(color);
+                markCarPairs.Add(color);
+            }
+
+
+
+            markCarPairs = markCarPairs.OrderBy(x => random.Next()).ToList();
+
+
+            for (int row = 0; row < GameModel.RowCount; row++)
+            {
+                for (int column = 0; column < GameModel.ColumnCount; column++)
+                {
+                    model[row, column] = markCarPairs[row * GameModel.ColumnCount + column];
+
+                }
+
+            }
+        }
+
+
+        public bool CheckMatch(GameModel model)
+        {
+
+            if (model.Fistbuuton == null || model.Secondbuuton == null) 
+            {
+                return false;
+            }
+
+            else if (model.Fistbuuton == model.Secondbuuton 
+                && (model.FisrsbuutonRowCol[0] != model.SecondbuutonRowCol[0] 
+                ||model.FisrsbuutonRowCol[1] != model.SecondbuutonRowCol[1]))
+            {
+                model.CountTrueColorButton++;
+
+                for (int row = 0; row < GameModel.RowCount; row++)
+                {
+                    for (int column = 0; column < GameModel.ColumnCount; column++)
+                    {
+                        if ((row == model.FisrsbuutonRowCol[0] &&
+                            column == model.FisrsbuutonRowCol[1]) || (row == model.SecondbuutonRowCol[0] &&
+                            column == model.SecondbuutonRowCol[1]))
+                        {
+
+                            model[row, column] = "";
+
+
+                        }
+                       
+
+                    }
+
+                }
+              
+
+                return true;
+            }
+             return false; 
+            
+        }
+        public bool IsGameOver(GameModel model)
+        {
+            if (model.CountTrueColorButton == 8)
+            {
+                model.CountTrueColorButton = 0;
+                model.SecondbuutonRowCol[0] = 4;
+                model.SecondbuutonRowCol[1] = 4;
+                model.FisrsbuutonRowCol[0] = 4;
+                model.FisrsbuutonRowCol[1] = 4;
+                return true; 
+            }
+            
+
+            return false;
+        }
+
+      
+    }
+}
---
+++ FifteenGame.Web/FifteenGame.Web/Models/CellViewModel.cs
@@ -0,0 +1,64 @@
+﻿using FifteenGame.Business.Models;
+using FifteenGame.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FifteenGame.Wpf.ViewModels
+{
+    public class CellViewModel : INotifyPropertyChanged
+    {
+        public string _carText;
+        public bool _isMakeUp;
+       
+
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public int[] ColumnRow { get; set; }
+        public bool IsMakeUp
+        {
+            get => _isMakeUp;
+            set
+            {
+                _isMakeUp = value;
+                OnPropertyChanged(nameof(TextButton));
+                OnPropertyChanged(nameof(IsMakeUp));
+            }
+        }
+       
+
+
+        public string CarText
+        {
+            get => _carText;
+            set
+            {
+                _carText = value;
+                OnPropertyChanged(nameof(CarText));
+                
+            }
+        }
+        
+        public string TextButton => IsMakeUp ? CarText : "Secret";
+
+        // Метод для проверки совпадения
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        
+
+
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
