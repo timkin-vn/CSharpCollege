0391fdcb9e15e68d93632718a48061fe1ae67bd3
NeForMal <164913951+NiGggA1998@users.noreply.github.com>
2025-03-13 16:33:18 +0200

    biss

---
+++ MIneSweepper.Bisiness/Model/CellsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIneSweepper.Bisiness.Model
+{
+    public class CellsModel
+    {
+        public bool IsMine { get; set; }
+        public int NeightborMines { get; set; }
+        public bool IsRevealed { get; set; }
+        public bool Isflag { get; set; }
+        
+        public CellsModel()
+        {
+            IsMine = false;
+            NeightborMines = 0;
+            IsRevealed = false;
+            Isflag = false;
+        }
+
+
+    }
+}
---
+++ MIneSweepper.Bisiness/Model/GameModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIneSweepper.Bisiness.Model
+{
+    public class GameModel
+    {
+       public const int RowCount = 9;
+
+        public const int ColumnCount = 9;
+        public int RedFlags { get; set; }
+
+        public int MineCount { get; set; }
+        public int CountRevealed { get; set; }
+
+        private CellsModel[,] _MineCells=new CellsModel[RowCount, ColumnCount];
+
+
+        public CellsModel this[int rows,int colums]
+        {
+            get => _MineCells[rows, colums];
+            internal set => _MineCells[rows, colums] = value;
+        }
+        
+    }
+}
