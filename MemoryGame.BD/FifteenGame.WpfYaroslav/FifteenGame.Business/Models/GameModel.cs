fe7ac8113f98fe220af3ce5975e2a4e6d679cfd7
Reksiyaroslav <maltzevv.ja@gmail.com>
2025-03-15 00:03:29 +0300

    Игра с бд
    игра с бд

---
+++ MemoryGame.BD/FifteenGame.WpfYaroslav/FifteenGame.Business/Models/GameModel.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace FifteenGame.Business.Models
+{
+
+    public class GameModel
+    {
+        public const int RowCount = 4;
+
+        public const int ColumnCount = 4;
+
+        private string[,] _cells = new string[RowCount, ColumnCount];
+
+
+        public string this[int row, int column]
+        {
+            get => _cells[row, column];
+            internal set => _cells[row, column] = value;
+        }
+       
+        public int[] SecondbuutonRowCol = { 4, 4 }, FisrsbuutonRowCol = {4,4};
+        public string Fistbuuton, Secondbuuton;
+        public int Clickbutton = 0;
+        public int CountMove = 0;
+
+        public int  CountTrueColorButton = 0;
+
+
+
+    }
+}
---
+++ MemoryGame.BD/FifteenGame.WpfYaroslav/FifteenGame.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,132 @@
+﻿using FifteenGame.Business.Models;
+using FifteenGame.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace FifteenGame.Wpf.ViewModels
+{
+    using FifteenGame.Business;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Windows;
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        private GameService _service = new GameService();
+        private GameModel _model = new GameModel();
+
+        public int countbuttonclick = 0;
+        
+        
+
+        
+        public ObservableCollection<CellViewModel> Cells { get; set; } = new ObservableCollection<CellViewModel>();
+
+        public MainWindowViewModel()
+        {
+            Initialize();
+
+
+        }
+
+        public void MakeMove(int[] colrow, Action gameFinishedAction)
+        {
+            if (countbuttonclick == 0)
+            {
+                _model.FisrsbuutonRowCol = colrow;
+                _model.Fistbuuton = _model[colrow[0], colrow[1]];
+                countbuttonclick++;
+                if (_service.CheckMatch(_model))
+                {
+                    FromModel(_model);
+                }
+
+            }
+            else if (countbuttonclick == 1)
+            {
+                _model.Secondbuuton = _model[colrow[0], colrow[1]];
+                _model.SecondbuutonRowCol = colrow;
+                if (_service.CheckMatch(_model))
+                {
+                    FromModel(_model);
+                }
+                
+
+                countbuttonclick = 0;
+               
+            }
+
+
+            FromModel(_model);
+            if (_service.IsGameOver(_model))
+            {
+                gameFinishedAction?.Invoke();
+            }
+
+        }
+                
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Initialize()
+        {
+            _service.Initialize(_model);
+            FromModel(_model);
+        }
+
+        private void FromModel(GameModel model)
+        {
+            Cells.Clear();
+            for (int row = 0; row < GameModel.RowCount; row++)
+            {
+                for (int column = 0; column < GameModel.ColumnCount; column++)
+                {
+
+                    if ((row == model.FisrsbuutonRowCol[0]&& column == model.FisrsbuutonRowCol[1])
+                        || (row == model.SecondbuutonRowCol[0] && column == model.SecondbuutonRowCol[1]))
+                    {
+                        Cells.Add(item: new CellViewModel
+                        {
+                            Row = row,
+                            ColumnRow = new int[] { row, column },
+                            Column = column,
+                            CarText = model[row, column],
+
+                            IsFaceUp = true,
+
+                        });
+                    }
+                    else if (model.Fistbuuton ==String.Empty|| model.Secondbuuton == String.Empty  || model[row, column] == "")
+                    {
+                        continue;
+                    }
+
+                    else {
+                        Cells.Add(item: new CellViewModel
+                        {
+                            Row = row,
+                            ColumnRow = new int[] { row, column },
+                            Column = column,
+                            CarText = model[row, column],
+
+                            IsFaceUp = false,
+
+                        });
+                    }
+                }
+            }
+        }
+    }
+   
+}
