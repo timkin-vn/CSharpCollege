abe2332e2d7b3e5e30e52787c0fbe0c130ddcf75
NektoPlaton <164889351+NektoPlaton@users.noreply.github.com>
2025-03-15 11:30:54 +0300

    GrafEditor

---
+++ GraphEditor/GraphEditor.Business/Models/File/XmlColorModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace GraphEditor.Business.Models.File
+{
+    [Serializable]
+    public class XmlColorModel
+    {
+        [XmlAttribute("Red")]
+        public int Red { get; set; }
+
+        [XmlAttribute("Green")]
+        public int Green { get; set; }
+
+        [XmlAttribute("Blue")]
+        public int Blue { get; set; }
+    }
+}
---
+++ GraphEditor/GraphEditor.Business/Models/PictureModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphEditor.Business.Models
+{
+    public class PictureModel
+    {
+        internal int Dx;
+
+        internal int Dy;
+
+        internal List<RectangleModel> RectangleList = new List<RectangleModel>();
+
+        public IEnumerable<RectangleModel> Rectangles => RectangleList;
+
+        public RectangleModel SelectedRectangle { get; internal set; }
+        public bool IsNewFillColor { get; set; } = false;
+        public bool IsNewDrawColor { get; set; } = false;
+        public Color FillColor_New { get; set; } = Color.Yellow;
+        public Color DrawColor_New { get; set; } = Color.Blue;
+
+        public PictureMode Mode { get; internal set; }
+    }
+}
