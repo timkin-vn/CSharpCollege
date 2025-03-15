661e496afd1fc7d93ea7449cf435ec8c1b6950c8
NeForMal <164913951+NiGggA1998@users.noreply.github.com>
2025-03-15 08:11:35 +0200

    bbb

---
+++ Dev22/FifteenGame.DB2/FifteenGame.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using FifteenGame.Common.BusinessModels;
+using FifteenGame.Wpf.ViewModels;
+using FifteenGame.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FifteenGame.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel(); // Убедитесь, что DataContext установлен
+        }
+
+        private void Cell_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var grid = sender as Grid;
+            if (grid != null)
+            {
+                var cellViewModel = grid.DataContext as CellViewModel;
+                if (cellViewModel != null)
+                {
+                    // Здесь вы можете выполнить нужные действия с cellViewModel
+                    // Например, сделать ход в игре
+                    int row = cellViewModel.Row;
+                    int column = cellViewModel.Column;
+
+                    // Вызовите метод для обработки хода
+                    (DataContext as MainWindowViewModel)?.MakeMove(row, column, () =>
+                    {
+                        // Действия при завершении игры
+                        if (MessageBox.Show("Игра окончена. Повторить?", "Поздравляем!", MessageBoxButton.YesNo, MessageBoxImage.Information) ==
+                        MessageBoxResult.Yes)
+                        {
+                            ViewModel.Initialize();
+                        }
+                    });
+                }
+            }
+        }
+        private void GameFinished()
+        {
+            if (MessageBox.Show("Игра окончена. Повторить?", "Поздравляем!", MessageBoxButton.YesNo, MessageBoxImage.Information) == 
+                MessageBoxResult.Yes)
+            {
+                ViewModel.ReInitialize();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var dialog = new UserLoginWindow();
+            dialog.ViewModel.MainViewModel = ViewModel;
+            dialog.ShowDialog();
+        }
+    }
+}
---
+++ Dev22/FifteenGame.DB2/FifteenGame.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,115 @@
+﻿using FifteenGame.Business.Services;
+using FifteenGame.Common.BusinessModels;
+using FifteenGame.Common.Definitions;
+using FifteenGame.Common.Infrastructure;
+using FifteenGame.Common.Services;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Wpf.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly IGameService _gameService = NinjectKernel.Instance.Get<IGameService>();
+
+        private GameModel _gameModel = new GameModel();
+
+        private UserModel _user;
+
+        public ObservableCollection<CellViewModel> Cells { get; set; } = new ObservableCollection<CellViewModel>();
+
+        public string UserName => _user?.Name ?? "<нет>";
+
+        public string MoveCountText => (_gameModel?.CountRevealed ?? 0).ToString();
+        private int _countFlags;
+        public int CountFlags
+        {
+            get => _countFlags;
+            set
+            {
+                _countFlags = value;
+                OnPropertyChanged(nameof(CountFlags));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Initialize();
+        }
+
+        public void SetUser(UserModel user)
+        {
+            _user = user;
+            OnPropertyChanged(nameof(UserName));
+
+            _gameModel = _gameService.GetByUserId(_user.Id);
+            FromModel(_gameModel);
+        }
+
+        public void MakeMove(int x, int y, Action gameFinishedAction)
+        {
+            _gameModel = _gameService.RevealCell(_gameModel.Id, x, y);
+            FromModel(_gameModel);
+
+            if (_gameService.IsGameOver(_gameModel.Id))
+            {
+                _gameService.RemoveGame(_gameModel.Id);
+                gameFinishedAction();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Initialize()
+        {
+            _gameModel = new GameModel { CountRevealed = 0, };
+            FromModel(_gameModel);
+        }
+
+        public void ReInitialize()
+        {
+            _gameModel = _gameService.GetByUserId(_user.Id);
+            FromModel(_gameModel);
+        }
+
+        private void FromModel(GameModel model)
+        {
+            Cells.Clear();
+            CountFlags = model.RedFlags;
+
+            for (int row = 0; row < Constants.RowCount; row++)
+            {
+                for (int column = 0; column < Constants.ColumnCount; column++)
+                {
+                    var cellModel = model[row, column];
+
+                    var cellViewModel = new CellViewModel
+                    {
+                        Row = row,
+                        Column = column,
+                        Num = cellModel.NeightborMines,
+                        IsRevealed = cellModel.IsRevealed,
+                        IsFlagged = cellModel.Isflag,
+                        IsmIne = cellModel.IsMine, 
+                    };
+
+                    Cells.Add(cellViewModel);
+                }
+            }
+
+            // Обновление текста количества ходов
+            OnPropertyChanged(nameof(MoveCountText));
+        }
+    }
+}
