2ff3caf5d958443b16dfbe2e9d4122ac679515b3
Тимкин Владимир <timkin_vn@mail.ru>
2025-03-26 14:35:54 +0700

    Пробный проект для групп 23/1, 23/2

---
+++ Samples.23.1.23.2/HelloApp/HelloApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Представьтесь, пожалуйста: ");
+            var input = Console.ReadLine();
+
+            var greetingMessage = $"Привет, {input}!";
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                greetingMessage = "Очень жаль, что вы так и не представились.";
+            }
+
+            Console.WriteLine(greetingMessage);
+
+            Calculate();
+
+            Console.ReadKey();
+        }
+
+        private static void Calculate()
+        {
+            Console.Write("Введите первое слагаемое: ");
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out var x))
+            {
+                Console.WriteLine("Внимательнее!");
+                return;
+            }
+
+            Console.Write("Введите второе слагаемое: ");
+            input = Console.ReadLine();
+            var y = int.Parse(input);
+
+            var sum = x + y;
+
+            Console.WriteLine($"Сумма равна {sum}");
+        }
+    }
+}
