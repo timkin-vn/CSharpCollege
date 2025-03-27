a983b045043643133ae040f384bf0f0d1ae57f48
Тимкин Владимир <timkin_vn@mail.ru>
2025-03-27 23:24:04 +0700

    Пробный проект для групп 24/1, 23/3

---
+++ Samples.24.1.23.3/Hello/Hello/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Представьтесь, пожалуйста: ");
+            var name = Console.ReadLine();
+
+            var greeting = $@"Привет, ""{name}""!";
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                greeting = @"Здравствуйте, ""незнакомец""!";
+            }
+
+            Console.WriteLine(greeting);
+
+            Calculate();
+
+            Console.ReadKey();
+        }
+
+        private static void Calculate()
+        {
+            Console.Write("Введите первое слагаемое: ");
+            var input = Console.ReadLine();
+            if (!int.TryParse(input, out var x))
+            {
+                Console.WriteLine("Внимательнее!");
+                return;
+            }
+
+            Console.Write("Введите второе слагаемое: ");
+            input = Console.ReadLine();
+            var y = int.Parse(input);
+
+            var sum = x + y;
+
+            Console.WriteLine($"Сумма = {sum}");
+        }
+    }
+}
