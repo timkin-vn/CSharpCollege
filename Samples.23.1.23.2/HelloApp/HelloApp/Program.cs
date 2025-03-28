18816f63b3d1d7d9d6de7b6fd33fd6b368de652c
Тимкин Владимир <timkin_vn@mail.ru>
2025-03-28 19:20:43 +0700

    Обновление проекта

--- Samples.23.1.23.2/HelloApp/HelloApp/Program.cs
+++ Samples.23.1.23.2/HelloApp/HelloApp/Program.cs
@@ -10,6 +10,19 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
+            //Greeting();
+
+            //Calculate();
+
+            //Arrays();
+
+            Arrays2D();
+
+            Console.ReadKey();
+        }
+
+        private static void Greeting()
+        {
             Console.Write("Представьтесь, пожалуйста: ");
             var input = Console.ReadLine();
 
@@ -20,10 +33,6 @@ namespace HelloApp
             }
 
             Console.WriteLine(greetingMessage);
-
-            Calculate();
-
-            Console.ReadKey();
         }
 
         private static void Calculate()
@@ -44,6 +53,71 @@ namespace HelloApp
             var sum = x + y;
 
             Console.WriteLine($"Сумма равна {sum}");
+        }
+
+        private static void Arrays()
+        {
+            var newArray = new[] { 1, 2, 3, 4, 5 };
+
+            //for (int i = 0; i < newArray.Length; i++)
+            //{
+            //    Console.Write($"{newArray[i]}, ");
+            //}
+
+            //Console.WriteLine();
+
+            foreach (var value in newArray)
+            {
+                Console.Write($"{value}, ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(string.Join(", ", newArray));
+
+            newArray = new int[4];
+
+            Console.WriteLine(string.Join(", ", newArray));
+        }
+
+        private static void Arrays2D()
+        {
+            int[,] array2d = new int[,]
+            {
+                { 1, 2, 3, 4, },
+                { 5, 6, 7, 8, },
+                { 9, 10, 11, 12, },
+                { 13, 14, 15, 16, },
+            };
+
+            for (int row = 0; row < array2d.GetLength(0); row++)
+            {
+                for (int col = 0; col < array2d.GetLength(1); col++)
+                {
+                    Console.Write($"{array2d[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            int[][] arrayOfArrays = new int[][]
+            {
+                new[] { 1, 2, 3, },
+                new[] { 4, 5, 6, 7, },
+                new[] { 8, 9, 10, 11, 12, },
+            };
+
+            for (int row = 0; row < arrayOfArrays.Length; row++)
+            {
+                for (int col = 0; col < arrayOfArrays[row].Length; col++)
+                {
+                    Console.Write($"{arrayOfArrays[row][col]} ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
--- Samples.24.1.23.3/Hello/Hello/Program.cs
+++ Samples.24.1.23.3/Hello/Hello/Program.cs
@@ -11,6 +11,19 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            //Greeting();
+
+            //Calculate();
+
+            //Arrays();
+
+            Arrays2d();
+
+            Console.ReadKey();
+        }
+
+        private static void Greeting()
+        {
             Console.Write("Представьтесь, пожалуйста: ");
             var name = Console.ReadLine();
 
@@ -22,10 +35,6 @@ namespace Hello
             }
 
             Console.WriteLine(greeting);
-
-            Calculate();
-
-            Console.ReadKey();
         }
 
         private static void Calculate()
@@ -45,6 +54,78 @@ namespace Hello
             var sum = x + y;
 
             Console.WriteLine($"Сумма = {sum}");
+        }
+
+        private static void Arrays()
+        {
+            var array = new int[] { 1, 2, 3, 4, };
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"{array[i]} ");
+            //}
+
+            //foreach (var item in array)
+            //{
+            //    Console.Write($"{item}, ");
+            //}
+
+            Console.Write(string.Join(", ", array));
+
+            Console.WriteLine();
+        }
+
+        private static void Arrays2d()
+        {
+            var array2d = new[,]
+            {
+                { 1, 2, 3, 4, },
+                { 5, 6, 7, 8, },
+                { 9, 10, 11, 12, },
+            };
+
+            for (int row = 0; row < array2d.GetLength(0); row++)
+            {
+                for (int column = 0; column < array2d.GetLength(1); column++)
+                {
+                    Console.Write($"{array2d[row, column]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            var arrayOfArray = new[]
+            {
+                new[] { 1, 2, 3, },
+                new[] { 4, 5, 6, 7, },
+                new[] { 8, 9, 10, 11, 12, },
+            };
+
+            for (int row = 0; row < arrayOfArray.Length; row++)
+            {
+                for (int column = 0; column < arrayOfArray[row].Length; column++)
+                {
+                    Console.Write($"{arrayOfArray[row][column]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            foreach (var subArray in arrayOfArray)
+            {
+                foreach (var item in subArray)
+                {
+                    Console.Write($"{item}, ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
         }
     }
 }
