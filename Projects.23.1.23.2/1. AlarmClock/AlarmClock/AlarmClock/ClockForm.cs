6f978a4910538776491f9e444ade8199153845f5
Тимкин Владимир <timkin_vn@mail.ru>
2025-04-02 15:57:45 +0700

    Проект Графика для групп 23/1, 23/2. Начало разработки

---
+++ Projects.23.1.23.2/1. AlarmClock/AlarmClock/AlarmClock/ClockForm.cs
@@ -0,0 +1,37 @@
+﻿using AlarmClock.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClock
+{
+    public partial class ClockForm : Form
+    {
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            var aboutForm = new AboutForm();
+            aboutForm.ShowDialog();
+        }
+    }
+}
