8f479466343cd8276faa514770b3b090df6d49f0
Тимкин Владимир <timkin_vn@mail.ru>
2025-04-05 14:21:51 +0700

    Проект Будильник для групп 24/1, 23/3. Завершение

--- Projects.24.1.23.3/1. AlarmClock/AlarmClock/AlarmClock/Models/AlarmSettings.cs
+++ Projects.24.1.23.3/1. AlarmClock/AlarmClock/AlarmClock/Models/AlarmSettings.cs
@@ -8,12 +8,29 @@ namespace AlarmClock.Models
 {
     internal class AlarmSettings
     {
+        /// <summary>
+        /// Время срабатывания
+        /// </summary>
         public TimeSpan AlarmTime { get; set; }
 
+        /// <summary>
+        /// Сообщение при срабатывании
+        /// </summary>
         public string AlarmMessage { get; set; }
 
+        /// <summary>
+        /// Включен ли режим будильника
+        /// </summary>
         public bool IsAlarmActive { get; set; }
 
+        /// <summary>
+        /// Включен ли звуковой сигнал при срабатывании
+        /// </summary>
         public bool IsSoundActive { get; set; }
+
+        /// <summary>
+        /// Сработал ли будильник
+        /// </summary>
+        public bool IsAwakeActivated { get; set; }
     }
 }
