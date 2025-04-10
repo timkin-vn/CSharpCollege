a18b0315c4cf174c37cfbac5cf3c6176426c822e
enot.al.enotov@gmail.com <enot.al.enotov@gmail.com>
2025-04-10 18:09:41 +0300

    Будильник

---
+++ Roman/AlarmClock/ClockForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace AlarmClock
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.DisplayLabel = new System.Windows.Forms.Label();
+            this.SettingsButton = new System.Windows.Forms.Button();
+            this.AboutButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ClockTimer = new System.Windows.Forms.Timer(this.components);
+            this.TimerAlarm = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DisplayLabel
+            // 
+            this.DisplayLabel.BackColor = System.Drawing.Color.Black;
+            this.DisplayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DisplayLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.DisplayLabel.Location = new System.Drawing.Point(99, 30);
+            this.DisplayLabel.Name = "DisplayLabel";
+            this.DisplayLabel.Size = new System.Drawing.Size(356, 97);
+            this.DisplayLabel.TabIndex = 0;
+            this.DisplayLabel.Text = "00:00:00";
+            this.DisplayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SettingsButton
+            // 
+            this.SettingsButton.BackColor = System.Drawing.Color.Black;
+            this.SettingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SettingsButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.SettingsButton.Location = new System.Drawing.Point(17, 170);
+            this.SettingsButton.Name = "SettingsButton";
+            this.SettingsButton.Size = new System.Drawing.Size(100, 23);
+            this.SettingsButton.TabIndex = 1;
+            this.SettingsButton.Text = "Настройки...";
+            this.SettingsButton.UseVisualStyleBackColor = false;
+            this.SettingsButton.Click += new System.EventHandler(this.SettingsButton_Click);
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.BackColor = System.Drawing.Color.Black;
+            this.AboutButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AboutButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.AboutButton.Location = new System.Drawing.Point(335, 170);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(100, 23);
+            this.AboutButton.TabIndex = 2;
+            this.AboutButton.Text = "О программе...";
+            this.AboutButton.UseVisualStyleBackColor = false;
+            this.AboutButton.Click += new System.EventHandler(this.AboutButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Black;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ExitButton.Location = new System.Drawing.Point(441, 170);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(100, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ClockTimer
+            // 
+            this.ClockTimer.Enabled = true;
+            this.ClockTimer.Interval = 1000;
+            this.ClockTimer.Tick += new System.EventHandler(this.ClockTimer_Tick);
+            // 
+            // TimerAlarm
+            // 
+            this.TimerAlarm.BackColor = System.Drawing.Color.Black;
+            this.TimerAlarm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TimerAlarm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.TimerAlarm.Location = new System.Drawing.Point(123, 170);
+            this.TimerAlarm.Name = "TimerAlarm";
+            this.TimerAlarm.Size = new System.Drawing.Size(100, 23);
+            this.TimerAlarm.TabIndex = 4;
+            this.TimerAlarm.Text = "Секундомер";
+            this.TimerAlarm.UseVisualStyleBackColor = false;
+            this.TimerAlarm.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.BackColor = System.Drawing.Color.Black;
+            this.Timer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Timer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Timer.Location = new System.Drawing.Point(229, 170);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(100, 23);
+            this.Timer.TabIndex = 5;
+            this.Timer.Text = "Таймер";
+            this.Timer.UseVisualStyleBackColor = false;
+            this.Timer.Click += new System.EventHandler(this.Timer_Click);
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(553, 205);
+            this.Controls.Add(this.Timer);
+            this.Controls.Add(this.TimerAlarm);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.AboutButton);
+            this.Controls.Add(this.SettingsButton);
+            this.Controls.Add(this.DisplayLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ClockForm";
+            this.Text = "Будильник";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DisplayLabel;
+        private System.Windows.Forms.Button SettingsButton;
+        private System.Windows.Forms.Button AboutButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Timer ClockTimer;
+        private System.Windows.Forms.Button TimerAlarm;
+        private System.Windows.Forms.Button Timer;
+    }
+}
+
---
+++ Roman/AlarmClock/Forms/Timer_test.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AlarmClock.Forms
+{
+    partial class Timer_test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Timer_m = new System.Windows.Forms.Label();
+            this.startTime = new System.Windows.Forms.Button();
+            this.Timer2 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.Minutes = new System.Windows.Forms.Label();
+            this.seconds = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Timer_m
+            // 
+            this.Timer_m.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Timer_m.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Timer_m.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Timer_m.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Timer_m.Location = new System.Drawing.Point(127, 9);
+            this.Timer_m.Name = "Timer_m";
+            this.Timer_m.Size = new System.Drawing.Size(194, 63);
+            this.Timer_m.TabIndex = 0;
+            this.Timer_m.Text = "00:00:00";
+            this.Timer_m.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Timer_m.Click += new System.EventHandler(this.Timer_m_Click);
+            // 
+            // startTime
+            // 
+            this.startTime.BackColor = System.Drawing.Color.Black;
+            this.startTime.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.startTime.Location = new System.Drawing.Point(180, 180);
+            this.startTime.Name = "startTime";
+            this.startTime.Size = new System.Drawing.Size(75, 23);
+            this.startTime.TabIndex = 1;
+            this.startTime.Text = "Пуск";
+            this.startTime.UseVisualStyleBackColor = false;
+            this.startTime.Click += new System.EventHandler(this.startTime_Click);
+            // 
+            // Timer2
+            // 
+            this.Timer2.Interval = 1000;
+            this.Timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(138, 126);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(254, 126);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // Minutes
+            // 
+            this.Minutes.AutoSize = true;
+            this.Minutes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Minutes.Location = new System.Drawing.Point(86, 130);
+            this.Minutes.Name = "Minutes";
+            this.Minutes.Size = new System.Drawing.Size(46, 13);
+            this.Minutes.TabIndex = 4;
+            this.Minutes.Text = "Минуты";
+            // 
+            // seconds
+            // 
+            this.seconds.AutoSize = true;
+            this.seconds.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.seconds.Location = new System.Drawing.Point(202, 130);
+            this.seconds.Name = "seconds";
+            this.seconds.Size = new System.Drawing.Size(43, 13);
+            this.seconds.TabIndex = 5;
+            this.seconds.Text = "Секунд";
+            // 
+            // Timer_test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(448, 215);
+            this.Controls.Add(this.seconds);
+            this.Controls.Add(this.Minutes);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.startTime);
+            this.Controls.Add(this.Timer_m);
+            this.Name = "Timer_test";
+            this.Text = "Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Timer_m;
+        private System.Windows.Forms.Button startTime;
+        private System.Windows.Forms.Timer Timer2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label Minutes;
+        private System.Windows.Forms.Label seconds;
+    }
+}
