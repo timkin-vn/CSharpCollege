9457234330e9438452a8b79ddb0e3a436646733a
Тимкин Владимир <timkin_vn@mail.ru>
2025-04-15 14:31:06 +0700

    Проект Графика

---
+++ Projects.23.1.23.2/2. Drawing/Drawing/Drawing/Services/Painter.cs
@@ -0,0 +1,42 @@
+﻿using Drawing.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing.Services
+{
+    internal class Painter
+    {
+        public Scaler Scaler { get; set; }
+
+        public void Initialize(Rectangle bounds)
+        {
+            Scaler = new Scaler();
+            Scaler.TargetBounds = bounds;
+            Scaler.SourceBounds = new RectangleModel
+            {
+                X = -2,
+                Y = -1,
+                Width = 5,
+                Height = 5,
+            };
+        }
+
+        public void Paint(Graphics g)
+        {
+            var pen = new Pen(Color.DarkCyan, 3);
+            var brush = Brushes.LightGoldenrodYellow;
+
+            var rect = Scaler.Scale(new RectangleModel { X = 0, Y = 2, Width = 1, Height = 1 });
+            g.FillEllipse(brush, rect);
+            g.DrawEllipse(pen, rect);
+
+            rect = Scaler.Scale(new RectangleModel { X = 0, Y = 1, Width = 1, Height = 1 });
+            g.FillRectangle(brush, rect);
+            g.DrawRectangle(pen, rect);
+        }
+    }
+}
---
+++ Projects.24.1.23.3/2. Drawing/Drawing/Drawing/Services/Painter.cs
@@ -0,0 +1,41 @@
+﻿using Drawing.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing.Services
+{
+    internal class Painter
+    {
+        private Scaler _scaler = new Scaler();
+
+        public void Initialize(Rectangle targetRectangle)
+        {
+            _scaler.Initialize(targetRectangle, new RectangleModel { X = -2, Width = 5, Y = -1, Height = 5, });
+        }
+
+        public void Paint(Graphics g)
+        {
+            var pen = new Pen(Color.DarkGreen, 3);
+            var brush = Brushes.LightGoldenrodYellow;
+
+            var headRect = _scaler.Scale(new RectangleModel { X = 0, Y = 2, Width = 1, Height = 1, });
+
+            g.FillEllipse(brush, headRect);
+            g.DrawEllipse(pen, headRect);
+
+            var bodyRect = _scaler.Scale(new RectangleModel { X = 0, Y = 1, Width = 1, Height = 1, });
+
+            g.FillRectangle(brush, bodyRect);
+            g.DrawRectangle(pen, bodyRect);
+
+            var point1 = _scaler.Scale(new PointModel { X = 0, Y = 2, });
+            var point2 = _scaler.Scale(new PointModel { X = -1, Y = 1, });
+
+            g.DrawLine(pen, point1, point2);
+        }
+    }
+}
