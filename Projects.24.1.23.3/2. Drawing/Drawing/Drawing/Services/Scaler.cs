957b46fc8375406433518d0460b1b4c0788dd3bb
Тимкин Владимир <timkin_vn@mail.ru>
2025-04-18 20:16:24 +0700

    Проект Графика для групп 24/1, 23/3. Окончание

--- Projects.24.1.23.3/2. Drawing/Drawing/Drawing/Services/Scaler.cs
+++ Projects.24.1.23.3/2. Drawing/Drawing/Drawing/Services/Scaler.cs
@@ -18,6 +18,7 @@ namespace Drawing.Services
         {
             TargetRectangle = targetRectangle;
             SourceRectangle = sourceRectangle;
+            Rescale();
         }
 
         public int ScaleX(double x)
@@ -58,6 +59,24 @@ namespace Drawing.Services
                 Width = ScaleWidth(rect.Width),
                 Height = ScaleHeigth(rect.Height),
             };
+        }
+
+        private void Rescale()
+        {
+            if (TargetRectangle.Width * SourceRectangle.Height > SourceRectangle.Width * TargetRectangle.Height)
+            {
+                // Добавки слева и справа
+                var newWidth = TargetRectangle.Width * SourceRectangle.Height / TargetRectangle.Height;
+                SourceRectangle.X -= (newWidth - SourceRectangle.Width) / 2;
+                SourceRectangle.Width = newWidth;
+            }
+            else
+            {
+                // Добавки сверху и снизу
+                var newHeight = TargetRectangle.Height * SourceRectangle.Width / TargetRectangle.Width;
+                SourceRectangle.Y -= (newHeight - SourceRectangle.Height) / 2;
+                SourceRectangle.Height = newHeight;
+            }
         }
     }
 }
