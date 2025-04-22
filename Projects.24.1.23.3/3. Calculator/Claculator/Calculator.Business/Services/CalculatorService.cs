1c7d6d0127673aacc060a00742d17f8cbe2e57f2
Тимкин Владимир <timkin_vn@mail.ru>
2025-04-22 14:34:52 +0700

    Проект Калькулятор для групп 24/1, 23/3. Начало

---
+++ Projects.24.1.23.3/3. Calculator/Claculator/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,65 @@
+﻿using Calculator.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void PressDigit(CalculatorState state, string digitText)
+        {
+            if (!byte.TryParse(digitText, out var digit))
+            {
+                return;
+            }
+
+            state.RegisterX = state.RegisterX * 10 + digit;
+        }
+
+        public void Clear(CalculatorState state)
+        {
+            state.RegisterX = 0;
+        }
+
+        public void MoveXToY(CalculatorState state)
+        {
+            state.RegisterY = state.RegisterX;
+        }
+
+        public void CompleteOperation(CalculatorState state, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    state.RegisterX = state.RegisterY + state.RegisterX;
+                    break;
+
+                case "-":
+                    state.RegisterX = state.RegisterY - state.RegisterX;
+                    break;
+
+                case "*":
+                    state.RegisterX = state.RegisterY * state.RegisterX;
+                    break;
+
+                case "/":
+                    state.RegisterX = state.RegisterY / state.RegisterX;
+                    break;
+            }
+        }
+
+        public void PressOperation(CalculatorState state, string operation)
+        {
+            MoveXToY(state);
+            state.Operation = operation;
+        }
+
+        public void PressEqual(CalculatorState state)
+        {
+            CompleteOperation(state, state.Operation);
+        }
+    }
+}
