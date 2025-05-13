0bf5f7a4ecbd31d1fc13e2b98f7e0b947359a765
𝕄𝕣.𝔽𝕣𝕖𝕖𝕝𝕒𝕟𝕔𝕖𝕣 <vladimirilyushichev1307@gmail.com>
2025-05-14 00:15:31 +0300

    Нарисовал розу

--- Projects.23.1.23.2/2. Drawing/Drawing/Drawing/Services/PictureBuilder.cs
+++ Projects.23.1.23.2/2. Drawing/Drawing/Drawing/Services/PictureBuilder.cs
@@ -20,7 +20,7 @@ namespace Drawing.Services
 
         public void DrawPicture(Painter painter)
         {
-            var mainPen = new Pen(Color.Black, 3);
+            /*var mainPen = new Pen(Color.Black, 3);
             var wheelBrush = Brushes.LightGray;
 
             var rect = new RectangleModel { X = -1, Y = 0, Width = 2, Height = 2 };
@@ -70,7 +70,103 @@ namespace Drawing.Services
                 new PointModel { X = 9, Y = 6, },
             };
 
-            painter.DrawPolygon(bodyBrush, mainPen, pipePoints);
+            painter.DrawPolygon(bodyBrush, mainPen, pipePoints);*/
+
+            var mainPen = new Pen(Color.Black, 2);
+            var RoseLeaf = Brushes.LightPink;
+
+            var LeafPoints1 = new[]
+            {
+                new PointModel { X = 4.5, Y = 10, },
+                new PointModel { X = 5.3, Y = 9.3, },
+                new PointModel { X = 7, Y = 8.5, },
+                new PointModel { X = 5.6, Y = 7.5, },
+                new PointModel { X = 5.4, Y = 8.4, },
+                new PointModel { X = 5.2, Y = 8.6, },
+                new PointModel { X = 5.2, Y = 8.8, },
+                new PointModel { X = 5.1, Y = 9, },
+                new PointModel { X = 4.7, Y = 9, },
+                new PointModel { X = 4.2, Y = 8.75, },
+            };
+            painter.DrawPolygon(RoseLeaf, mainPen, LeafPoints1);
+
+            var LeafPoints2 = new[]
+            {
+                new PointModel { X = 4.45, Y = 10, },
+                new PointModel { X = 4.2, Y = 8.75, },
+                new PointModel { X = 3.2, Y = 7.75, },
+                new PointModel { X = 3.2, Y = 7.25, },
+                new PointModel { X = 2.7, Y = 7.75, },
+                new PointModel { X = 2.67, Y = 8.5, },
+                
+            };
+            painter.DrawPolygon(RoseLeaf, mainPen, LeafPoints2);
+
+            var LeafPoints3 = new[]
+            {
+                new PointModel { X = 2.67, Y = 8.5, },
+                new PointModel { X = 2.7, Y = 7.75, },
+                new PointModel { X = 2.2, Y = 7.25, },
+
+            };
+            painter.DrawPolygon(RoseLeaf, mainPen, LeafPoints3);
+
+            var LeafPoints4 = new[]
+            {
+                new PointModel { X = 2.7, Y = 7.75, },
+                new PointModel { X = 3.4, Y = 6.2, },
+                new PointModel { X = 3.9, Y = 5.9, },
+                new PointModel { X = 3.2, Y = 7.25, },
+            };
+            painter.DrawPolygon(RoseLeaf, mainPen, LeafPoints4);
+
+            var LeafPoints5 = new[]
+            {
+                new PointModel { X = 2.7, Y = 7.75, },
+                new PointModel { X = 2.2, Y = 7.2, },
+                new PointModel { X = 3.4, Y = 6.2, },
+
+            };
+            painter.DrawPolygon(RoseLeaf, mainPen, LeafPoints5);
+
+            var LeafPoints6 = new[]
+            {
+                new PointModel { X = 2.2, Y = 7.25, },
+                new PointModel { X = 1.45, Y = 7.75, },
+                new PointModel { X = 2.2, Y = 8.9, },
+                new PointModel { X = 2.8, Y = 8.7, },
+
+            };
+            painter.DrawPolygon(RoseLeaf, mainPen, LeafPoints6);
+
+            var LeafPoints7 = new[]
+            {
+                new PointModel { X = 3.9, Y = 5.9, },
+                new PointModel { X = 5.6, Y = 7.5, },
+                new PointModel { X = 5.2, Y = 8.6, },
+                new PointModel { X = 5.2, Y = 8.8, },
+                new PointModel { X = 5.1, Y = 9, },
+                new PointModel { X = 4.7, Y = 9, },
+                new PointModel { X = 4.2, Y = 8.75, },
+                new PointModel { X = 3.2, Y = 7.75, },
+                new PointModel { X = 3.2, Y = 7.25, },
+            };
+            painter.DrawPolygon(RoseLeaf, mainPen, LeafPoints7);
+
+            var LeafPoints8 = new[]
+            {
+                new PointModel { X = 3.9, Y = 5.9, },
+                new PointModel { X = 6, Y = 5.9, },
+                new PointModel { X = 6, Y = 8.3, },
+                
+            };
+            painter.DrawPolygon(RoseLeaf, mainPen, LeafPoints8);
+
+            var RoseStem = Brushes.DarkGreen;
+            var stem = new RectangleModel { X = 3.9, Y = 2, Width = 0.75, Height = 3.9, };
+            painter.DrawRectangle(RoseStem, mainPen, stem);
+
+
         }
     }
 }
