52fc64ccf0d2aa87f31a2d605b6683e019192de4
DIO <denis.kuzmin94@bk.ru>
2025-05-16 19:28:01 +0300

    Создал Калькулятор WPF

---
+++ KuzminDenis/CalculatorWPF/CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using CalculatorWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, RoutedEventArgs e)
+        {
+            var digit = (string)((Button)sender).Content;
+            ViewModel.PressDigit(digit);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressClear();
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var operationCode = (string)((Button)sender).Content;
+            ViewModel.PressOperation(operationCode);
+        }
+
+        private void DecimalButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.AddDecimal();
+        }
+
+        private void SquareButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressSquare();
+        }
+
+        private void SquareRootButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressSquareRoot();
+        }
+
+        private void LogarithmButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressLogarithm();
+        }
+    }
+}
---
+++ KuzminDenis/CalculatorWPF/CalculatorWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorWPF.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private CalculatorState _state = new CalculatorState();
+        private CalculatorService _service = new CalculatorService();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string DisplayValue
+        {
+            get
+            {
+                if (_state.HasDecimalPoint)
+                {
+                    return _state.RegisterX.ToString("0." + new string('0', _state.DecimalPlaces));
+                }
+                return _state.RegisterX.ToString();
+            }
+        }
+
+        public void PressDigit(string digit)
+        {
+            _service.PressDigit(_state, digit);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressClear()
+        {
+            _service.Clear(_state);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressOperation(string operationCode)
+        {
+            _service.PressOperation(_state, operationCode);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void AddDecimal()
+        {
+            _service.AddDecimal(_state);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressSquare()
+        {
+            _service.PressSquare(_state);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressSquareRoot()
+        {
+            _service.PressSquareRoot(_state);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressLogarithm()
+        {
+            _service.PressLogarithm(_state);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
