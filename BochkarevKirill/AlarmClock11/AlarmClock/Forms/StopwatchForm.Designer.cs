245772648a852492634da65a9a01d5387e8cf846
KOMPIC\bocka <bockarevkirill36@gmail.com>
2025-06-04 09:04:49 +0300

    будильник

---
+++ BochkarevKirill/AlarmClock11/AlarmClock/Forms/StopwatchForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AlarmClock.Forms
+{
+    partial class StopwatchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.BackColor = System.Drawing.Color.DimGray;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTime.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.lblTime.Location = new System.Drawing.Point(12, 9);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(320, 83);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "00:00:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(25, 107);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStartStop.TabIndex = 2;
+            this.btnStartStop.Text = "Старт";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(225, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сброс";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // StopwatchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 142);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "StopwatchForm";
+            this.Text = "Секундомер";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
---
+++ BochkarevKirill/AlarmClock11/AlarmClock/Forms/StopwatchForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClock.Forms
+{
+    public partial class StopwatchForm : Form
+    {
+        private TimeSpan elapsedTime;
+        private bool isRunning = false;
+
+        public StopwatchForm()
+        {
+            InitializeComponent();
+            elapsedTime = TimeSpan.Zero;
+            UpdateLabel();
+        }
+
+        private void UpdateLabel()
+        {
+            lblTime.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsedTime = elapsedTime.Add(TimeSpan.FromSeconds(1));
+            UpdateLabel();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            isRunning = !isRunning;
+            Timer.Enabled = isRunning;
+
+            btnStartStop.Text = isRunning ? "Стоп" : "Старт";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Timer.Enabled = false;
+            isRunning = false;
+            elapsedTime = TimeSpan.Zero;
+            UpdateLabel();
+            btnStartStop.Text = "Старт";
+        }
+    }
+}
