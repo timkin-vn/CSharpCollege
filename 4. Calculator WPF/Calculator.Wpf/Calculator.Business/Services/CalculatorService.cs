82575c51883be14a5127679948c5c1b60deeb5ea
Solus831 <1marnn07@mail.ru>
2025-06-04 09:28:37 +0300

    калькулятор wpf

---
+++ 4. Calculator WPF/Calculator.Wpf/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,88 @@
+﻿using Calculator.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void PressDigit(CalculatorState state, string digitString)
+        {
+            if (!int.TryParse(digitString, out var digit))
+            {
+                return;
+            }
+
+            if (state.IsClearNeeded)
+            {
+                state.RegisterX = 0;
+            }
+
+            if (state.IsFloat)
+            {
+                state.RegisterX = state.RegisterX + digit / state.bonus;
+                state.bonus *= 10.0;
+                state.IsClearNeeded = false;
+            }
+
+            else
+            {
+                state.RegisterX = state.RegisterX * 10 + digit;
+                state.IsClearNeeded = false;
+            }
+        }
+
+        public void Clear(CalculatorState state)
+        {
+            state.RegisterX = 0;
+            state.RegisterY = 0;
+            state.IsFloat = false;
+        }
+
+        public void MoveXToY(CalculatorState state)
+        {
+            state.RegisterY = state.RegisterX;
+        }
+
+        public void CompleteOperation(CalculatorState state, string operationCode)
+        {
+            switch (operationCode)
+            {
+                case "+":
+                    state.RegisterX = state.RegisterY + state.RegisterX;
+                    break;
+
+                case "-":
+                    state.RegisterX = state.RegisterY - state.RegisterX;
+                    break;
+
+                case "*":
+                    state.RegisterX = state.RegisterY * state.RegisterX;
+                    break;
+
+                case "/":
+                    state.RegisterX = state.RegisterY / state.RegisterX;
+                    break;
+            }
+        }
+
+        public void PressOperation(CalculatorState state, string operationCode)
+        {
+            CompleteOperation(state, state.OperationCode);
+            MoveXToY(state);
+            state.bonus = 10.0;
+            state.OperationCode = operationCode;
+            state.IsClearNeeded = true;
+        }
+
+        public void PressEqual(CalculatorState state)
+        {
+            CompleteOperation(state, state.OperationCode);
+            state.IsClearNeeded = true;
+        }
+    }
+}
---
+++ 4. Calculator WPF/Calculator.Wpf/Calculator.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Wpf.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private CalculatorState _state = new CalculatorState();
+
+        private CalculatorService _service = new CalculatorService();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string DisplayValue => _state.RegisterX.ToString();
+
+        public void PressDigit(string digit)
+        {
+            _service.PressDigit(_state, digit);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressClear()
+        {
+            _service.Clear(_state);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressOperation(string operationCode)
+        {
+            _state.bonus = 10.0;
+            _state.IsFloat = false;
+            _service.PressOperation(_state, operationCode);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressProcent()
+        {
+            _state.RegisterY = _state.RegisterY > 0 ? _state.RegisterY : 1;
+            _state.RegisterX = _state.RegisterY / 100 * _state.RegisterX;
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+        public void PressFloat()
+        {
+            _state.IsFloat = true;
+            _state.bonus = 10.0;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
