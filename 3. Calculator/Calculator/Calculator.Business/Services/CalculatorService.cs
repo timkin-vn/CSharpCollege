14f159f703d0b42c5ebbb467b730a316a60365cb
Artem <naswai.hhh@yandex.ru>
2025-06-04 09:17:30 +0300

    Проект №3 закончен

---
+++ 3. Calculator/Calculator/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,124 @@
+﻿using Calculator.Business.Models;
+using System;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void PressDigit(CalculatorState state, string digitString)
+        {
+            if (!int.TryParse(digitString, out var digit))
+            {
+                return;
+            }
+
+            if (state.IsClearNeeded)
+            {
+                state.RegisterX = 0;
+            }
+
+            if (state.IsFloat)
+            {
+                state.RegisterX = state.RegisterX + digit / state.bonus;
+                state.bonus *= 10.0;
+                state.IsClearNeeded = false;
+            }
+            else
+            {
+                state.RegisterX = state.RegisterX * 10 + digit;
+                state.IsClearNeeded = false;
+            }
+        }
+
+        public void Clear(CalculatorState state)
+        {
+            state.RegisterX = 0;
+            state.RegisterY = 0;
+            state.IsFloat = false;
+        }
+
+        public void MoveXToY(CalculatorState state)
+        {
+            state.RegisterY = state.RegisterX;
+        }
+
+        public void CompleteOperation(CalculatorState state, string operationCode)
+        {
+            switch (operationCode)
+            {
+                case "+":
+                    state.RegisterX = state.RegisterY + state.RegisterX;
+                    break;
+                case "-":
+                    state.RegisterX = state.RegisterY - state.RegisterX;
+                    break;
+                case "*":
+                    state.RegisterX = state.RegisterY * state.RegisterX;
+                    break;
+                case "/":
+                    state.RegisterX = state.RegisterY / state.RegisterX;
+                    break;
+            }
+        }
+
+        public void PressOperation(CalculatorState state, string operationCode)
+        {
+            CompleteOperation(state, state.OperationCode);
+            MoveXToY(state);
+            state.bonus = 10.0;
+            state.OperationCode = operationCode;
+            state.IsClearNeeded = true;
+        }
+
+        public void PressEqual(CalculatorState state)
+        {
+            CompleteOperation(state, state.OperationCode);
+            state.IsClearNeeded = true;
+        }
+
+        public void Backspace(CalculatorState state)
+        {
+            string str = state.RegisterX.ToString();
+
+            if (str.Length > 1)
+            {
+                str = str.Substring(0, str.Length - 1);
+
+                if (str == "." || str == "-")
+                {
+                    str = "0";
+                    state.IsFloat = false;
+                }
+
+                if (double.TryParse(str, out var result))
+                {
+                    state.RegisterX = result;
+                }
+                else
+                {
+                    state.RegisterX = 0;
+                }
+            }
+            else
+            {
+                state.RegisterX = 0;
+                state.IsFloat = false;
+            }
+
+            state.IsClearNeeded = false;
+        }
+
+        public void SquareRoot(CalculatorState state)
+        {
+            if (state.RegisterX >= 0)
+            {
+                state.RegisterX = Math.Sqrt(state.RegisterX);
+                state.IsClearNeeded = true;
+            }
+            else
+            {
+                state.RegisterX = 0;
+            }
+        }
+    }
+}
---
+++ 3. Calculator/Calculator/Calculator/CalculatorForm.cs
@@ -0,0 +1,80 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private readonly CalculatorState _state = new CalculatorState();
+        private readonly CalculatorService _service = new CalculatorService();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, EventArgs e)
+        {
+            var digit = ((Button)sender).Text;
+            _service.PressDigit(_state, digit);
+            ShowResult();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            _service.Clear(_state);
+            ShowResult();
+        }
+
+        private void ShowResult()
+        {
+            DisplayLabel.Text = _state.RegisterX.ToString();
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            _state.bonus = 10.0;
+            _state.IsFloat = false;
+            var operationCode = ((Button)sender).Text;
+            _service.PressOperation(_state, operationCode);
+            ShowResult();
+        }
+
+        private void EqualButton_Click(object sender, EventArgs e)
+        {
+            _service.PressEqual(_state);
+            ShowResult();
+        }
+
+        private void FloatButton_Click(object sender, EventArgs e)
+        {
+            _state.IsFloat = true;
+            _state.bonus = 10.0;
+        }
+
+        private void PercentButton_Click(object sender, EventArgs e)
+        {
+            _state.RegisterY = _state.RegisterY > 0 ? _state.RegisterY : 1;
+            _state.RegisterX = _state.RegisterY / 100 * _state.RegisterX;
+            ShowResult();
+        }
+
+        private void BackspaceButton_Click(object sender, EventArgs e)
+        {
+            _service.Backspace(_state);
+            ShowResult();
+        }
+
+        private void SquareRootButton_Click(object sender, EventArgs e)
+        {
+            _service.SquareRoot(_state);
+            ShowResult();
+        }
+
+        private void CalculatorForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
