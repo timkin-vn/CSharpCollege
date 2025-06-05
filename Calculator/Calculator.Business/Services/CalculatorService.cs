1f56a0e5b48b8e7ee12bedd5ca870e810ae1a6d4
EgorPrytkov <qw3rbo@yandex.ru>
2025-06-05 16:38:18 +0300

    калькулятор

---
+++ Calculator/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,97 @@
+﻿using Calculator.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void PressDigit(CalculatorState state, string digitString)
+        {
+            if (digitString == ".")
+            {
+                if (!state.HasDecimalPoint)
+                {
+                    state.HasDecimalPoint = true;
+                }
+                return;
+            }
+
+            int digit = int.Parse(digitString);
+
+            if (state.IsClearNeeded)
+            {
+                state.RegisterX = 0;
+                state.HasDecimalPoint = false;
+                state.DecimalPlaces = 0;
+                state.IsClearNeeded = false;
+            }
+
+            if (state.HasDecimalPoint)
+            {
+                state.DecimalPlaces++;
+                state.RegisterX += digit / Math.Pow(10, state.DecimalPlaces);
+            }
+            else
+            {
+                state.RegisterX = state.RegisterX * 10 + digit;
+            }
+        }
+
+        public void Clear(CalculatorState state)
+        {
+            state.RegisterX = 0;
+        }
+
+        public void MoveXToY(CalculatorState state)
+        {
+            state.RegisterY = state.RegisterX;
+        }
+
+        public void CompleteOperation(CalculatorState state, string operationCode)
+        {
+            switch (operationCode)
+            {
+                case "+":
+                    state.RegisterX = state.RegisterY + state.RegisterX;
+                    break;
+
+                case "-":
+                    state.RegisterX = state.RegisterY - state.RegisterX;
+                    break;
+
+                case "*":
+                    state.RegisterX = state.RegisterY * state.RegisterX;
+                    break;
+
+                case "/":
+                    state.RegisterX = state.RegisterY / state.RegisterX;
+                    break;
+                case "X2":
+                    state.RegisterX *= state.RegisterX;
+                    break;
+                case "X3": 
+                    state.RegisterX = Math.Pow(state.RegisterX, 3);
+                    break;
+            }
+        }   
+
+        public void PressOperation(CalculatorState state, string operationCode)
+        {
+            CompleteOperation(state, state.OperationCode);
+            MoveXToY(state);
+            state.OperationCode = operationCode;
+            state.IsClearNeeded = true;
+        }
+
+        public void PressEqual(CalculatorState state)
+        {
+            CompleteOperation(state, state.OperationCode);
+            state.IsClearNeeded = true;
+        }
+    }
+}
---
+++ Calculator/Calculator/CalculatorForm.cs
@@ -0,0 +1,77 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private readonly CalculatorState _state = new CalculatorState();
+
+        private readonly CalculatorService _service = new CalculatorService();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, EventArgs e)
+        {
+            var digit = ((Button)sender).Text;
+            _service.PressDigit(_state, digit);
+            ShowResult();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            _service.Clear(_state);
+            ShowResult();
+        }
+
+        private void ShowResult()
+        {
+            DisplayLabel.Text = _state.RegisterX.ToString();
+        }
+
+        private void XToYButton_Click(object sender, EventArgs e)
+        {
+            _service.MoveXToY(_state);
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            var operationCode = ((Button)sender).Text;
+            _service.PressOperation(_state, operationCode);
+            ShowResult();
+        }
+        private void X2Button_Click(object sender, EventArgs e)
+        {
+            _service.CompleteOperation(_state, "X2");
+            ShowResult();
+        }
+        private void RootButton_Click(object sender, EventArgs e)
+        {
+            _service.CompleteOperation(_state, "X3");
+            ShowResult();
+        }
+
+        private void EqualButton_Click(object sender, EventArgs e)
+        {
+            _service.PressEqual(_state);
+            ShowResult();
+        }
+
+        private void CalculatorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
