0569bd028f7a784139982ae12ba6dfd5d88ec34a
Nika47384938389 <nikastepanenkowa@gmail.com>
2025-06-07 20:39:44 +0300

    Update FileDataService.cs

--- CardFile/CaedFile.DataAccess/FileDataAccess/FileDataService.cs
+++ CardFile/CaedFile.DataAccess/FileDataAccess/FileDataService.cs
@@ -1,4 +1,4 @@
-﻿using CardFile.DataAccess.DataCollection;
+using CardFile.DataAccess.DataCollection;
 using CardFile.DataAccess.FileDataAccess.FileSavers;
 using System;
 using System.Collections.Generic;
@@ -11,16 +11,16 @@ namespace CardFile.DataAccess.FileDataAccess
 {
     public class FileDataService
     {
-        public void SaveToFile(string fileName, CardProductsCollection collection)
+        public void SaveToFile(string fileName, CardCollection collection)
         {
-            var fileSaver = GetFileSaver(fileName);
-            fileSaver.Save(fileName, collection);
+            var saver = GetFileSaver(fileName);
+            saver.SaveFile(fileName, collection);
         }
 
-        public void OpenFile(string fileName, CardProductsCollection collection)
+        public void OpenFromFile(string fileName, CardCollection collection)
         {
-            var fileSaver = GetFileSaver(fileName);
-            fileSaver.Open(fileName, collection);
+            var saver = GetFileSaver(fileName);
+            saver.OpenFile(fileName, collection);
         }
 
         private IFileSaver GetFileSaver(string fileName)
@@ -29,17 +29,22 @@ namespace CardFile.DataAccess.FileDataAccess
             {
                 case ".txt":
                     return new TextFileSaver();
+
                 case ".cardbin":
                     return new BinaryFileSaver();
-                case ".xml":
-                    return new XmlFileSaver();
-                case ".json":
-                    return new JsonFileSaver();
-                case ".zip":
-                    return new ZipFileSaver();
-            }
 
-            return new TextFileSaver();
+                case ".cardxml":
+                    return new XmlFileSaver();
+
+                case ".cardjson":
+                    return new JsonFileSaver();
+
+                case ".cardzip":
+                    return new ZipFileSaver();
+
+                default:
+                    throw new Exception("Неверное расширение имени файла");
+            }
         }
     }
 }
