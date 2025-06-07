0e28fefacf4d95040036353e89acc629b92095e6
Nika47384938389 <nikastepanenkowa@gmail.com>
2025-06-07 22:32:06 +0300

    Update ClockForm.cs

--- AlarmClock/AlarmClock/ClockForm.cs
+++ AlarmClock/AlarmClock/ClockForm.cs
@@ -1,9 +1,14 @@
-﻿using AlarmClock.Forms;
+using AlarmClock.Forms;
 using AlarmClock.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Media;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AlarmClock
@@ -11,74 +16,12 @@ namespace AlarmClock
     public partial class ClockForm : Form
     {
         private AlarmSettings _settings = new AlarmSettings();
-                                              static  bool _isplayer    = false;
-        private AwakeForm _awakeForm;
-        private const string ImageFolderName = "Images";
 
-        private List<string> _fileNames = new List<string>();
-
-        private int _imageIndex = 0;
         public ClockForm()
         {
             InitializeComponent();
-            
-           
-        }
-
-        private void AlarmTimer_Tick(object sender, EventArgs e)
-        {
-            // Обновление индикатора времени
-           
-
-            // Проверка срабатывания
-            if (_settings.IsAlarmActive && DateTime.Now.TimeOfDay >= _settings.AlarmTime.TimeOfDay)
-            {
-                if (_awakeForm == null || _awakeForm.IsDisposed)
-                {
-                    _awakeForm = new AwakeForm();
-                }
-
-                _awakeForm.Settings = _settings;
-                _awakeForm.Show();
-                string soundFilePath = @"C:Musing\bel.WAV";
-                System.Media.SoundPlayer snd = new SoundPlayer(soundFilePath);
-               
-                if (_settings.IsSoundActive)
-                {
-                    if (_isplayer)
-                    {
-                        snd.Stop();
-                        _isplayer = false;
-                    }
-                    else
-                    {
-                        snd.Play();
-                        _isplayer = true;
-                    }
-                }
-                else
-                {
-                    if (_isplayer)
-                    {
-                        snd.Play();
-                        _isplayer = true;
-                    }
-                    else
-                    {
-                        snd.Stop();
-                        _isplayer = false;
-                    }
-                };
-                
-              
-
-            }
-            Invalidate();
-        }
-
-        private void AboutButton_Click(object sender, EventArgs e)
-        {
-            new AboutForm().ShowDialog();
+            var currentTime = DateTime.Now.AddMinutes(1).TimeOfDay;
+            _settings.AlarmTime = new TimeSpan(currentTime.Hours, currentTime.Minutes, 0);
         }
 
         private void ExitButton_Click(object sender, EventArgs e)
@@ -86,91 +29,59 @@ namespace AlarmClock
             Close();
         }
 
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DateTime.Now.ToLongTimeString();
+
+            if (!_settings.IsAlarmActive)
+            {
+                return;
+            }
+
+            if (!_settings.IsAwakeActivated && 
+                DateTime.Now.Hour == _settings.AlarmTime.Hours &&
+                DateTime.Now.Minute == _settings.AlarmTime.Minutes)
+            {
+                _settings.IsAwakeActivated = true;
+                var awakeForm = new AwakeForm();
+                awakeForm.Settings = _settings;
+                awakeForm.FormClosed += AwakeForm_FormClosed;
+                awakeForm.ShowDialog();
+            }
+
+            if (_settings.IsSoundActive && _settings.IsAwakeActivated)
+            {
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        private void AwakeForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ((Form)sender).FormClosed -= AwakeForm_FormClosed;
+            ShowControls();
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            var aboutForm = new AboutForm();
+            aboutForm.ShowDialog();
+        }
+
         private void SettingsButton_Click(object sender, EventArgs e)
         {
-            var form = new SettingsForm();
-            form.Settings = _settings;
-            form.ShowDialog();
-        }
-        private int[] hrCoord(int hval, int mval, int hlen,int cx,int cy)
-        {
-            int[] coord = new int[2];
-            int val = (int)((hval * 30) + (mval * 0.5));
+            var settingsForm = new SettingsForm();
+            settingsForm.Settings = _settings;
+            if (settingsForm.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
 
-            if (val >= 0 && val <= 180)
-            {
-                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
-                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
-            }
-            else
-            {
-                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
-                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
-            }
-            return coord;
-        }
-        private int[] msCoord(int val, int hlen, int cx, int cy)
-        {
-            int[] coord = new int[2];
-            val *= 6;
-
-            if (val >= 0 && val <= 180)
-            {
-                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
-                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
-            }
-            else
-            {
-                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
-                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
-            }
-            return coord;
+            ShowControls();
         }
 
-
-
-        protected override void OnPaint(PaintEventArgs e)
+        private void ShowControls()
         {
-            base.OnPaint(e);
-           const  int secHAND = 80, minHAND = 60, hrHAND =55;
-
-            // Рисуем аналоговые часы
-            Pen seconPen = new Pen(Color.Black,1);
-            Pen hourPen = new Pen(Color.Red, 2);
-            Pen minutesPen  =  new Pen (Color.Green, 2);
-            ;
-           
-            float centerX = TimePikter1.Width+60;
-            float centerY = TimePikter1.Height /2+10;
-
-            
-
-        
-
-           
-            int[] hourCoords = hrCoord(DateTime.Now.Hour, DateTime.Now.Minute, hrHAND + 4, ((int)centerX),(int)(centerY)) ;
-            e.Graphics.DrawLine(hourPen, centerX, centerY, hourCoords[0], hourCoords[1]);
-           
-
-            int[] minuteCoords = msCoord(DateTime.Now.Minute, minHAND+4, ((int)centerX), (int)(centerY));
-            e.Graphics.DrawLine(minutesPen, centerX, centerY, minuteCoords[0], minuteCoords[1]);
-
-            int[] secondCoords = msCoord(DateTime.Now.Second, secHAND + 4, ((int)centerX), (int)(centerY));
-            e.Graphics.DrawLine(seconPen, centerX, centerY, secondCoords[0], secondCoords[1]);
-            e.Graphics.ResetTransform();
-
-
-           
-        }
-
-        private void TimePikter1_Click(object sender, EventArgs e)
-        {
-
+            Text = _settings.IsAlarmActive ? "Будильник (ожидание)" : "Будильник";
         }
     }
 }
-    
-
-
-
-
