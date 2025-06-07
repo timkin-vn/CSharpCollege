9466106e4e18d22d6452f7974e90077e6337be8c
Колчин Андрей <co6aka503@yandex.ru>
2025-06-07 17:51:12 +0300

    дабавил картотеку

---
+++ Projects.23.1.23.2/CardFile.Wpf/CardFile.Business/Models/Card.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Business.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название техники (например, "Т-34")
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Производитель/страна (например, "СССР")
+        /// </summary>
+        public string Manufacturer { get; set; }
+
+        /// <summary>
+        /// Конструктор (КБ или личность)
+        /// </summary>
+        public string Designer { get; set; }
+
+        /// <summary>
+        /// Год начала производства
+        /// </summary>
+        public int ProductionYear { get; set; }
+
+        /// <summary>
+        /// Тип техники (танк, самолёт и т.д.)
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Максимальная скорость (км/ч)
+        /// </summary>
+        public double MaxSpeed { get; set; }
+
+        /// <summary>
+        /// Характеристики орудия
+        /// </summary>
+        public string Gun { get; set; }
+
+        /// <summary>
+        /// Вес в тоннах/килограммах
+        /// </summary>
+        public double Weight { get; set; }
+    }
+}
---
+++ Projects.23.1.23.2/CardFile.Wpf/CardFile.DataAccess/Dtos/CardDto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.DataAccess.Dtos
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название техники (например, "Т-34")
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Производитель/страна (например, "СССР")
+        /// </summary>
+        public string Manufacturer { get; set; }
+
+        /// <summary>
+        /// Конструктор (например, "КБ Морозова" или "Михаил Кошкин")
+        /// </summary>
+        public string Designer { get; set; }
+
+        /// <summary>
+        /// Год начала производства (например, 1940)
+        /// </summary>
+        public int ProductionYear { get; set; }
+
+        /// <summary>
+        /// Тип техники (например, "Танк", "Истребитель")
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Максимальная скорость (в км/ч)
+        /// </summary>
+        public double MaxSpeed { get; set; }
+
+        /// <summary>
+        /// Калибр/модель орудия (например, "76-мм Ф-34")
+        /// </summary>
+        public string Gun { get; set; }
+
+        /// <summary>
+        /// Вес в тоннах/килограммах (например, 26.5)
+        /// </summary>
+        public double Weight { get; set; }
+
+        public CardDto Clone()
+        {
+            return new CardDto
+            {
+                Id = Id,
+                Name = Name,
+                Manufacturer = Manufacturer,
+                Designer = Designer,
+                ProductionYear = ProductionYear,
+                Type = Type,
+                MaxSpeed = MaxSpeed,
+                Gun = Gun,
+                Weight = Weight
+            };
+        }
+    }
+}
