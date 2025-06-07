18e59a9765236a3e82ffe4a4ee494f3f8521b7c3
Nika47384938389 <nikastepanenkowa@gmail.com>
2025-06-07 20:12:37 +0300

    Update CardProductsCollection.cs

--- CardFile/CaedFile.DataAccess/DataCollection/CardProductsCollection.cs
+++ CardFile/CaedFile.DataAccess/DataCollection/CardProductsCollection.cs
@@ -1,150 +1,121 @@
-﻿using CardFile.DataAccess.Dtos;
-using CardFile.DataAccess.Infrastructure;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using CardFile.DataAccess.Dtos;
+using CardFile.DataAccess.Infrastructure;
 
 namespace CardFile.DataAccess.DataCollection
 {
-    public class CardProductsCollection
+    public class CardCollection
     {
-        private List<CardProductsDto> _cards = new List<CardProductsDto>
+        private List<CardDto> _cards = new List<CardDto>
         {
-           new CardProductsDto
+            new CardDto
             {
                 Id = 1,
-                NameProducts = "Яблоки" ,
-                TypeProducts = "Fruits",
-                DateManufacture = new DateTime(2024,11,9),
-                DateExpiration = new DateTime(2024,11,15),
-
-                CountProducts = 1000,
-                PriceOneProducts = 50,
-                SectionProducts = "A",
-                ShirtProducts = "3",
+                LicensePlate = "A123BC45",
+                OwnerName = "Иван Иванов",
+                VehicleType = "Легковая",
+                EntryDate = new DateTime(2025, 6, 1, 8, 0, 0),
+                ExitDate = null,
+                HourlyRate = 100m,
+                IsPaid = true,
+                ParkingSpot = 1
             },
-            new CardProductsDto
+            new CardDto
             {
                 Id = 2,
-                NameProducts = "Оргурецы" ,
-                TypeProducts = "Vegetables",
-                DateManufacture = new DateTime(2024,11,9),
-                DateExpiration = new DateTime(2024,11,13),
-
-                CountProducts = 4000,
-                PriceOneProducts = 100,
-                SectionProducts = "B",
-                ShirtProducts = "1",
+                LicensePlate = "X456YZ78",
+                OwnerName = "Анна Петрова",
+                VehicleType = "Middle",
+                EntryDate = new DateTime(2025, 6, 1, 9, 15, 0),
+                ExitDate = new DateTime(2025, 6, 1, 14, 0, 0),
+                HourlyRate = 150m,
+                IsPaid = false,
+                ParkingSpot = 2
             },
-            new CardProductsDto
+            new CardDto
             {
                 Id = 3,
-                NameProducts = "Рыбные консервы" ,
-                TypeProducts = "Canned_Food",
-                DateManufacture = new DateTime(2023,11,9),
-                DateExpiration = new DateTime(2024,9,13),
-
-                CountProducts = 3000,
-                PriceOneProducts = 125,
-                SectionProducts = "Р",
-                ShirtProducts = "4",
+                LicensePlate = "M789NP12",
+                OwnerName = "Сергей Сидоров",
+                VehicleType = "Грузовая",
+                EntryDate = new DateTime(2025, 6, 1, 10, 30, 0),
+                ExitDate = new DateTime(2025, 6, 1, 16, 0, 0),
+                HourlyRate = 200m,
+                IsPaid = true,
+                ParkingSpot = 3
             },
-            new CardProductsDto
+            new CardDto
             {
                 Id = 4,
-                NameProducts = "Сыры" ,
-                TypeProducts = "Dairy",
-                DateManufacture = new DateTime(2024,11,9),
-                DateExpiration = new DateTime(2024,11,16),
-
-                CountProducts = 5000,
-                PriceOneProducts = 145,
-                SectionProducts = "М",
-                ShirtProducts = "3",
-            },
+                LicensePlate = "K321GH98",
+                OwnerName = "Елена Козлова",
+                VehicleType = "Легковая",
+                EntryDate = new DateTime(2025, 6, 1, 11, 45, 0),
+                ExitDate = null,
+                HourlyRate = 100m,
+                IsPaid = true,
+                ParkingSpot = 4
+            }
         };
 
         internal int CurrentId = 5;
 
-        public CardProductsCollection()
+        public CardCollection()
         {
-            MapperInitialize.Initialize();
+            MapperRegistrator.Register();
         }
 
-        public IEnumerable<CardProductsDto> GetAll()
+        public IEnumerable<CardDto> GetAll()
         {
-            return _cards.Select(c => c.Clone());
+            return _cards.Select(c => c.Clone()).ToList();
         }
 
-        public CardProductsDto Get(int id)
+        public int Save(CardDto cardDto)
         {
-            return _cards.FirstOrDefault(c => c.Id == id)?.Clone();
-        }
-
-        public bool Update(CardProductsDto card)
-        {
-            if (card.Id == 0)
+            if (cardDto.Id == 0)
             {
-                return false;
-            }
-
-            var existingCard = _cards.FirstOrDefault(c => c.Id == card.Id);
-            if (existingCard == null)
-            {
-                return false;
-            }
-
-            existingCard.Update(card);
-            return true;
-        }
-
-        public int Save(CardProductsDto card)
-        {
-            if (card.Id == 0)
-            {
-                var newCard = card.Clone();
-                newCard.Id = CurrentId++;
+                var newCard = cardDto.Clone();
+                var id = CurrentId++;
+                newCard.Id = id;
                 _cards.Add(newCard);
-                return newCard.Id;
+                return id;
             }
 
-            var existingCard = _cards.FirstOrDefault(c => c.Id == card.Id);
-            if (existingCard == null)
+            var index = _cards.FindIndex(c => c.Id == cardDto.Id);
+            if (index >= 0)
             {
-                return -1;
+                _cards[index] = cardDto.Clone();
+                return cardDto.Id;
             }
 
-            existingCard.Update(card);
-            return card.Id;
+            return -1;
         }
 
-        public bool Delete(int id)
+        public bool Delete(int cardId)
         {
-            var existingCard = _cards.FirstOrDefault(c => c.Id == id);
-
-            if (existingCard == null)
+            var index = _cards.FindIndex(c => c.Id == cardId);
+            if (index < 0)
             {
                 return false;
             }
 
-            _cards.Remove(existingCard);
+            _cards.RemoveAt(index);
             return true;
         }
 
-        internal void ReplaceCollection(IEnumerable<CardProductsDto> collection)
+        internal void ReplaceAll(IEnumerable<CardDto> source)
         {
             _cards.Clear();
-            _cards.AddRange(collection);
-            CurrentId = _cards.Max(c => c.Id) + 1;
+            _cards.AddRange(source);
+            CurrentId = _cards.Any() ? _cards.Max(c => c.Id) + 1 : 1;
         }
 
-        internal void ReplaceCollection(IEnumerable<CardProductsDto> collection, int currentId)
+        internal void ReplaceAll(IEnumerable<CardDto> source, int currentId)
         {
             _cards.Clear();
-            _cards.AddRange(collection);
+            _cards.AddRange(source);
             CurrentId = currentId;
         }
     }
