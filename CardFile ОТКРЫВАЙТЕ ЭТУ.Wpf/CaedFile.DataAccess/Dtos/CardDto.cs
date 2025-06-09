2c39b62ba196cdefe3547b8b2b0e16c72aa589a0
ArtyoskyWest <artyomslobodskoy1604@gmail.com>
2025-06-09 13:33:26 +0300

    АКТУАЛЬНОЕ 5ТОЕ ЗАДАНИЕ
    АКТУАЛЬНОЕ 5ТОЕ ЗАДАНИЕ

---
+++ CardFile ОТКРЫВАЙТЕ ЭТУ.Wpf/CaedFile.DataAccess/Dtos/CardDto.cs
@@ -0,0 +1,64 @@
+﻿using CardFile.Common.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.DataAccess.Dtos
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public decimal PaymentAmount { get; set; }
+
+        public int Circulation { get; set; }
+
+        public string Booktitle { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Year { get; set; }
+
+        public CardDto Clone()
+        {
+            return Mapping.Mapper.Map<CardDto>(this);
+            //return new CardDto
+            //{
+            //    Id = Id,
+            //    FirstName = FirstName,
+            //    MiddleName = MiddleName,
+            //    LastName = LastName,
+            //    BirthDate = BirthDate,
+            //    PaymentAmount = PaymentAmount,
+            //    Circulation = Circulation,
+            //    Booktitle = Booktitle,
+            //    Year = Year,
+            //    Publisher = Publisher,
+            //};
+        }
+
+        public void Update(CardDto newCard)
+        {
+            Mapping.Mapper.Map(newCard, this);
+            //FirstName = newCard.FirstName;
+            //MiddleName = newCard.MiddleName;
+            //LastName = newCard.LastName;
+            //BirthDate = newCard.BirthDate;
+            //PaymentAmount = newCard.PaymentAmount;
+            //Circulation = newCard.Circulation;
+            //ArticleNumber = newCard.ArticleNumber;
+            //Year = newCard.Year;
+            //Publisher = newCard.Publisher;
+        }
+    }
+}
---
+++ CardFile ОТКРЫВАЙТЕ ЭТУ.Wpf/CardFile.Wpf/ViewModels/CardViewModel.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Wpf.ViewModels
+{
+    public class CardViewModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+
+        private string _lastName;
+
+        private string _middleName;
+
+        private DateTime _birthDate = new DateTime(2000, 6, 15);
+
+        private decimal _paymentAmount;
+
+        private int _сirculation;
+
+        private string _booktitle;
+
+        private string _year;
+
+        private string _publisher;
+
+        public int Id { get; set; }
+
+        public string Artist => $"{LastName} {FirstName} {MiddleName}";
+
+        public string Booktitle
+        {
+            get => _booktitle;
+            set
+            {
+                _booktitle = value; 
+                OnPropertyChanged(nameof(Info));
+                OnPropertyChanged(nameof(Booktitle));
+            }
+        }
+        public string Publisher
+        {
+            get => _publisher;
+            set
+            {
+                _publisher = value;
+                OnPropertyChanged(nameof(Info));
+                OnPropertyChanged(nameof(Publisher));
+            }
+        }
+
+        public string Year
+        {
+            get => _year;
+            set
+            {
+                _year = value;
+                OnPropertyChanged(nameof(Info));
+                OnPropertyChanged(nameof(Year));
+            }
+        }
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(Artist));
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string MiddleName
+        {
+            get => _middleName;
+            set
+            {
+                _middleName = value;
+                OnPropertyChanged(nameof(Artist));
+                OnPropertyChanged(nameof(MiddleName));
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(Artist));
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get => _birthDate;
+            set
+            {
+                _birthDate = value;
+                OnPropertyChanged(nameof(BirthDate));
+            }
+        }
+
+
+        public string BirthDateText => BirthDate.ToShortDateString();
+
+        public decimal PaymentAmount
+        {
+            get => _paymentAmount;
+            set
+            {
+                _paymentAmount = value;
+                OnPropertyChanged(nameof(PaymentAmount));
+            }
+        }
+
+        public string PaymentAmountText => PaymentAmount.ToString("#,##0.00 р\\.");
+
+        public int Circulation
+        {
+            get => _сirculation;
+            set
+            {
+                _сirculation = value;
+                OnPropertyChanged(nameof(Circulation));
+            }
+        }
+
+        public string Info => $"{Booktitle} - {Publisher} ({Year})";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void CopyFrom(CardViewModel model)
+        {
+            Id = model.Id;
+            FirstName = model.FirstName;
+            MiddleName = model.MiddleName;
+            LastName = model.LastName;
+            BirthDate = model.BirthDate;
+            PaymentAmount = model.PaymentAmount;
+            Circulation = model.Circulation;
+            Booktitle = model.Booktitle;
+            Year = model.Year;
+            Publisher = model.Publisher;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
