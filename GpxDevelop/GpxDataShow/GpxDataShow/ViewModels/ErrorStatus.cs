6b822002868db1bd65c9b8b8915ff4cd0dfecbc9
Тимкин Владимир <timkin_vn@mail.ru>
2025-09-17 21:21:42 +0700

    Проект Игра для групп 23/2 и 23/3. Начало

---
+++ GpxDevelop/GpxDataShow/GpxDataShow/ViewModels/ErrorStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GpxDataShow.ViewModels
+{
+    [Flags]
+    internal enum ErrorStatus
+    {
+        None = 0,
+        Missed1Second = 1,
+        MissedManySeconds = 2,
+        SpeedTooDifferent = 4,
+    }
+}
