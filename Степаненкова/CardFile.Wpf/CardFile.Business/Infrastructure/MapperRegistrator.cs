76153ba5e7ff6f1e2181a3d5c40e8a8a37271dc7
Nika47384938389 <nikastepanenkowa@gmail.com>
2025-09-26 13:01:43 +0300

    Create MapperRegistrator.cs

---
+++ Степаненкова/CardFile.Wpf/CardFile.Business/Infrastructure/MapperRegistrator.cs
@@ -0,0 +1,26 @@
+using AutoMapper;
+using CardFile.Business.Models;
+using CardFile.Common.Infrastructure;
+using CardFile.DataAccess.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Business.Infrastructure
+{
+    internal class MapperRegistrator
+    {
+        public static void Register()
+        {
+            Mapping.InitializationAction += Configure;
+        }
+
+        private static void Configure(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<Card, CardDto>();
+            cfg.CreateMap<CardDto, Card>();
+        }
+    }
+}
