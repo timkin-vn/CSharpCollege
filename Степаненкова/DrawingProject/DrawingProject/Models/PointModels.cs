d9b6e040f081f281b6c1e348ecee6c574d778bf8
Nika47384938389 <nikastepanenkowa@gmail.com>
2025-09-29 20:25:35 +0300

    Create PointModels.cs

---
+++ Степаненкова/DrawingProject/DrawingProject/Models/PointModels.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingProject.Models
+{
+    internal class PointModels
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
