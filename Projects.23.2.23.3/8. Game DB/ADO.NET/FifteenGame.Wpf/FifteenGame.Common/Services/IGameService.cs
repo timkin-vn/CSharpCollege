00854c6a0640c140d52a1500600d60ad489c0021
Тимкин Владимир <timkin_vn@mail.ru>
2025-10-01 20:34:02 +0700

    Проект Игра БД для 23/2, 23/3

---
+++ Projects.23.2.23.3/8. Game DB/ADO.NET/FifteenGame.Wpf/FifteenGame.Common/Services/IGameService.cs
@@ -0,0 +1,26 @@
+﻿using FifteenGame.Common.BusinessModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Common.Services
+{
+    public interface IGameService
+    {
+        void Initialize(GameModel model);
+
+        bool MakeMove(GameModel model, MoveDirection direction);
+
+        void Shuffle(GameModel model);
+
+        bool IsGameOver(GameModel model);
+
+        GameModel GetByUserId(int userId);
+
+        GameModel GetByGameId(int gameId);
+
+        void RemoveGame(int gameId);
+    }
+}
