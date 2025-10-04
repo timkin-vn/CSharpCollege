3a04d5b92af2639ce240fb9c5f202de8c4ce2445
DESKTOP-GGKUQB9\Екатерина <ktrn.pchelkina@gmail.com>
2025-10-04 13:12:03 +0300

    Пятнашки (Без БД)

---
+++ PchelkinaEV/FifteenGame.Wpf/FifteenGame.Business/Services/GameService.cs
@@ -0,0 +1,114 @@
+﻿using FifteenGame.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Business.Services
+{
+    public class GameService
+    {
+        public void Initialize(GameField model)
+        {
+            int value = 1;
+            for (int row = 0; row < GameField.RowCount; row++)
+            {
+                for (int column = 0; column < GameField.ColumnCount; column++)
+                {
+                    model[row, column] = value++;
+                }
+            }
+
+            model[GameField.RowCount - 1, GameField.ColumnCount - 1] = GameField.FreeCellValue;
+            model.FreeCellRow = GameField.RowCount - 1;
+            model.FreeCellColumn = GameField.ColumnCount - 1;
+        }
+
+        public bool MakeMove(GameField model, MoveDirection direction)
+        {
+            switch (direction)
+            {
+                case MoveDirection.Left:
+                    if (model.FreeCellColumn == GameField.ColumnCount - 1)
+                    {
+                        return false;
+                    }
+
+                    model[model.FreeCellRow, model.FreeCellColumn] = model[model.FreeCellRow, model.FreeCellColumn + 1];
+                    model[model.FreeCellRow, model.FreeCellColumn + 1] = GameField.FreeCellValue;
+                    model.FreeCellColumn++;
+                    return true;
+
+                case MoveDirection.Right:
+                    if (model.FreeCellColumn == 0)
+                    {
+                        return false;
+                    }
+
+                    model[model.FreeCellRow, model.FreeCellColumn] = model[model.FreeCellRow, model.FreeCellColumn - 1];
+                    model[model.FreeCellRow, model.FreeCellColumn - 1] = GameField.FreeCellValue;
+                    model.FreeCellColumn--;
+                    return true;
+
+                case MoveDirection.Up:
+                    if (model.FreeCellRow == GameField.RowCount - 1)
+                    {
+                        return false;
+                    }
+
+                    model[model.FreeCellRow, model.FreeCellColumn] = model[model.FreeCellRow + 1, model.FreeCellColumn];
+                    model[model.FreeCellRow + 1, model.FreeCellColumn] = GameField.FreeCellValue;
+                    model.FreeCellRow++;
+                    return true;
+
+                case MoveDirection.Down:
+                    if (model.FreeCellRow == 0)
+                    {
+                        return false;
+                    }
+
+                    model[model.FreeCellRow, model.FreeCellColumn] = model[model.FreeCellRow - 1, model.FreeCellColumn];
+                    model[model.FreeCellRow - 1, model.FreeCellColumn] = GameField.FreeCellValue;
+                    model.FreeCellRow--;
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void Shuffle(GameField model)
+        {
+            Initialize(model);
+
+            var rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                var NextMove = (MoveDirection)(rnd.Next(4) + 1);
+                MakeMove(model, NextMove);
+            }
+        }
+
+        public bool IsGameOver(GameField model)
+        {
+            int value = 1;
+            for (int row = 0; row < GameField.RowCount; row++)
+            {
+                for (int column = 0; column < GameField.ColumnCount; column++)
+                {
+                    if (row == GameField.RowCount-1 && column == GameField.ColumnCount - 1)
+                    {
+                        return true;
+                    }
+
+                    if (model[row, column] != value++)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
