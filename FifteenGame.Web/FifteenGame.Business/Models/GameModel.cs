c4b1aa56e09a39b60f31df2e640e45a6c8560a52
THEKIDDDDD <148780888+THEKIDDDDD@users.noreply.github.com>
2025-10-08 20:50:45 +0300

    Add files via upload

---
+++ FifteenGame.Web/FifteenGame.Business/Models/GameModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Business.Models
+{
+    public class GameModel
+    {
+        public const int RowCount = 5;
+
+        public const int ColumnCount = 5;
+
+        public int PlayerValue = 0;
+
+        public int StepsLeft = 10;
+
+        private int[,] _cells = new int[RowCount, ColumnCount];
+
+        public int this[int row, int column]
+        {
+            get => _cells[row, column];
+            internal set => _cells[row, column] = value;
+        }
+
+        public int PlayerRow { get; internal set; }
+
+        public int PlayerColumn { get; internal set; }
+    }
+}
---
+++ FifteenGame.Web/FifteenGame.Business/Services/GameService.cs
@@ -0,0 +1,129 @@
+﻿using FifteenGame.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Business.Services
+{
+    public class GameService
+    {
+        public void Initialize(GameModel model)
+        {
+            int value = 1;
+            for (int row = 0; row < GameModel.RowCount; row++)
+            {
+                for (int column = 0; column < GameModel.ColumnCount; column++)
+                {
+                    if (value % 2 == 0)
+                    {
+                        model[row, column] = 0-value;
+                    }
+                    else
+                    {
+                        model[row, column] = 0+value;
+                    }
+                    value++;
+                }
+            }
+            model.PlayerValue = 0;
+            model.StepsLeft = 10;
+            model[GameModel.RowCount - 1, GameModel.ColumnCount - 1] = model.PlayerValue;
+            model.PlayerRow = GameModel.RowCount - 1;
+            model.PlayerColumn = GameModel.ColumnCount - 1;
+        }
+
+        public bool IsGameOver(GameModel model)
+        {
+            if (model.StepsLeft > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool MakeMove(GameModel model, MoveDirection direction, bool isShuffling)
+        {
+            switch (direction)
+            {
+                case MoveDirection.Left:
+                    if (model.PlayerColumn == GameModel.ColumnCount - 1)
+                    {
+                        return false;
+                    }
+                    if (!isShuffling)
+                    {
+                        model.PlayerValue += model[model.PlayerRow, model.PlayerColumn + 1];
+                        model[model.PlayerRow, model.PlayerColumn + 1] = 0;
+                        model.StepsLeft--;
+                    }
+                    model[model.PlayerRow, model.PlayerColumn] = model[model.PlayerRow, model.PlayerColumn + 1];
+                    model[model.PlayerRow, model.PlayerColumn + 1] = model.PlayerValue;
+                    model.PlayerColumn++;
+                    return true;
+
+                case MoveDirection.Right:
+                    if (model.PlayerColumn == 0)
+                    {
+                        return false;
+                    }
+                    if (!isShuffling)
+                    {
+                        model.PlayerValue += model[model.PlayerRow, model.PlayerColumn - 1];
+                        model[model.PlayerRow, model.PlayerColumn - 1] = 0;
+                        model.StepsLeft--;
+                    }
+                    model[model.PlayerRow, model.PlayerColumn] = model[model.PlayerRow, model.PlayerColumn - 1];
+                    model[model.PlayerRow, model.PlayerColumn - 1] = model.PlayerValue;
+                    model.PlayerColumn--;
+                    return true;
+
+                case MoveDirection.Up:
+                    if (model.PlayerRow == GameModel.RowCount - 1)
+                    {
+                        return false;
+                    }
+                    if (!isShuffling)
+                    {
+                        model.PlayerValue += model[model.PlayerRow + 1, model.PlayerColumn];
+                        model[model.PlayerRow + 1, model.PlayerColumn] = 0;
+                        model.StepsLeft--;
+                    }
+                    model[model.PlayerRow, model.PlayerColumn] = model[model.PlayerRow + 1, model.PlayerColumn];
+                    model[model.PlayerRow + 1, model.PlayerColumn] = model.PlayerValue;
+                    model.PlayerRow++;
+                    return true;
+
+                case MoveDirection.Down:
+                    if (model.PlayerRow == 0)
+                    {
+                        return false;
+                    }
+                    if (!isShuffling)
+                    {
+                        model.PlayerValue += model[model.PlayerRow - 1, model.PlayerColumn];
+                        model[model.PlayerRow - 1, model.PlayerColumn] = 0;
+                        model.StepsLeft--;
+                    }
+                    model[model.PlayerRow, model.PlayerColumn] = model[model.PlayerRow - 1, model.PlayerColumn];
+                    model[model.PlayerRow - 1, model.PlayerColumn] = model.PlayerValue;
+                    model.PlayerRow--;
+                    return true;
+            }
+            return false;
+        }
+
+        public void Shuffle(GameModel model)
+        {
+            Initialize(model);
+
+            var rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                var nextMove = (MoveDirection)(rnd.Next(4) + 1);
+                MakeMove(model, nextMove, true);
+            }
+        }
+    }
+}
