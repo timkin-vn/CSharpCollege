1da5cc1c7d94e02f28d2d153f06d22183df83735
Iimadix <sanxeigg@mail.ru>
2025-10-09 23:39:39 +0300

    3vRyad

---
+++ 7. Game/FifteenGame.Wpf/FifteenGame.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace FifteenGame.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
