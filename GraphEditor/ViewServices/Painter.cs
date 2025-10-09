0965316fee9cab8f5e7757ee823cb7a18726df04
remod3 <slenderzet@gmail.com>
2025-10-09 23:06:34 +0300

    Refactor Painter class for improved readability and formatting

--- GraphEditor/ViewServices/Painter.cs
+++ GraphEditor/ViewServices/Painter.cs
@@ -1,9 +1,9 @@
+using GraphEditor.Business.Models;
 using GraphEditor.ViewModels;
-using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
 
 namespace GraphEditor.ViewServices;
+
 
 internal class Painter {
     public void Paint(Graphics g, PictureViewModel? viewModel, bool isInteractive) {
@@ -18,14 +18,17 @@ internal class Painter {
             if (!string.IsNullOrWhiteSpace(rect.Text)) {
                 using var font = new Font(rect.FontFamily, rect.FontSize);
                 using var textBrush = new SolidBrush(rect.TextColor);
-                using var format = new StringFormat { Alignment = ToHorizontal(rect.TextAlign), LineAlignment = StringAlignment.Center, Trimming = StringTrimming.EllipsisCharacter };
+                using var format = new StringFormat {
+                    Alignment = ToHorizontal(rect.TextAlign),
+                    LineAlignment = StringAlignment.Center,
+                    Trimming = StringTrimming.EllipsisCharacter,
+                };
                 format.FormatFlags |= StringFormatFlags.LineLimit;
                 g.DrawString(rect.Text, font, textBrush, rect.Rectangle, format);
             }
 
             if (rect.IsSelected) {
-                using var selectionPen = new Pen(Color.Black);
-                selectionPen.DashStyle = DashStyle.Dot;
+                using var selectionPen = new Pen(Color.Black) { DashStyle = DashStyle.Dot };
                 g.DrawRectangle(selectionPen, rect.Rectangle);
             }
         }
