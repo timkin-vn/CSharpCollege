17b74b85663e00f8e772b916e8cc14f15253a786
misa47 <hayatotima@gmail.com>
2025-10-12 23:34:29 +0300

    update cell

--- Projects.24.1.23.1/7. Game/Match3Game.Wpf/Match3Game.Business/Models/Cell.cs
+++ Projects.24.1.23.1/7. Game/Match3Game.Wpf/Match3Game.Business/Models/Cell.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FifteenGame.Business.Models
+namespace Match3Game.Business.Models
 {
-    internal class Cell
+    public class Cell
     {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public TileType Type { get; set; }
+
+        public Cell(int row, int column, TileType type)
+        {
+            Row = row;
+            Column = column;
+            Type = type;
+        }
+    }
+
+    public enum TileType
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Purple
     }
 }
+
