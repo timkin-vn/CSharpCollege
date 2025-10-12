22e94a1de0dbae47c856a53b38e7fd5a0283b0ba
DIO <denis.kuzmin94@bk.ru>
2025-10-12 13:28:02 +0300

    Создал проект Lights Out

---
+++ KuzminDenis/LightsOut.Wpf/LightsOut.Wpf/ViewModels/CellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightsOut.Wpf.ViewModels
+{
+    internal class CellViewModel
+    {
+    }
+}
