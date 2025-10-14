499238f284a6e9fb3bf0a87369a8fa6df8773544
Martynova Nadia <mart.1764.nadia.2007@gmail.com>
2025-10-14 17:29:09 +0300

    commit game

---
+++ FrogGame.Wpf2/FrogGame.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using FrogGame.Wpf.ViewModels;
+using System.Windows;
+
+namespace FrogGame.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
