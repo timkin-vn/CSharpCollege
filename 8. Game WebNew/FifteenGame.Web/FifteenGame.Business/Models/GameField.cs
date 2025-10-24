d56e00f88aa5b9b6fe6e25056e7773d05f2284cf
Iimadix <sanxeigg@mail.ru>
2025-10-25 02:14:48 +0300

    Web

---
+++ 8. Game WebNew/FifteenGame.Web/FifteenGame.Business/Models/GameField.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Business.Models
+{
+    public class GameField
+    {
+        public const int RowCount = 8;
+        public const int ColumnCount = 8;
+        public const int MinMatchLength = 3;
+        public const int GemTypes = 6;
+        public const int EmptyCell = -1;
+
+        private int[,] _gems = new int[RowCount, ColumnCount];
+        private bool[,] _matched = new bool[RowCount, ColumnCount];
+
+        public int this[int row, int column]
+        {
+            get => _gems[row, column];
+            internal set => _gems[row, column] = value;
+        }
+
+        public bool[,] Matched => _matched;
+        public int Score { get; set; }
+        public int MovesLeft { get; set; } = 20;
+        public GameState GameState { get; set; }
+        public int SelectedRow { get; set; } = -1;
+        public int SelectedColumn { get; set; } = -1;
+    }
+
+    public enum GameState
+    {
+        Playing,
+        Won,
+        Lost
+    }
+}
---
+++ 8. Game WebNew/FifteenGame.Web/FifteenGame.Web/Controllers/GameController.cs
@@ -0,0 +1,98 @@
+﻿using FifteenGame.Business.Models;
+using FifteenGame.Business.Services;
+using FifteenGame.Web.Models;
+using System.Web.Mvc;
+
+namespace FifteenGame.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly GameService _service = new GameService();
+
+        public ActionResult Index()
+        {
+            var model = GetModel();
+            if (Session.IsNewSession)
+            {
+                _service.Initialize(model);
+                SaveModel(model);
+            }
+
+            return View(GetViewModel(model));
+        }
+
+        public ActionResult SelectGem(int row, int column)
+        {
+            var model = GetModel();
+            bool moveMade = _service.SelectGem(model, row, column);
+            SaveModel(model);
+
+            var viewModel = GetViewModel(model);
+
+            if (model.GameState != GameState.Playing)
+            {
+                viewModel.GameOverMessage = model.GameState == GameState.Won
+                    ? $"Поздравляем с победой! Набрано {model.Score} очков!"
+                    : $"Игра окончена. Набрано {model.Score} очков.";
+            }
+
+            return View("Index", viewModel);
+        }
+
+
+
+        public ActionResult NewGame()
+        {
+            Session.Remove("GameField");
+
+            var newModel = new GameField();
+            _service.Initialize(newModel);
+            SaveModel(newModel);
+
+            return RedirectToAction("Index");
+        }
+
+        private GameField GetModel()
+        {
+            if (Session["GameField"] == null)
+            {
+                Session["GameField"] = new GameField();
+            }
+
+            return (GameField)Session["GameField"];
+        }
+
+        private void SaveModel(GameField model)
+        {
+            Session["GameField"] = model;
+        }
+
+        private GameViewModel GetViewModel(GameField model)
+        {
+            var viewModel = new GameViewModel
+            {
+                Score = model.Score,
+                MovesLeft = model.MovesLeft,
+                GameState = model.GameState,
+                Cells = new CellViewModel[GameField.RowCount, GameField.ColumnCount]
+            };
+
+            for (int row = 0; row < GameField.RowCount; row++)
+            {
+                for (int column = 0; column < GameField.ColumnCount; column++)
+                {
+                    viewModel.Cells[row, column] = new CellViewModel
+                    {
+                        Row = row,
+                        Column = column,
+                        GemType = model[row, column],
+                        IsSelected = (row == model.SelectedRow && column == model.SelectedColumn),
+                        IsMatched = model.Matched[row, column]
+                    };
+                }
+            }
+
+            return viewModel;
+        }
+    }
+}
