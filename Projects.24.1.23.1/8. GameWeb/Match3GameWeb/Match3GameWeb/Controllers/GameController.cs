dcda3ac23c5b4bc02edc588810ff8eff13380ad4
misa47 <hayatotima@gmail.com>
2025-10-28 14:13:00 +0300

    Match3GameWeb

---
+++ Projects.24.1.23.1/8. GameWeb/Match3GameWeb/Match3GameWeb/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Match3GameWeb.Controllers
+{
+    public class GameController
+    {
+    }
+}
---
+++ Projects.24.1.23.1/8. GameWeb/Match3GameWeb/Match3GameWeb/Models/GameViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Match3GameWeb.Models
+{
+    public class GameViewModel
+    {
+    }
+}
