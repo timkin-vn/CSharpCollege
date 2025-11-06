85ef0a013c945141d508b367c951de756383b71c
AspikRay <117273181+AspikRay@users.noreply.github.com>
2025-11-06 18:35:40 +0300

    add
    GraphEditor+FifteenGame.Wpf

---
+++ project/Игра/FifteenGame.Wpf/FifteenGame.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FifteenGame.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FifteenGame
+{
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new MainWindowViewModel();
+            DataContext = _viewModel;
+
+
+            Closed += (s, e) => Application.Current.Shutdown();
+        }
+
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel = new MainWindowViewModel();
+            DataContext = _viewModel;
+        }
+    }
+}
