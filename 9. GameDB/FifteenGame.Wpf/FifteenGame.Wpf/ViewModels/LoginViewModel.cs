1e641e3151a8d950126e9edb42c791fc68143ef2
Iimadix <sanxeigg@mail.ru>
2025-11-23 22:59:28 +0300

    .

---
+++ 9. GameDB/FifteenGame.Wpf/FifteenGame.Wpf/ViewModels/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using FifteenGame.Business.Services;
+using FifteenGame.Data.Entities;
+using FifteenGame.Wpf.Commands;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FifteenGame.Wpf.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private readonly AuthService _authService = new AuthService();
+        public event Action<User> OnLoginSuccess;
+
+        public string Username { get; set; }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RegisterCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand<object>(Login);
+            RegisterCommand = new RelayCommand<object>(Register);
+        }
+
+        private void Login(object parameter)
+        {
+            try
+            {
+                var passwordBox = parameter as PasswordBox;
+                var user = _authService.Login(Username, passwordBox.Password);
+                OnLoginSuccess?.Invoke(user);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка входа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Register(object parameter)
+        {
+            try
+            {
+                var passwordBox = parameter as PasswordBox;
+                _authService.Register(Username, passwordBox.Password);
+                MessageBox.Show("Регистрация успешна! Теперь выполните вход.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка регистрации", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ 9. GameDB/FifteenGame.Wpf/FifteenGame.Wpf/ViewModels/ModeSelectionViewModel.cs
@@ -0,0 +1,32 @@
+﻿using FifteenGame.Business.Models;
+using FifteenGame.Data.Entities;
+using FifteenGame.Wpf.Commands;
+using System;
+using System.Windows.Input;
+
+namespace FifteenGame.Wpf.ViewModels
+{
+    public class ModeSelectionViewModel
+    {
+        private readonly User _currentUser;
+        public event Action<GameMode> OnGameModeSelected;
+
+        public string WelcomeMessage => $"Добро пожаловать, {_currentUser.Username}!";
+        public string BestScoreInfo => $"Ваш лучший рекорд: {_currentUser.BestScore}";
+
+        public ICommand StartClassicCommand { get; }
+        public ICommand StartRankedCommand { get; }
+
+        public ModeSelectionViewModel(User user)
+        {
+            _currentUser = user;
+            StartClassicCommand = new RelayCommand(() => SelectMode(GameMode.Classic));
+            StartRankedCommand = new RelayCommand(() => SelectMode(GameMode.Ranked));
+        }
+
+        private void SelectMode(GameMode mode)
+        {
+            OnGameModeSelected?.Invoke(mode);
+        }
+    }
+}
