5ec8422dcab2139c86d87ed95031a8bcf77e4eaf
Kami-slp <johndrums07@gmail.com>
2025-12-12 22:19:49 +0300

    d

---
+++ BD/FifteenGame.Wpf/FifteenGame.Business/Models/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace FifteenGame.Business.Models
+{
+    public class Game
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public Field PlayerField { get; set; } = new Field();
+        public Field OpponentField { get; set; } = new Field();
+        public bool IsPlayerTurn { get; set; } = true;
+
+        public bool IsOver
+        {
+            get { return PlayerField.Ships.All(s => s.IsSunk) || OpponentField.Ships.All(s => s.IsSunk); }
+        }
+
+        public string Winner
+        {
+            get
+            {
+                if (!IsOver) return null;
+                return PlayerField.Ships.All(s => s.IsSunk) ? "Opponent" : "Player";
+            }
+        }
+    }
+}
---
+++ BD/FifteenGame.Wpf/FifteenGame.Data/Entities/user.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FifteenGame.Data.Entities
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Index(IsUnique = true)]
+        public string Username { get; set; }
+
+        // Рекорд времени в секундах (nullable)
+        public double? BestTimeSeconds { get; set; }
+    }
+}
