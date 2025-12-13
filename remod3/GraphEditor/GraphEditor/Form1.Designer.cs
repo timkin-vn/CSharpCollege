c1ddd738d5aa4d74fb6bef9dc058007a3f6a5481
slend <slenderzet@gmail.com>
2025-12-13 22:31:56 +0300

    Add Form1.Designer.cs for GraphEditorForm UI
    Introduces the designer file for GraphEditorForm, defining the UI layout and controls such as toolbars, menus, dialogs, and event handlers for the GraphEditor application.

---
+++ remod3/GraphEditor/GraphEditor/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace GraphEditor {
+    sealed partial class GraphEditorForm {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GraphEditorForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.CreateRectangleButton = new System.Windows.Forms.ToolStripButton();
+            this.DeleteRectangleButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.FillColorMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.MoveForwardMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileCreateMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileOpenMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileSaveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileSaveAsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.FileExportMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.FileExportSvgMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileExportJsonMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileOpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.FileSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.FileExportDialog = new System.Windows.Forms.SaveFileDialog();
+            this.FillColorDialog = new System.Windows.Forms.ColorDialog();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateRectangleButton,
+            this.DeleteRectangleButton,
+            this.toolStripSeparator1,
+            this.toolStripSplitButton1,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            
+            this.SetTextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SetTextMenuItem.Name = "SetTextMenuItem";
+            this.SetTextMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.SetTextMenuItem.Text = "Задать текст...";
+            this.SetTextMenuItem.Click += new System.EventHandler(this.SetTextMenuItem_Click);
+
+            this.GroupMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GroupMenuItem.Name = "GroupMenuItem";
+            this.GroupMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.GroupMenuItem.Text = "Сгруппировать";
+            this.GroupMenuItem.Click += new System.EventHandler(this.GroupMenuItem_Click);
+
+            this.UngroupMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UngroupMenuItem.Name = "UngroupMenuItem";
+            this.UngroupMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.UngroupMenuItem.Text = "Разгруппировать";
+            this.UngroupMenuItem.Click += new System.EventHandler(this.UngroupMenuItem_Click);
+            
+            this.CreateRectangleButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.CreateRectangleButton.Image = ((System.Drawing.Image)(resources.GetObject("CreateRectangleButton.Image")));
+            this.CreateRectangleButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CreateRectangleButton.Name = "CreateRectangleButton";
+            this.CreateRectangleButton.Size = new System.Drawing.Size(54, 22);
+            this.CreateRectangleButton.Text = "Создать";
+            this.CreateRectangleButton.Click += new System.EventHandler(this.CreateRectangleButton_Click);
+
+            this.DeleteRectangleButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.DeleteRectangleButton.Image = ((System.Drawing.Image)(resources.GetObject("DeleteRectangleButton.Image")));
+            this.DeleteRectangleButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DeleteRectangleButton.Name = "DeleteRectangleButton";
+            this.DeleteRectangleButton.Size = new System.Drawing.Size(55, 22);
+            this.DeleteRectangleButton.Text = "Удалить";
+            this.DeleteRectangleButton.Click += new System.EventHandler(this.DeleteRectangleButton_Click);
+            
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FillColorMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(49, 22);
+            this.toolStripSplitButton1.Text = "Цвет";
+           
+            this.FillColorMenuItem.Name = "FillColorMenuItem";
+            this.FillColorMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.FillColorMenuItem.Text = "Цвет заливки...";
+            this.FillColorMenuItem.Click += new System.EventHandler(this.FillColorMenuItem_Click);
+           
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MoveForwardMenuItem,
+            this.SetTextMenuItem,
+            this.GroupMenuItem,
+            this.UngroupMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(68, 22);
+            this.toolStripDropDownButton1.Text = "Порядок";
+         
+            this.MoveForwardMenuItem.Name = "MoveForwardMenuItem";
+            this.MoveForwardMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.MoveForwardMenuItem.Text = "Ближе";
+            this.MoveForwardMenuItem.Click += new System.EventHandler(this.MoveForwardMenuItem_Click);
+       
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+          
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileCreateMenuItem,
+            this.FileOpenMenuItem,
+            this.FileSaveMenuItem,
+            this.FileSaveAsMenuItem,
+            this.toolStripMenuItem1,
+            this.FileExportMenuItem,
+            this.FileExportSvgMenuItem,
+            this.FileExportJsonMenuItem,
+            this.toolStripMenuItem2,
+            this.FileExitMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+          
+            this.FileCreateMenuItem.Name = "FileCreateMenuItem";
+            this.FileCreateMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileCreateMenuItem.Text = "Создать";
+            this.FileCreateMenuItem.Click += new System.EventHandler(this.FileCreateMenuItem_Click);
+          
+            this.FileOpenMenuItem.Name = "FileOpenMenuItem";
+            this.FileOpenMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileOpenMenuItem.Text = "Открыть...";
+            this.FileOpenMenuItem.Click += new System.EventHandler(this.FileOpenMenuItem_Click);
+         
+            this.FileSaveMenuItem.Name = "FileSaveMenuItem";
+            this.FileSaveMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileSaveMenuItem.Text = "Сохранить";
+            this.FileSaveMenuItem.Click += new System.EventHandler(this.FileSaveMenuItem_Click);
+        
+            this.FileSaveAsMenuItem.Name = "FileSaveAsMenuItem";
+            this.FileSaveAsMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileSaveAsMenuItem.Text = "Сохранить как...";
+            this.FileSaveAsMenuItem.Click += new System.EventHandler(this.FileSaveAsMenuItem_Click);
+   
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+    
+            this.FileExportMenuItem.Name = "FileExportMenuItem";
+            this.FileExportMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileExportMenuItem.Text = "Экспорт...";
+            this.FileExportMenuItem.Click += new System.EventHandler(this.FileExportMenuItem_Click);
+            
+            this.FileExportSvgMenuItem.Name = "FileExportSvgMenuItem";
+            this.FileExportSvgMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileExportSvgMenuItem.Text = "Экспорт в SVG...";
+            this.FileExportSvgMenuItem.Click += new System.EventHandler(this.FileExportSvgMenuItem_Click);
+            
+            this.FileExportJsonMenuItem.Name = "FileExportJsonMenuItem";
+            this.FileExportJsonMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileExportJsonMenuItem.Text = "Экспорт в JSON...";
+            this.FileExportJsonMenuItem.Click += new System.EventHandler(this.FileExportJsonMenuItem_Click);
+       
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
+      
+            this.FileExitMenuItem.Name = "FileExitMenuItem";
+            this.FileExitMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.FileExitMenuItem.Text = "Выход";
+            this.FileExitMenuItem.Click += new System.EventHandler(this.FileExitMenuItem_Click);
+       
+            this.FileOpenDialog.Filter = "Файлы рисунка|*.pict|Все файлы|*.*";
+        
+            this.FileSaveDialog.Filter = "Файлы рисунка|*.pict|Все файлы|*.*";
+       
+            this.FileExportDialog.Filter = "Файлы PNG|*.png";
+     
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GraphEditorForm";
+            this.Text = "Графический редактор";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GraphEditorForm_Paint);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.GraphEditorForm_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GraphEditorForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GraphEditorForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.GraphEditorForm_MouseUp);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripMenuItem SetTextMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem GroupMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem UngroupMenuItem;
+        private System.Windows.Forms.ToolStripButton CreateRectangleButton;
+        private System.Windows.Forms.ToolStripButton DeleteRectangleButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileCreateMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileOpenMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileSaveMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileSaveAsMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem FileExportMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileExportSvgMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileExportJsonMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem FileExitMenuItem;
+        private System.Windows.Forms.OpenFileDialog FileOpenDialog;
+        private System.Windows.Forms.SaveFileDialog FileSaveDialog;
+        private System.Windows.Forms.SaveFileDialog FileExportDialog;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem FillColorMenuItem;
+        private System.Windows.Forms.ColorDialog FillColorDialog;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem MoveForwardMenuItem;
+    }
+}
+
