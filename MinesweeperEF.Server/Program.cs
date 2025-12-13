9c5ed49fda7774efcdf96e9d6c2c9dde5d655662
slend <slenderzet@gmail.com>
2025-12-13 19:21:10 +0300

    データベース初期化処理を追加し、JwtOptionsをDIコンテナに登録

--- MinesweeperEF.Server/Program.cs
+++ MinesweeperEF.Server/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using MinesweeperEF.Server;
 using MinesweeperEF.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(o =>
     o.UseSqlite("Data Source=minesweeper.db"));
 
 const string JwtKey = "REPLACE_THIS_SUPER_SECRET_AND_LONG_KEY_123";
+builder.Services.AddSingleton(new JwtOptions(JwtKey));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(o => {
@@ -27,6 +29,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
