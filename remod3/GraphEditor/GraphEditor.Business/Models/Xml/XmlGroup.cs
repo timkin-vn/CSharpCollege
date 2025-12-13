6bdb6943a3dd69b257ca0ca7d660ad880bccbf75
slend <slenderzet@gmail.com>
2025-12-13 22:30:55 +0300

    Add XML model classes for picture, group, rectangle, and color
    Introduces XmlPicture, XmlGroup, XmlRectangle, and XmlColor classes to represent figures, groups, and color information for XML serialization in the GraphEditor. These models will support structured import/export of graphical data.

---
+++ remod3/GraphEditor/GraphEditor.Business/Models/Xml/XmlGroup.cs
@@ -0,0 +1,16 @@
+using System.Xml.Serialization;
+
+namespace GraphEditor.Business.Models.Xml;
+
+[Serializable]
+public class XmlGroup {
+    [XmlAttribute("Id")]
+    public Guid Id { get; set; }
+
+    [XmlAttribute("Name")]
+    public string? Name { get; set; }
+
+    [XmlArray("Members")]
+    [XmlArrayItem("RectangleId")]
+    public List<Guid> RectangleIds { get; set; } = [];
+}
