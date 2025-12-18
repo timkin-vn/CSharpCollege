87da5396d19452fb74afba07c239df81743bfe24
ArtyoskyWest <artyomslobodskoy1604@gmail.com>
2025-12-18 09:04:14 +0300

    bla bla

---
+++ GraphEditor/GraphEditor/GraphEditor/ViewModels/RectangleViewModel.cs
@@ -0,0 +1,206 @@
+﻿using GraphEditor.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphEditor.ViewModels
+{
+    internal class RectangleViewModel
+    {
+        public int Dx { get; set; }
+
+        public int Dy { get; set; }
+
+        public int Left { get; set; }
+
+        public int Top { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int Bottom
+        {
+            get => Top + Height;
+            set => Height = value - Top;
+        }
+
+        public int Right
+        {
+            get => Left + Width;
+            set => Width = value - Left;
+        }
+
+        public EditMode EditMode { get; set; }
+
+        public Color FillColor { get; set; } = Color.Yellow;
+
+        public Brush FillBrush { get; set; }
+
+        public Color BorderColor { get; set; } = Color.Blue;
+
+        public Pen BorderPen { get; set; }
+
+        public ShapeType Shape { get; set; } = ShapeType.Rectangle;
+
+        public bool IsMirrored { get; set; }
+
+        public Rectangle Rectangle => new Rectangle
+        {
+            X = Left < Right ? Left : Right,
+            Y = Top < Bottom ? Top : Bottom,
+            Width = Width > 0 ? Width : -Width,
+            Height = Height > 0 ? Height : -Height,
+        };
+
+        public IEnumerable<MarkerViewModel> Markers => new[]
+        {
+            new MarkerViewModel
+            {
+                Rectangle = new Rectangle
+                {
+                    X = Left - MarkerViewModel.MarkerHalfSize,
+                    Y = Top - MarkerViewModel.MarkerHalfSize,
+                    Width = MarkerViewModel.MarkerHalfSize * 2,
+                    Height = MarkerViewModel.MarkerHalfSize * 2,
+                },
+                IsActive = false,
+                EditMode = EditMode.ResizeTL,
+                Cursor = Cursors.SizeNWSE,
+            },
+            new MarkerViewModel
+            {
+                Rectangle = new Rectangle
+                {
+                    X = (Left + Right) / 2 - MarkerViewModel.MarkerHalfSize,
+                    Y = Top - MarkerViewModel.MarkerHalfSize,
+                    Width = MarkerViewModel.MarkerHalfSize * 2,
+                    Height = MarkerViewModel.MarkerHalfSize * 2,
+                },
+                IsActive = false,
+                EditMode = EditMode.ResizeT,
+                Cursor = Cursors.SizeNS,
+            },
+            new MarkerViewModel
+            {
+                Rectangle = new Rectangle
+                {
+                    X = Right - MarkerViewModel.MarkerHalfSize,
+                    Y = Top - MarkerViewModel.MarkerHalfSize,
+                    Width = MarkerViewModel.MarkerHalfSize * 2,
+                    Height = MarkerViewModel.MarkerHalfSize * 2,
+                },
+                IsActive = false,
+                EditMode = EditMode.ResizeTR,
+                Cursor = Cursors.SizeNESW,
+            },
+            new MarkerViewModel
+            {
+                Rectangle = new Rectangle
+                {
+                    X = Right - MarkerViewModel.MarkerHalfSize,
+                    Y = (Top + Bottom) /2 - MarkerViewModel.MarkerHalfSize,
+                    Width = MarkerViewModel.MarkerHalfSize * 2,
+                    Height = MarkerViewModel.MarkerHalfSize * 2,
+                },
+                IsActive = true,
+                EditMode = EditMode.ResizeR,
+                Cursor = Cursors.SizeWE,
+            },
+            new MarkerViewModel
+            {
+                Rectangle = new Rectangle
+                {
+                    X = Right - MarkerViewModel.MarkerHalfSize,
+                    Y = Bottom - MarkerViewModel.MarkerHalfSize,
+                    Width = MarkerViewModel.MarkerHalfSize * 2,
+                    Height = MarkerViewModel.MarkerHalfSize * 2,
+                },
+                IsActive = true,
+                EditMode = EditMode.ResizeBR,
+                Cursor = Cursors.SizeNWSE,
+            },
+            new MarkerViewModel
+            {
+                Rectangle = new Rectangle
+                {
+                    X = (Left + Right) / 2 - MarkerViewModel.MarkerHalfSize,
+                    Y = Bottom - MarkerViewModel.MarkerHalfSize,
+                    Width = MarkerViewModel.MarkerHalfSize * 2,
+                    Height = MarkerViewModel.MarkerHalfSize * 2,
+                },
+                IsActive = false,
+                EditMode = EditMode.ResizeB,
+                Cursor = Cursors.SizeNS,
+            },
+            new MarkerViewModel
+            {
+                Rectangle = new Rectangle
+                {
+                    X = Left - MarkerViewModel.MarkerHalfSize,
+                    Y = Bottom - MarkerViewModel.MarkerHalfSize,
+                    Width = MarkerViewModel.MarkerHalfSize * 2,
+                    Height = MarkerViewModel.MarkerHalfSize * 2,
+                },
+                IsActive = false,
+                EditMode = EditMode.ResizeBL,
+                Cursor = Cursors.SizeNESW,
+            },
+            new MarkerViewModel
+            {
+                Rectangle = new Rectangle
+                {
+                    X = Left - MarkerViewModel.MarkerHalfSize,
+                    Y = (Top + Bottom) /2 - MarkerViewModel.MarkerHalfSize,
+                    Width = MarkerViewModel.MarkerHalfSize * 2,
+                    Height = MarkerViewModel.MarkerHalfSize * 2,
+                },
+                IsActive = false,
+                EditMode = EditMode.ResizeL,
+                Cursor = Cursors.SizeWE,
+            },
+        };
+
+        public static RectangleViewModel FromBusiness(RectangleModel model)
+        {
+            return new RectangleViewModel
+            {
+                Dx = model.Dx,
+                Dy = model.Dy,
+                Left = model.Left,
+                Top = model.Top,
+                Width = model.Width,
+                Height = model.Height,
+                EditMode = model.EditMode,
+                FillColor = model.FillColor,
+                FillBrush = new SolidBrush(model.FillColor),
+                BorderColor = model.BorderColor,
+                BorderPen = new Pen(model.BorderColor, 3),
+                Shape = model.Shape,
+                IsMirrored = model.IsMirrored,
+            };
+        }
+
+        public RectangleModel ToBusiness()
+        {
+            return new RectangleModel
+            {
+                Dx = Dx,
+                Dy = Dy,
+                Left = Left,
+                Top = Top,
+                Width = Width,
+                Height = Height,
+                EditMode = EditMode,
+                FillColor = FillColor,
+                BorderColor = BorderColor,
+                Shape = Shape,
+                IsMirrored = IsMirrored,
+            };
+        }
+    }
+}
---
+++ GraphEditor/GraphEditor/GraphEditor/ViewServices/Painter.cs
@@ -0,0 +1,132 @@
+﻿using GraphEditor.ViewModels;
+using System;
+using System.Drawing;
+using System.Linq;
+
+namespace GraphEditor.ViewServices
+{
+    internal class Painter
+    {
+        public void Paint(Graphics g, PictureViewModel viewModel, bool isInteractive)
+        {
+            if (viewModel?.Rectangles == null || !viewModel.Rectangles.Any())
+                return;
+
+            foreach (var rect in viewModel.Rectangles)
+            {
+                var r = rect.Rectangle;
+
+                if (r.Width <= 0 || r.Height <= 0)
+                    continue;
+
+                if (rect.FillBrush != null)
+                {
+                    switch (rect.Shape)
+                    {
+                        case GraphEditor.Business.Models.ShapeType.Circle:
+                            g.FillEllipse(rect.FillBrush, r);
+                            break;
+                        case GraphEditor.Business.Models.ShapeType.Triangle:
+                            var t1 = GetTrianglePoints(r);
+                            g.FillPolygon(rect.FillBrush, t1);
+                            break;
+                        case GraphEditor.Business.Models.ShapeType.Hexagon:
+                            var hx = GetHexagonPoints(r);
+                            g.FillPolygon(rect.FillBrush, hx);
+                            break;
+                        case GraphEditor.Business.Models.ShapeType.Parallelogram:
+                            var pp = GetParallelogramPoints(r);
+                            g.FillPolygon(rect.FillBrush, pp);
+                            break;
+                        default:
+                            g.FillRectangle(rect.FillBrush, r);
+                            break;
+                    }
+                }
+
+                if (rect.BorderPen != null)
+                {
+                    switch (rect.Shape)
+                    {
+                        case GraphEditor.Business.Models.ShapeType.Circle:
+                            g.DrawEllipse(rect.BorderPen, r);
+                            break;
+                        case GraphEditor.Business.Models.ShapeType.Triangle:
+                            var t2 = GetTrianglePoints(r);
+                            g.DrawPolygon(rect.BorderPen, t2);
+                            break;
+                        case GraphEditor.Business.Models.ShapeType.Hexagon:
+                            var hx2 = GetHexagonPoints(r);
+                            g.DrawPolygon(rect.BorderPen, hx2);
+                            break;
+                        case GraphEditor.Business.Models.ShapeType.Parallelogram:
+                            var pp2 = GetParallelogramPoints(r);
+                            g.DrawPolygon(rect.BorderPen, pp2);
+                            break;
+                        default:
+                            g.DrawRectangle(rect.BorderPen, r);
+                            break;
+                    }
+                }
+            }
+
+            if (isInteractive && viewModel.Markers != null)
+            {
+                var activeBrush = Brushes.Black;
+                var inactiveBrush = Brushes.White;
+                var pen = Pens.Black;
+
+                foreach (var marker in viewModel.Markers)
+                {
+                    var r = marker.Rectangle;
+                    if (r.Width <= 0 || r.Height <= 0)
+                        continue;
+
+                    var brush = marker.IsActive ? activeBrush : inactiveBrush;
+                    g.FillRectangle(brush, r);
+                    g.DrawRectangle(pen, r);
+                }
+            }
+        }
+
+        private Point[] GetTrianglePoints(Rectangle r)
+        {
+            // Use normalized rectangle (r.Top is min Y, r.Bottom is max Y) so triangle always points up
+            var pApex = new Point(r.Left + r.Width / 2, r.Top);
+            var pLeft = new Point(r.Left, r.Bottom);
+            var pRight = new Point(r.Right, r.Bottom);
+            return new[] { pApex, pLeft, pRight };
+        }
+
+        private Point[] GetHexagonPoints(Rectangle r)
+        {
+            // regular hexagon inscribed into rectangle horizontally
+            int cx = r.Left + r.Width / 2;
+            int cy = r.Top + r.Height / 2;
+            int rx = r.Width / 2;
+            int ry = r.Height / 2;
+
+            var pts = new Point[6];
+            for (int i = 0; i < 6; i++)
+            {
+                double angle = Math.PI / 6 + i * Math.PI / 3; // start at 30 degrees
+                int x = cx + (int)(rx * Math.Cos(angle));
+                int y = cy + (int)(ry * Math.Sin(angle));
+                pts[i] = new Point(x, y);
+            }
+
+            return pts;
+        }
+
+        private Point[] GetParallelogramPoints(Rectangle r)
+        {
+            // skew horizontally by 25% of width
+            int skew = (int)(r.Width * 0.25);
+            var p1 = new Point(r.Left + skew, r.Top);
+            var p2 = new Point(r.Right, r.Top);
+            var p3 = new Point(r.Right - skew, r.Bottom);
+            var p4 = new Point(r.Left, r.Bottom);
+            return new[] { p1, p2, p3, p4 };
+        }
+    }
+}
