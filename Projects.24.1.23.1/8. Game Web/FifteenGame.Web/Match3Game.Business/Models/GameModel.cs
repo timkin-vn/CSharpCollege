9dc57ad0d60af440480dbbf1042dd484e064f3d5
misa47 <hayatotima@gmail.com>
2025-12-20 20:44:50 +0300

    3. Game Web start

---
+++ Projects.24.1.23.1/8. Game Web/FifteenGame.Web/Match3Game.Business/Models/GameModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match3Game.Business.Models
+{
+    public class GameModel
+    {
+        public const int RowCount = 8;
+        public const int ColumnCount = 8;
+        public const int GemTypeCount = 5; // фишки
+
+        private int[,] _cells = new int[RowCount, ColumnCount];
+
+        public int this[int row, int column]
+        {
+            get => _cells[row, column];
+            internal set => _cells[row, column] = value;
+        }
+
+        public GameModel()
+        {
+            GenerateInitialBoard();
+        }
+
+        private void GenerateInitialBoard()
+        {
+            var rnd = new Random();
+
+            for (int row = 0; row < RowCount; row++)
+            {
+                for (int column = 0; column < ColumnCount; column++)
+                {
+                    _cells[row, column] = rnd.Next(GemTypeCount);
+                }
+            }
+        }
+    }
+}
--- Projects.24.1.23.1/8. GameWeb/Match3GameWeb/Match3GameWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Match3GameWeb.MvcApplication" Language="C#" %>
