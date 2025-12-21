6ffc772ba3d0fbbc1a344341ee23f3f9d64f4109
misa47 <hayatotima@gmail.com>
2025-12-21 15:42:06 +0300

    3. png добавил

--- Projects.24.1.23.1/8. Game Web/FifteenGame.Web/FifteenGame.Web/Models/CellViewModel.cs
+++ Projects.24.1.23.1/8. Game Web/FifteenGame.Web/FifteenGame.Web/Models/CellViewModel.cs
@@ -14,5 +14,28 @@ namespace FifteenGame.Web.Models
         public int GemType { get; set; }
 
         public bool IsEmpty => GemType == 0;
+
+        public string ImageName
+        {
+            get
+            {
+                switch (GemType)
+                {
+                    case 1:
+                        return "crystal_green";
+                    case 2:
+                        return "crystal_red";
+                    case 3:
+                        return "crystal_violet";
+                    case 4:
+                        return "crystal_yellow";
+                    case 5:
+                        return "crystal_blue";
+                    default:
+                        return null;
+                }
+            }
+        }
+
     }
 }
