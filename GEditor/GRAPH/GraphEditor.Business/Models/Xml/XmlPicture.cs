69fedf6fd988868bd077656f044ffc7cdc861ac3
SerGo <egor.sergeev16@yandex.ru>
2025-12-22 15:28:48 +0300

    GrafEditor

---
+++ GEditor/GRAPH/GraphEditor.Business/Models/Xml/XmlPicture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace GraphEditor.Business.Models.Xml
+{
+    [Serializable]
+    [XmlRoot("Picture")]
+    public class XmlPicture
+    {
+        [XmlArray("Figures")]
+        [XmlArrayItem("Figure")]
+        public List<XmlRectangle> Rectangles { get; set; }
+    }
+}
