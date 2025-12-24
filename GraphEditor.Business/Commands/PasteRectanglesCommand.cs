adfa7ebf4d88260e334a3d069211c887254966d0
slend <slenderzet@gmail.com>
2025-12-24 16:01:24 +0300

    Refactor `PasteRectanglesCommand`: Convert to record-like class, simplify constructor, and streamline `Execute` and `Undo` methods for clarity and efficiency.

--- GraphEditor.Business/Commands/PasteRectanglesCommand.cs
+++ GraphEditor.Business/Commands/PasteRectanglesCommand.cs
@@ -2,32 +2,22 @@ using GraphEditor.Business.Models;
 
 namespace GraphEditor.Business.Commands;
 
-public class PasteRectanglesCommand : ICommand {
-    private readonly PictureModel _picture;
-    private readonly List<RectangleModel> _pastedRectangles;
-
-    public string Description => "Paste rectangles";
-
-    public PasteRectanglesCommand(PictureModel picture, IEnumerable<RectangleModel> rectangles) {
-        _picture = picture;
-        _pastedRectangles = rectangles.ToList();
-    }
+public class PasteRectanglesCommand(PictureModel picture, IEnumerable<RectangleModel> rectangles) : ICommand {
+    private readonly List<RectangleModel> _pastedRectangles = rectangles.ToList();
 
     public void Execute() {
-        foreach (var rect in _pastedRectangles) {
-            if (!_picture.Rectangles.Contains(rect)) {
-                _picture.Rectangles.Add(rect);
-            }
+        foreach (var rect in _pastedRectangles.Where(rect => !picture.Rectangles.Contains(rect))) {
+            picture.Rectangles.Add(rect);
         }
     }
 
     public void Undo() {
         foreach (var rect in _pastedRectangles) {
-            _picture.Rectangles.Remove(rect);
-            _picture.SelectedRectangleIds.Remove(rect.Id);
+            picture.Rectangles.Remove(rect);
+            picture.SelectedRectangleIds.Remove(rect.Id);
         }
-        if (_pastedRectangles.Any(r => r.Id == _picture.SelectedRectangle?.Id)) {
-            _picture.SelectedRectangle = null;
+        if (_pastedRectangles.Any(r => r.Id == picture.SelectedRectangle?.Id)) {
+            picture.SelectedRectangle = null;
         }
     }
 }
