4f7721e960617991eb17aee6d2dc92db88388fcf
slend <slenderzet@gmail.com>
2025-12-24 16:01:31 +0300

    Refactor `PictureModel`: Use `init` accessors for `Groups` property for immutability and consistency.

--- GraphEditor.Business/Models/PictureModel.cs
+++ GraphEditor.Business/Models/PictureModel.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-
 namespace GraphEditor.Business.Models;
 
 public class PictureModel {
     public IList<RectangleModel> Rectangles { get; init; } = new List<RectangleModel>();
 
-    public IList<GroupModel> Groups { get; set; } = new List<GroupModel>();
+    public IList<GroupModel> Groups { get; init; } = new List<GroupModel>();
 
     public RectangleModel? SelectedRectangle { get; set; }
     
