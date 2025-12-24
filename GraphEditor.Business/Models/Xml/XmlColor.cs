6fd0dfcde900d77056c386a2cb191af485b50232
slend <slenderzet@gmail.com>
2025-12-24 16:15:22 +0300

    Refactor `XmlColor`: Remove redundant `using` directives, simplify namespace declaration, and clean up formatting for improved clarity and consistency.

--- GraphEditor.Business/Models/Xml/XmlColor.cs
+++ GraphEditor.Business/Models/Xml/XmlColor.cs
@@ -1,16 +1,15 @@
-using System;
 using System.Xml.Serialization;
 
-namespace GraphEditor.Business.Models.Xml {
-    [Serializable]
-    public class XmlColor {
-        [XmlAttribute("Red")]
-        public byte Red { get; set; }
+namespace GraphEditor.Business.Models.Xml;
 
-        [XmlAttribute("Green")]
-        public byte Green { get; set; }
+[Serializable]
+public class XmlColor {
+    [XmlAttribute("Red")]
+    public byte Red { get; set; }
 
-        [XmlAttribute("Blue")]
-        public byte Blue { get; set; }
-    }
+    [XmlAttribute("Green")]
+    public byte Green { get; set; }
+
+    [XmlAttribute("Blue")]
+    public byte Blue { get; set; }
 }
