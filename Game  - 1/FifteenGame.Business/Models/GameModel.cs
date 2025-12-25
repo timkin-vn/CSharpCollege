ba26f685ce8177ff5223e4f3556dadcd3cb3569f
Fr1zer0k <catflash2006@gmail.com>
2025-12-25 23:44:06 +0300

    Add files via upload

---
+++ Game  - 1/FifteenGame.Business/Models/GameModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FifteenGame.Business.Models
+{
+    public class GameModel
+    {
+        public int Size { get; set; } = 9;
+        public int MinesCount { get; set; } = 10;
+
+        private Cell[,] _cells;
+
+        public Cell this[int row, int column]
+        {
+            get => _cells[row, column];
+            set => _cells[row, column] = value;
+        }
+
+        public int FlagsPlaced { get; set; }
+        public int CellsOpened { get; set; }
+        public bool GameOver { get; set; }
+        public bool GameWon { get; set; }
+
+        public void InitializeCells(int size)
+        {
+            Size = size;
+            _cells = new Cell[size, size];
+
+            for (int r = 0; r < size; r++)
+            {
+                for (int c = 0; c < size; c++)
+                {
+                    _cells[r, c] = new Cell();
+                }
+            }
+
+            FlagsPlaced = 0;
+            CellsOpened = 0;
+            GameOver = false;
+            GameWon = false;
+        }
+
+        public int GetTotalNonMineCells()
+        {
+            return (Size * Size) - MinesCount;
+        }
+    }
+
+    public class Cell
+    {
+        public bool IsMine { get; set; }
+        public bool IsOpened { get; set; }
+        public bool IsFlagged { get; set; }
+        public int MinesAround { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+    }
+}
