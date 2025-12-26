c1964d37e2a5c8033c2a0406036f312e9b3a56f1
slend <slenderzet@gmail.com>
2025-12-26 08:55:52 +0300

    Remove GameBoard, GameSettings, and GameActionResult
    Deleted GameBoard, GameSettings, and GameActionResult from the Minesweeper.Web.Business namespace. This likely reflects a refactor or migration of game logic to another location or a change in project structure.

--- remod3/Minesweeper.Web/Minesweeper.Web.Business/GameActionResult.cs
+++
@@ -1,5 +0,0 @@
-using Minesweeper.Business;
-
-namespace Minesweeper.Web.Business;
-
-public sealed record GameActionResult(IReadOnlyList<CellUpdate> Updates, bool GameOver, bool HasWon,  bool HitMine);
--- remod3/Minesweeper.Web/Minesweeper.Web.Business/GameSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace Minesweeper.Business;
-
-public sealed record GameSettings(int Rows, int Columns, int Mines) {
-    public static GameSettings Beginner() => new(9, 9, 10);
-    public static GameSettings Intermediate() => new(16, 16, 40);
-    public static GameSettings Expert() => new(16, 30, 99);
-}
