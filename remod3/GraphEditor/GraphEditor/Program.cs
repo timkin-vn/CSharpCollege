33343ef1cd5d9a80155fd62e46941d59f13bd9a3
slend <slenderzet@gmail.com>
2025-12-26 15:27:42 +0300

    Change `Program` class to `internal`, remove redundant comments, and adjust method access modifier.

--- remod3/GraphEditor/GraphEditor/Program.cs
+++ remod3/GraphEditor/GraphEditor/Program.cs
@@ -1,13 +1,8 @@
 namespace GraphEditor;
 
-static class Program {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
+internal static class Program {
     [STAThread]
     private static void Main() {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
         Application.Run(new GraphEditorForm());
     }
