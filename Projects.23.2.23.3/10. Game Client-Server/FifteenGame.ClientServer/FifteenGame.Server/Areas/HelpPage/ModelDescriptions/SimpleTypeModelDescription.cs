795f2f6fbe811de2fc826e44fa8cbbd76748d072
Тимкин Владимир <timkin_vn@mail.ru>
2026-02-12 16:10:43 +0700

    Очистка главной ветки

--- Projects.23.2.23.3/10. Game Client-Server/FifteenGame.ClientServer/FifteenGame.Server/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FifteenGame.Server.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Projects.24.1.23.1/8. Game Web/FifteenGame.Web/FifteenGame.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FifteenGame.Web.MvcApplication" Language="C#" %>
