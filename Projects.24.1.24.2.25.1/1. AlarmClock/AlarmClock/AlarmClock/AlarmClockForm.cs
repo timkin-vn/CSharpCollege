12c3c46549b7a68f6fb9f99b13f17e7f5fda177d
Ivan Konev <kvanyusha111@mail.ru>
2026-02-20 15:45:09 +0300

    alarmclock matquiz konev

--- Projects.24.1.24.2.25.1/1. AlarmClock/AlarmClock/AlarmClock/AlarmClockForm.cs
+++ Projects.24.1.24.2.25.1/1. AlarmClock/AlarmClock/AlarmClock/AlarmClockForm.cs
@@ -16,7 +16,8 @@ namespace AlarmClock
     public partial class AlarmClockForm : Form
     {
         private AlarmState _alarmState = new AlarmState();
-
+        // таймер плодил окна, пришлось добавить этот флаг
+        private bool _isQuizOpen = false;
         public AlarmClockForm()
         {
             InitializeComponent();
@@ -26,27 +27,72 @@ namespace AlarmClock
         {
             DisplayLabel.Text = DateTime.Now.ToLongTimeString();
 
-            if (!_alarmState.IsAlarmActive)
-            {
-                return;
-            }
-
-            if (!_alarmState.IsAwakeActivated && 
-                DateTime.Now.Hour == _alarmState.AlarmTime.Hour &&
-                DateTime.Now.Minute == _alarmState.AlarmTime.Minute)
-            {
-                _alarmState.IsAwakeActivated = true;
-
-                var awakeForm = new AwakeForm();
-                awakeForm.AlarmState = _alarmState;
-                awakeForm.FormClosed += AwakeForm_FormClosed;
-
-                awakeForm.ShowDialog();
-            }
+            if (!_alarmState.IsAlarmActive) return;
 
             if (_alarmState.IsSoundActive && _alarmState.IsAwakeActivated)
             {
                 SystemSounds.Beep.Play();
+            }
+
+    
+            if (!_alarmState.IsAwakeActivated &&
+                DateTime.Now.Hour == _alarmState.AlarmTime.Hour &&
+                DateTime.Now.Minute == _alarmState.AlarmTime.Minute &&
+                !_isQuizOpen)
+            {
+                _isQuizOpen = true;
+
+                try
+                {
+                    // рандомные числа для примера
+                    Random rand = new Random();
+                    int a = rand.Next(10, 50);
+                    int b = rand.Next(10, 50);
+                    int correctAnswer = a + b;
+
+                    using (Form quizForm = new Form() { Width = 300, Height = 150, Text = "ПРОСЫПАЙСЯ!", StartPosition = FormStartPosition.CenterScreen, FormBorderStyle = FormBorderStyle.FixedDialog, TopMost = true })
+                    {
+                        Label textLabel = new Label() { Left = 20, Top = 20, Width = 250, Text = $"Реши пример, чтобы выключить звук: {a} + {b} = ?" };
+                        TextBox inputBox = new TextBox() { Left = 20, Top = 50, Width = 100 };
+                        Button confirmBtn = new Button() { Text = "ОК", Left = 130, Top = 48, DialogResult = DialogResult.OK };
+
+                        quizForm.Controls.Add(textLabel);
+                        quizForm.Controls.Add(inputBox);
+                        quizForm.Controls.Add(confirmBtn);
+                        quizForm.AcceptButton = confirmBtn;
+
+                        
+                        Timer beepTimer = new Timer();
+                        beepTimer.Interval = 1000; 
+                        beepTimer.Tick += (s, ev) => { SystemSounds.Beep.Play(); };
+                        beepTimer.Start();
+                        
+
+                        if (quizForm.ShowDialog() == DialogResult.OK)
+                        {
+                            beepTimer.Stop(); 
+                            if (int.TryParse(inputBox.Text, out int userAnswer) && userAnswer == correctAnswer)
+                            {
+                                _alarmState.IsAwakeActivated = true;
+                                var awakeForm = new AwakeForm { AlarmState = _alarmState };
+                                awakeForm.FormClosed += AwakeForm_FormClosed;
+                                awakeForm.ShowDialog();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Неверно! Слушай писк дальше!");
+                            }
+                        }
+                        else
+                        {
+                            beepTimer.Stop();
+                        }
+                    }
+                }
+                finally
+                {
+                    _isQuizOpen = false;
+                }
             }
         }
 
