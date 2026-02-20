0104f103426c53a37a4dce5152582d674de9b67a
tim248200 <kuznetsovtimofey24@gmail.com>
2026-02-20 18:58:27 +0300

    Кузнецов Тимофей Рпо 24/1 (Проект будильник)

---
+++ Кузнецов_рпо_24_1/AlarmClock_Кузнецов_24_1/AlarmClock/Forms/AwakeForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AlarmClock.Forms
+{
+    partial class AwakeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AwakePictureBox = new System.Windows.Forms.PictureBox();
+            this.AwakeButton = new System.Windows.Forms.Button();
+            this.AwakeTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.AwakePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AwakePictureBox
+            // 
+            this.AwakePictureBox.Location = new System.Drawing.Point(12, 12);
+            this.AwakePictureBox.Name = "AwakePictureBox";
+            this.AwakePictureBox.Size = new System.Drawing.Size(573, 333);
+            this.AwakePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.AwakePictureBox.TabIndex = 0;
+            this.AwakePictureBox.TabStop = false;
+            // 
+            // AwakeButton
+            // 
+            this.AwakeButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.AwakeButton.Location = new System.Drawing.Point(240, 351);
+            this.AwakeButton.Name = "AwakeButton";
+            this.AwakeButton.Size = new System.Drawing.Size(113, 23);
+            this.AwakeButton.TabIndex = 1;
+            this.AwakeButton.Text = "Я проснулся";
+            this.AwakeButton.UseVisualStyleBackColor = true;
+            // 
+            // AwakeTimer
+            // 
+            this.AwakeTimer.Enabled = true;
+            this.AwakeTimer.Interval = 5000;
+            this.AwakeTimer.Tick += new System.EventHandler(this.AwakeTimer_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отложить будильник на 5 минут";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AwakeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 386);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AwakeButton);
+            this.Controls.Add(this.AwakePictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AwakeForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AwakeForm_FormClosed);
+            this.Load += new System.EventHandler(this.AwakeForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.AwakePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox AwakePictureBox;
+        private System.Windows.Forms.Button AwakeButton;
+        private System.Windows.Forms.Timer AwakeTimer;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Кузнецов_рпо_24_1/AlarmClock_Кузнецов_24_1/AlarmClock/Models/AlarmState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock.Models
+{
+    public class AlarmState
+    {
+        public DateTime AlarmTime { get; set; }
+
+        public string AlarmMessage { get; set; }
+
+        public bool IsAlarmActive { get; set; }
+
+        public bool IsSoundActive { get; set; }
+
+        public bool IsAwakeActivated { get; set; }
+
+        public bool IsSnoozeEnabled { get; set; }
+
+        public void Snooze()
+        {
+            AlarmTime = DateTime.Now.AddMinutes(5);
+            IsAwakeActivated = false;
+        }
+    }
+}
