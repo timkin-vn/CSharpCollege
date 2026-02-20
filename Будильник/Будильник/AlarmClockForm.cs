9b444429f1544180c1a0e59f86a9b74b54ae8037
kotendru <kotov12345678900@gmail.com>
2026-02-20 19:54:33 +0300

    Add files via upload

---
+++ Будильник/Будильник/AlarmClockForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Будильник.Forms;
+using Будильник.Models;
+
+namespace Будильник
+{
+
+
+    public partial class AlarmClockForm : Form
+    {
+        private AlarmState _alarmState = new AlarmState();
+
+
+        public AlarmClockForm()
+        {
+            InitializeComponent();
+            UpdateControls();
+        }
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            DisplayLabel.Text = DateTime.Now.ToLongTimeString();
+
+            if (!_alarmState.IsAlarmActive)
+            {
+                return;
+            }
+
+            if (!_alarmState.IsAwakeActivated &&
+                DateTime.Now.Hour == _alarmState.AlarmTime.Hour &&
+                DateTime.Now.Minute == _alarmState.AlarmTime.Minute)
+            {
+                _alarmState.IsAwakeActivated = true;
+
+                var awakeForm = new AwakeForm();
+                awakeForm.AlarmState = _alarmState;
+
+                awakeForm.FormClosed += AwakeForm_FormClosed;
+
+                awakeForm.ShowDialog();
+            }
+
+            if (_alarmState.IsSoundActive && _alarmState.IsAwakeActivated)
+            {
+                SystemSounds.Beep.Play();
+            }
+        }
+        private void AwakeForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ((Form)sender).FormClosed -= AwakeForm_FormClosed;
+            UpdateControls();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            var form = new AboutForm();
+            form.ShowDialog();
+        }
+
+        private void SettingsButton_Click(object sender, EventArgs e)
+        {
+            var form = new SettingsForm();
+            form.AlarmState = _alarmState;
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                UpdateControls();
+            }
+        }
+        private void UpdateControls()
+        {
+            if (_alarmState.IsAlarmActive)
+            {
+                Text = $"Будильник (ожидает срабатывания в {_alarmState.AlarmTime.ToShortTimeString()})";
+            }
+            else
+            {
+                Text = "Будильник";
+            }
+        }
+
+        private void DisplayLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Будильник/Будильник/Forms/AboutForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Будильник.Forms
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Будильник";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(21, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(326, 61);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Для Windows";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(25, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(322, 60);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Версия 0.1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(96, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(37, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(263, 79);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "(с) 2026";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 380);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.Text = "AboutForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+    }
+}
