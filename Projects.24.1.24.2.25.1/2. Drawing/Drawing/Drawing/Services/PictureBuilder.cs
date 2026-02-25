b4aab2d9c7269b03cf2a2799a6c54e50a1994537
Тимкин Владимир <timkin_vn@mail.ru>
2026-02-25 14:27:53 +0700

    Создан проект Графика

---
+++ Projects.24.1.24.2.25.1/2. Drawing/Drawing/Drawing/Services/PictureBuilder.cs
@@ -0,0 +1,76 @@
+﻿using Drawing.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing.Services
+{
+    internal class PictureBuilder
+    {
+        public RectangleModel SourceBounds { get; } = new RectangleModel
+        {
+            X = -5,
+            Y = -1,
+            Width = 19,
+            Height = 12,
+        };
+
+        public void DrawPicture(Painter painter)
+        {
+            var mainPen = new Pen(Color.Black, 3);
+            var wheelBrush = Brushes.LightGray;
+
+            var rect = new RectangleModel { X = -1, Y = 0, Width = 2, Height = 2, };
+            painter.DrawEllipse(wheelBrush, mainPen, rect);
+
+            rect.X = 2;
+            painter.DrawEllipse(wheelBrush, mainPen, rect);
+
+            rect.X = 5;
+            painter.DrawEllipse(wheelBrush, mainPen, rect);
+
+            rect.X = 8;
+            painter.DrawEllipse(wheelBrush, mainPen, rect);
+
+            var bodyBrush = Brushes.DarkGray;
+
+            rect = new RectangleModel { X = 0, Y = 2, Width = 12, Height = 4, };
+            painter.DrawRectangle(bodyBrush, mainPen, rect);
+
+            rect = new RectangleModel { X = -4, Y = 2, Width = 4, Height = 8, };
+            painter.DrawRectangle(bodyBrush, mainPen, rect);
+
+            var windowBrush = Brushes.LightSkyBlue;
+
+            rect = new RectangleModel { X = -2, Y = 6, Width = 1.5, Height = 3, };
+            painter.DrawRectangle(windowBrush, mainPen, rect);
+
+            rect = new RectangleModel { X = 11, Y = 2, Width = 2, Height = 4, };
+            painter.DrawPie(bodyBrush, mainPen, rect, 270, 180);
+
+            var sweeperBrush = Brushes.Red;
+            var sweeperPoints = new[]
+            {
+                new PointModel { X = 11, Y = 2, },
+                new PointModel { X = 12, Y = 2, },
+                new PointModel { X = 13, Y = 0.5, },
+                new PointModel { X = 11, Y = 0.5, },
+            };
+
+            painter.DrawPolygon(sweeperBrush, mainPen, sweeperPoints);
+
+            var pipePoints = new[]
+            {
+                new PointModel { X = 8.5, Y = 9, },
+                new PointModel { X = 10.5, Y = 9, },
+                new PointModel { X = 10, Y = 6, },
+                new PointModel { X = 9, Y = 6, },
+            };
+
+            painter.DrawPolygon(bodyBrush, mainPen, pipePoints);
+        }
+    }
+}
---
+++ Projects.24.3.24.4/2. Drawing/Drawing/Drawing/ViewServices/Painter.cs
@@ -0,0 +1,102 @@
+﻿using Drawing.Models;
+using Drawing.Services;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing.ViewServices
+{
+    internal class Painter
+    {
+        private Graphics _graphics;
+
+        private Scaler _scaler;
+
+        private PictureBuilder _builder = new PictureBuilder();
+
+        public void Paint(Rectangle bounds, Graphics g)
+        {
+            _scaler = new Scaler
+            {
+                TargetBounds = bounds,
+                SourceBounds = _builder.SourceBounds,
+            };
+
+            _scaler.Initialize();
+
+            _graphics = g;
+
+            _builder.DrawPicture(this);
+
+            _graphics = null;
+        }
+
+        public void DrawEllipse(Pen pen, Brush brush, RectangleModel rectangleModel)
+        {
+            var rectangle = _scaler.Scale(rectangleModel);
+
+            if (brush != null)
+            {
+                _graphics.FillEllipse(brush, rectangle);
+            }
+
+            if (pen != null)
+            {
+                _graphics.DrawEllipse(pen, rectangle);
+            }
+        }
+
+        public void DrawRectangle(Pen pen, Brush brush, RectangleModel rectangleModel)
+        {
+            var rectangle = _scaler.Scale(rectangleModel);
+
+            if (brush != null)
+            {
+                _graphics.FillRectangle(brush, rectangle);
+            }
+
+            if (pen != null)
+            {
+                _graphics.DrawRectangle(pen, rectangle);
+            }
+        }
+
+        public void DrawPie(Pen pen, Brush brush, RectangleModel rectangleModel, double startAngle, double sweepAngle)
+        {
+            var rectangle = _scaler.Scale(rectangleModel);
+
+            if (brush != null)
+            {
+                _graphics.FillPie(brush, rectangle, (float)startAngle, (float)sweepAngle);
+            }
+
+            if (pen != null)
+            {
+                _graphics.DrawPie(pen, rectangle, (float)startAngle, (float)sweepAngle);
+            }
+        }
+
+        public void DrawPolygon(Pen pen, Brush brush, PointModel[] pointModels)
+        {
+            //var points = new Point[pointModels.Length];
+            //for (int i = 0; i < pointModels.Length; i++)
+            //{
+            //    points[i] = _scaler.Scale(pointModels[i]);
+            //}
+            var points = pointModels.Select(pm => _scaler.Scale(pm)).ToArray();
+
+            if (brush != null)
+            {
+                _graphics.FillPolygon(brush, points);
+            }
+
+            if (pen != null)
+            {
+                _graphics.DrawPolygon(pen, points);
+            }
+        }
+    }
+}
