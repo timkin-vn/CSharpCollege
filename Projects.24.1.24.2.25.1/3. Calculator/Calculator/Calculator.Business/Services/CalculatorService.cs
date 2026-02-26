a4117fb22e1b7c68cf8f4bf85380e9481f4ed167
Тимкин Владимир <timkin_vn@mail.ru>
2026-02-26 17:56:04 +0700

    Создан проект Калькулятор для групп 24/1,24/2, 25/1

---
+++ Projects.24.1.24.2.25.1/3. Calculator/Calculator/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,76 @@
+﻿using Calculator.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void PressDigit(CalculatorModel calculatorModel, string digitString)
+        {
+            if (!int.TryParse(digitString, out var digit))
+            {
+                return;
+            }
+
+            if (!calculatorModel.IsLastDigitPressed)
+            {
+                calculatorModel.RegisterX = 0;
+            }
+
+            calculatorModel.RegisterX = calculatorModel.RegisterX * 10 + digit;
+            calculatorModel.IsLastDigitPressed = true;
+        }
+
+        public void PressClear(CalculatorModel calculatorModel)
+        {
+            calculatorModel.RegisterX = 0;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        public void MoveXToY(CalculatorModel calculatorModel)
+        {
+            calculatorModel.RegisterY = calculatorModel.RegisterX;
+        }
+
+        private void CompleteOperation(CalculatorModel calculatorModel)
+        {
+            switch (calculatorModel.OperationCode)
+            {
+                case "+":
+                    calculatorModel.RegisterX = calculatorModel.RegisterX + calculatorModel.RegisterY;
+                    break;
+
+                case "-":
+                    calculatorModel.RegisterX = calculatorModel.RegisterY - calculatorModel.RegisterX;
+                    break;
+
+                case "*":
+                    calculatorModel.RegisterX = calculatorModel.RegisterX * calculatorModel.RegisterY;
+                    break;
+
+                case "/":
+                    calculatorModel.RegisterX = calculatorModel.RegisterY / calculatorModel.RegisterX;
+                    break;
+            }
+        }
+
+        public void PressOperation(CalculatorModel calculatorModel, string operationCode)
+        {
+            CompleteOperation(calculatorModel);
+
+            MoveXToY(calculatorModel);
+            calculatorModel.OperationCode = operationCode;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        public void PressEqual(CalculatorModel calculatorModel)
+        {
+            CompleteOperation(calculatorModel);
+            calculatorModel.IsLastDigitPressed = false;
+        }
+    }
+}
---
+++ Projects.24.1.24.2.25.1/3. Calculator/Calculator/Calculator/CalculatorForm.cs
@@ -0,0 +1,61 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private CalculatorModel _calculatorModel = new CalculatorModel();
+
+        private CalculatorService _service = new CalculatorService();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, EventArgs e)
+        {
+            _service.PressDigit(_calculatorModel, ((Button)sender).Text);
+            DisplayValue();
+        }
+
+        private void DisplayValue()
+        {
+            DisplayLabel.Text = _calculatorModel.RegisterX.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            _service.PressClear(_calculatorModel);
+            DisplayValue();
+        }
+
+        private void MoveXToYButton_Click(object sender, EventArgs e)
+        {
+            _service.MoveXToY(_calculatorModel);
+            DisplayValue();
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            _service.PressOperation(_calculatorModel, ((Button)sender).Text);
+            DisplayValue();
+        }
+
+        private void EqualButton_Click(object sender, EventArgs e)
+        {
+            _service.PressEqual(_calculatorModel);
+            DisplayValue();
+        }
+    }
+}
