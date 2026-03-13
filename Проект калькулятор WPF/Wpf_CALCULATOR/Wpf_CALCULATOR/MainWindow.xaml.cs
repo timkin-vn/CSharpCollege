8f569ac4789af03a825640d9738fa13813284b23
tim248200 <kuznetsovtimofey24@gmail.com>
2026-03-13 23:40:40 +0300

    Add files via upload

---
+++ Проект калькулятор WPF/Wpf_CALCULATOR/Wpf_CALCULATOR/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf_CALCULATOR.ViewModels;
+
+namespace Wpf_CALCULATOR
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, RoutedEventArgs e)
+        {
+            var digitString = ((Button)sender).Content as string;
+            ViewModel.PressDigit(digitString);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressClear();
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var operationCode = ((Button)sender).Content as string;
+            ViewModel.PressOperation(operationCode);
+        }
+
+        private void SquareButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressSquare();
+        }
+
+        private void CommaButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressComma();
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressEqual();
+        }
+    }
+}
---
+++ Проект калькулятор WPF/Wpf_CALCULATOR/Wpf_CALCULATOR/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_CALCULATOR.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private CalculatorModel _calculatorModel = new CalculatorModel();
+
+        private CalculatorService _calculatorService = new CalculatorService();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Result => _calculatorModel.RegisterX.ToString();
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void PressDigit(string digitString)
+        {
+            _calculatorService.PressDigit(_calculatorModel, digitString);
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void PressClear()
+        {
+            _calculatorService.PressClear(_calculatorModel);
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void PressOperation(string operationCode)
+        {
+            _calculatorService.PressOperation(_calculatorModel, operationCode);
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void PressSquare()
+        {
+            _calculatorService.PressSquare(_calculatorModel);
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void PressComma()
+        {
+            if (!_calculatorModel.IsDrob)
+            {
+                _calculatorModel.IsDrob = true;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public void PressEqual()
+        {
+            _calculatorService.PressEqual(_calculatorModel);
+            OnPropertyChanged(nameof(Result));
+        }
+
+    }
+}
