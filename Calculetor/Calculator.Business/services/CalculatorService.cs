f8542a84ca1aea5f1c5ec18f8f93e389a221bf01
Joseph Sans <arhiva.net1@gmail.com>
2026-03-18 12:19:44 +0300

    Кирилл Дятлов РПО 24/1

---
+++ Calculetor/Calculator.Business/services/CalculatorService.cs
@@ -0,0 +1,114 @@
+﻿using Calculator.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void PressDigit(CalculatorModel calculatorModel, string digitString)
+        {
+            if (!int.TryParse(digitString, out var digit))
+                return;
+
+            if (!calculatorModel.IsLastDigitPressed)
+            {
+                calculatorModel.RegisterX = 0;
+                calculatorModel.IsDecimalMode = false;
+                calculatorModel.DecimalFactor = 0.1;
+            }
+
+            if (!calculatorModel.IsDecimalMode)
+            {
+                // обычное число
+                calculatorModel.RegisterX = calculatorModel.RegisterX * 10 + digit;
+            }
+            else
+            {
+                // дробная часть
+                calculatorModel.RegisterX += digit * calculatorModel.DecimalFactor;
+                calculatorModel.DecimalFactor /= 10;
+            }
+
+            calculatorModel.IsLastDigitPressed = true;
+        }
+
+        public void PressClear(CalculatorModel calculatorModel)
+        {
+            calculatorModel.RegisterX = 0;
+            calculatorModel.IsLastDigitPressed = false;
+
+            calculatorModel.IsDecimalMode = false;
+            calculatorModel.DecimalFactor = 0.1;
+        }
+
+        public void MoveXToY(CalculatorModel calculatorModel)
+        {
+            calculatorModel.RegisterY = calculatorModel.RegisterX;
+        }
+
+        private void CompleteOperation(CalculatorModel calculatorModel)
+        {
+            switch (calculatorModel.OperationCode)
+            {
+                case "+":
+                    calculatorModel.RegisterX = calculatorModel.RegisterX + calculatorModel.RegisterY;
+                    break;
+
+                case "-":
+                    calculatorModel.RegisterX = calculatorModel.RegisterY - calculatorModel.RegisterX;
+                    break;
+
+                case "*":
+                    calculatorModel.RegisterX = calculatorModel.RegisterX * calculatorModel.RegisterY;
+                    break;
+
+                case "/":
+                    calculatorModel.RegisterX = calculatorModel.RegisterY / calculatorModel.RegisterX;
+                    break;
+            }
+        }
+
+        public void PressOperation(CalculatorModel calculatorModel, string operationCode)
+        {
+            CompleteOperation(calculatorModel);
+
+            MoveXToY(calculatorModel);
+            calculatorModel.OperationCode = operationCode;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        public void PressEqual(CalculatorModel calculatorModel)
+        {
+            CompleteOperation(calculatorModel);
+            calculatorModel.IsLastDigitPressed = false;
+        }
+        public void PressDecimal(CalculatorModel calculatorModel)
+        {
+            if (!calculatorModel.IsDecimalMode)
+            {
+                calculatorModel.IsDecimalMode = true;
+                calculatorModel.DecimalFactor = 0.1;
+            }
+        }
+        public void Square(CalculatorModel calculatorModel)
+        {
+            calculatorModel.RegisterX = calculatorModel.RegisterX * calculatorModel.RegisterX;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        public void Sqrt(CalculatorModel calculatorModel)
+        {
+            if (calculatorModel.RegisterX < 0)
+            {
+                throw new Exception("Нельзя извлечь корень из отрицательного числа");
+            }
+
+            calculatorModel.RegisterX = Math.Sqrt(calculatorModel.RegisterX);
+            calculatorModel.IsLastDigitPressed = false;
+        }
+    }
+}
---
+++ Calculetor/Calculetor/Calculetor.cs
@@ -0,0 +1,101 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculetor
+{
+    public partial class Calculetor : Form
+    {
+        private const string ImFolderName = "fons";
+
+        private string[] ImFileName;
+
+        private int indexer;
+
+        private CalculatorModel _calculatorModel = new CalculatorModel();
+
+        private CalculatorService _service = new CalculatorService();
+
+        public Calculetor()
+        {
+            InitializeComponent();
+        }
+        private void Ricerd()
+        {
+            ImFileName = Directory.EnumerateFiles(ImFolderName).ToArray();
+            indexer = 0;
+            pictureBox1.Load(ImFileName[indexer]);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ricerd();
+        }
+
+        private void DigitButton_Click(object sender, EventArgs e)
+        {
+            _service.PressDigit(_calculatorModel, ((Button)sender).Text);
+            DisplayValue();
+        }
+
+        private void DisplayValue()
+        {
+            DisplayLabel.Text = _calculatorModel.RegisterX.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            _service.PressClear(_calculatorModel);
+            DisplayValue();
+        }
+
+        private void MoveXToYButton_Click(object sender, EventArgs e)
+        {
+            _service.MoveXToY(_calculatorModel);
+            DisplayValue();
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            _service.PressOperation(_calculatorModel, ((Button)sender).Text);
+            DisplayValue();
+        }
+
+        private void EqualButton_Click(object sender, EventArgs e)
+        {
+            _service.PressEqual(_calculatorModel);
+            DisplayValue();
+        }
+        private void DecimalButton_Click(object sender, EventArgs e)
+        {
+            _service.PressDecimal(_calculatorModel);
+            DisplayValue();
+        }
+        private void SquareButton_Click(object sender, EventArgs e)
+        {
+            _service.Square(_calculatorModel);
+            DisplayValue();
+        }
+        private void SqrtButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _service.Sqrt(_calculatorModel);
+                DisplayValue();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
