3954f62e0d55ad137805401e76a8d0653a6700cd
Тимкин Владимир <timkin_vn@mail.ru>
2026-03-24 18:09:17 +0700

    Проект Картотека для групп 24/3, 24/4

---
+++ Projects.24.3.24.4/5. CardFile/CardFile/CardFile.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,163 @@
+﻿using CardFile.Business.Models;
+using CardFile.Business.Services;
+using CardFile.Common.Infrastructure;
+using CardFile.Wpf.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFile.Wpf.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private CardFileService _service = new CardFileService();
+
+        public ObservableCollection<CardViewModel> Cards { get; } = new ObservableCollection<CardViewModel>();
+
+        public CardViewModel SelectedCard { get; set; }
+
+        public bool IsEditButtonEnabled => SelectedCard != null;
+
+        public bool IsDeleteButtonEnabled => SelectedCard != null;
+
+        public MainWindowViewModel()
+        {
+            MapperInitialization.PreRegister();
+        }
+
+        public void WindowLoaded()
+        {
+            Mapping.Initialize();
+
+            LoadAllData();
+        }
+
+        public CardViewModel GetSelectedCard()
+        {
+            return SelectedCard;
+        }
+
+        public CardViewModel GetNewCard()
+        {
+            return new CardViewModel
+            {
+                BirthDate = new DateTime(2000, 6, 15),
+                EmploymentDate = new DateTime(2020, 6, 15),
+            };
+        }
+
+        public void SaveEditedCard(CardViewModel card)
+        {
+            var index = Cards.ToList().FindIndex(c => c.Id == card.Id);
+
+            if (index < 0)
+            {
+                throw new Exception("Карточка с таким Id не существует");
+            }
+
+            var id =_service.Save(ToBusiness(card));
+
+            if (id < 0)
+            {
+                Cards.RemoveAt(index);
+            }
+            else
+            {
+                Cards[index].LoadViewModel(card);
+            }
+        }
+
+        public void SaveNewCard(CardViewModel card)
+        {
+            var newCard = new CardViewModel();
+            newCard.LoadViewModel(card);
+
+            var id = _service.Save(ToBusiness(card));
+
+            if (id < 0)
+            {
+                return;
+            }
+
+            newCard.Id = id;
+            Cards.Add(newCard);
+        }
+
+        public void DeleteSelectedCard()
+        {
+            if (SelectedCard == null)
+            {
+                return;
+            }
+
+            _service.Delete(SelectedCard.Id);
+            var index = Cards.ToList().FindIndex(c => c.Id == SelectedCard.Id);
+
+            if (index < 0)
+            {
+                throw new Exception("Карточка с таким Id не существует");
+            }
+
+            Cards.RemoveAt(index);
+            SelectedCard = null;
+
+            OnPropertyChanged(nameof(SelectedCard));
+        }
+
+        public void SelectionChanged()
+        {
+            OnPropertyChanged(nameof(IsDeleteButtonEnabled));
+            OnPropertyChanged(nameof(IsEditButtonEnabled));
+        }
+
+        private void LoadAllData()
+        {
+            var cards = _service.GetAll();
+            Cards.Clear();
+            foreach (var card in cards)
+            {
+                Cards.Add(ToViewModel(card));
+            }
+        }
+
+        private CardViewModel ToViewModel(Card card)
+        {
+            return Mapping.Mapper.Map<CardViewModel>(card);
+            //return new CardViewModel
+            //{
+            //    Id = card.Id,
+            //    FirstName = card.FirstName,
+            //    LastName = card.LastName,
+            //    MiddleName = card.MiddleName,
+            //    BirthDate = card.BirthDate,
+            //    Department = card.Department,
+            //    Position = card.Position,
+            //    EmploymentDate = card.EmploymentDate,
+            //    DismissalDate = card.DismissalDate,
+            //    Salary = card.Salary,
+            //};
+        }
+
+        private Card ToBusiness(CardViewModel card)
+        {
+            return Mapping.Mapper.Map<Card>(card);
+            //return new Card
+            //{
+            //    Id = card.Id,
+            //    FirstName = card.FirstName,
+            //    LastName = card.LastName,
+            //    MiddleName = card.MiddleName,
+            //    BirthDate = card.BirthDate,
+            //    Department = card.Department,
+            //    Position = card.Position,
+            //    EmploymentDate = card.EmploymentDate,
+            //    DismissalDate = card.DismissalDate,
+            //    Salary = card.Salary,
+            //};
+        }
+    }
+}
