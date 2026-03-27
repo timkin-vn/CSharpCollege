9dda4adf686918464f4754f31485bc599cee4ba7
Nikita Mokhin <mokhin.nikita@yandex.ru>
2026-03-27 12:44:09 +0300

    Мой проект

---
+++ Projects.24.1.24.2.25.1/MokhinNikita/CardFile/CardFile.DataStore/FileDataAccess/FileManagers/BinaryFileManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardFile.DataStore.DataCollection;
+using CardFile.DataStore.Dtos;
+
+namespace CardFile.DataStore.FileDataAccess.FileManagers
+{
+    public class BinaryFileManager : IFileManager
+    {
+        public void OpenFromFile(string fileName, CardCollection collection)
+        {
+            using(var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using(var reader=new BinaryReader(fs))
+                {
+                    var records = new List<CardDto>();
+                    try
+                    {
+                        while (reader.BaseStream.Position < reader.BaseStream.Length)
+                        {
+                            var card = new CardDto();
+                            card.Id = reader.ReadInt32();
+                            card.FirstName = reader.ReadString();
+                            card.MiddleName = reader.ReadString();
+                            card.LastName = reader.ReadString();
+                            var ticks = reader.ReadInt64();
+                            card.BirthDate = new DateTime(ticks);
+                            card.Department = reader.ReadString();
+                            card.Position = reader.ReadString();
+                            ticks = reader.ReadInt64();
+                            card.EmploymentDate = new DateTime(ticks);
+                            var isDicimalDatePresent = reader.ReadBoolean();
+                            if (isDicimalDatePresent)
+                            {
+                                ticks = reader.ReadInt64();
+                                card.DismissalDate = new DateTime(ticks);
+                            }
+                            else
+                            {
+                                card.DismissalDate = null;
+                            }
+                            card.Salary = reader.ReadDecimal();
+
+                            records.Add(card);
+                        }
+                    }
+                    catch
+                    {
+                        throw new Exception($"Неверные данные в файле {fileName}");
+                    }
+                    collection.ReplaceAll(records);
+                }
+            }
+        }
+
+        public void SaveToFile(string fileName, CardCollection collection)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                using (var writer = new BinaryWriter(fs))
+                {
+                    foreach (var item in collection.GetAll())
+                    {
+                        writer.Write(item.Id);
+                        writer.Write(item.FirstName);
+                        writer.Write(item.MiddleName);
+                        writer.Write(item.LastName);
+                        writer.Write(item.BirthDate.Ticks);
+                        writer.Write(item.Department);
+                        writer.Write(item.Position);
+                        writer.Write(item.EmploymentDate.Ticks);
+                        writer.Write(item.DismissalDate.HasValue);
+                        if(item.DismissalDate.HasValue)
+                        {
+                            writer.Write(item.DismissalDate.Value.Ticks);
+                        }
+                        writer.Write(item.Salary);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Projects.24.1.24.2.25.1/MokhinNikita/CardFile/CardFile.WPF/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CardFile.WPF.ViewModels;
+using CardFile.WPF.Views;
+using Microsoft.Win32;
+
+namespace CardFile.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string FILE_FILTER = "Текстовые файлы|*.txt|Файлы CSV|*.csv|Двоичный файл картотеки|*.cardbin" +
+            "|XML-файлы картотеки|*.cardxml|JSON-файл картотеки|*.cardjson|ZIP-архив картотеки|*.cardzip";
+
+        public MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if(ViewModel.Changed)
+            {
+                var warningDialog = GetWarningDialog();
+                switch(warningDialog)
+                {
+                    case MessageBoxResult.Yes:
+                        SaveFile();
+                        break;
+                    case MessageBoxResult.Cancel:
+                        return;
+                }
+            }
+            var dialog = new OpenFileDialog()
+            {
+                Filter = FILE_FILTER,
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    ViewModel.OpenFormFile(dialog.FileName);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void Save_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void SaveFile()
+        {
+            if (string.IsNullOrEmpty(ViewModel.FileName))
+            {
+                DoSaveAs();
+            }
+            else
+            {
+                ViewModel.SaveToFile();
+            }
+        }
+
+        private void SaveAs_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            DoSaveAs();
+        }
+        private void DoSaveAs()
+        {
+            var dialog = new SaveFileDialog()
+            {
+                Filter = FILE_FILTER
+            };
+            if(dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    ViewModel.SaveToFile(dialog.FileName);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new CardEditWindow();
+            var card = ViewModel.GetNewCard();
+            window.ViewModel.LoadViewModel(card);
+            if (window.ShowDialog() != true)
+            {
+                return;
+            }
+            ViewModel.SaveNewCard(window.ViewModel);
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var card = ViewModel.GetSelectedCard();
+            if(card == null)
+            {
+                return;
+            }
+            var window = new CardEditWindow();
+            window.ViewModel.LoadViewModel(card);
+            if(window.ShowDialog() != true)
+            {
+                return;
+            }
+            ViewModel.SaveEditedCard(window.ViewModel);
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.DeleteSelectedCard();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel.WindowLoaded();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            ViewModel.Initialized();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ViewModel.SelectionCard();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(ViewModel.Changed)
+            {
+                var dialog = GetWarningDialog();
+                switch (dialog)
+                {
+                    case MessageBoxResult.Yes:
+                        SaveFile();
+                        break;
+                    case MessageBoxResult.Cancel:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+        }
+
+        private static MessageBoxResult GetWarningDialog()
+        {
+            return MessageBox.Show("Сохранить изменения", "Предупреждение", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+        }
+    }
+}
