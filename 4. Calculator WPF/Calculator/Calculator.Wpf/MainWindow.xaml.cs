b097f80aee08c4ac4eea77c4ae3eb8ce48ff8827
Fire_Fox <Danila.Shesterikov28@gmail.com>
2026-04-01 00:42:27 +0300

    Add files via upload

---
+++ 4. Calculator WPF/Calculator/Calculator.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Calculator.Wpf.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, RoutedEventArgs e)
+        {
+            var digit = ((Button)sender).Content.ToString();
+            ViewModel.PressDigit(digit);
+        }
+
+        private void DecimalButton_Click(object sender, RoutedEventArgs e)
+        {
+            var separator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            ViewModel.PressDecimal(separator);
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = ((Button)sender).Content.ToString();
+            ViewModel.PressOperation(operation);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressClear();
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressEqual();
+        }
+
+        private void SqrtButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressSqrt();
+        }
+
+        private void SqrButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressSqr();
+        }
+    }
+}
---
+++ 4. Calculator WPF/Calculator/Calculator.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Wpf.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly CalculatorModel _model = new CalculatorModel();
+        private readonly CalculatorService _service = new CalculatorService();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Result => _model.RegisterX.ToString(CultureInfo.CurrentCulture);
+
+        public void PressDigit(string digit) => Update(() => _service.PressDigit(_model, digit));
+
+        public void PressDecimal(string separator) => Update(() => _service.PressDecimal(_model, separator));
+
+        public void PressOperation(string operation) => Update(() => _service.PressOperation(_model, operation));
+
+        public void PressClear() => Update(() => _service.PressClear(_model));
+
+        public void PressEqual() => Update(() => _service.PressEqual(_model));
+
+        public void PressSqrt() => Update(() => _service.PressSqrt(_model));
+
+        public void PressSqr() => Update(() => _service.PressSqr(_model));
+
+        private void Update(System.Action action)
+        {
+            action();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Result)));
+        }
+    }
+}
