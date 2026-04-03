b76308791d72f2d76f661312bd9c85fe0ad8c25e
Joseph Sans <arhiva.net1@gmail.com>
2026-04-03 17:10:29 +0300

    Кирилл Дятлов 9/2 РПО 24/1

---
+++ MyProject/5. CardFile/CardFile/CardFile.DataStore/Dtos/CardDto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CardFile.DataStore.Dtos
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public DateTime DeathDate { get; set; }
+
+        public string CauseOfDeath { get; set; }
+
+        public string PlaceOfDeath { get; set; }
+
+        public DateTime AdmissionDate { get; set; }
+
+        public string SectionNumber { get; set; }
+
+        public bool IsReleased { get; set; }
+
+        public CardDto Clone()
+        {
+            return new CardDto
+            {
+                Id = Id,
+                FirstName = FirstName,
+                MiddleName = MiddleName,
+                LastName = LastName,
+                BirthDate = BirthDate,
+                DeathDate = DeathDate,
+                CauseOfDeath = CauseOfDeath,
+                PlaceOfDeath = PlaceOfDeath,
+                AdmissionDate = AdmissionDate,
+                SectionNumber = SectionNumber,
+                IsReleased = IsReleased
+            };
+        }
+    }
+}
---
+++ MyProject/5. CardFile/CardFile/CardFile.DataStore/FileDataAccess/FileManagers/TextFileManager.cs
@@ -0,0 +1,65 @@
+﻿using CardFile.DataStore.DataCollection;
+using CardFile.DataStore.Dtos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CardFile.DataStore.FileDataAccess.FileManagers
+{
+    public class TextFileManager : IFileManager
+    {
+        public void OpenFromFile(string fileName, CardCollection collection)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            using (var reader = new StreamReader(fs))
+            {
+                var records = new List<CardDto>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (string.IsNullOrEmpty(line))
+                        continue;
+
+                    var lineParts = line.Split(';');
+                    if (lineParts.Length != 11)
+                        throw new Exception($"В строке файла {fileName} неверное количество полей");
+
+                    var newCard = new CardDto
+                    {
+                        Id = int.Parse(lineParts[0]),
+                        FirstName = lineParts[1],
+                        MiddleName = lineParts[2],
+                        LastName = lineParts[3],
+                        BirthDate = DateTime.Parse(lineParts[4]),
+                        DeathDate = DateTime.Parse(lineParts[5]),
+                        CauseOfDeath = lineParts[6],
+                        PlaceOfDeath = lineParts[7],
+                        AdmissionDate = DateTime.Parse(lineParts[8]),
+                        SectionNumber = lineParts[9],
+                        IsReleased = bool.Parse(lineParts[10])
+                    };
+
+                    records.Add(newCard);
+                }
+
+                collection.ReplaceAll(records);
+            }
+        }
+
+        public void SaveToFile(string fileName, CardCollection collection)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            using (var writer = new StreamWriter(fs))
+            {
+                foreach (var item in collection.GetAll())
+                {
+                    writer.WriteLine($"{item.Id};{item.FirstName};{item.MiddleName};{item.LastName};" +
+                                     $"{item.BirthDate.ToShortDateString()};{item.DeathDate.ToShortDateString()};" +
+                                     $"{item.CauseOfDeath};{item.PlaceOfDeath};{item.AdmissionDate.ToShortDateString()};" +
+                                     $"{item.SectionNumber};{item.IsReleased}");
+                }
+            }
+        }
+    }
+}
