3a14078f30b2e345621abade2c1b16812b668c5c
79101 <tyukina005@mail.ru>
2026-04-08 09:54:43 +0300

    правки по проекту

--- Projects.24.1.24.2.25.1/1. AlarmClock/AlarmClock/AlarmClock/AlarmClockForm.Designer.cs
+++ Projects.24.1.24.2.25.1/1. AlarmClock/AlarmClock/AlarmClock/AlarmClockForm.Designer.cs
@@ -34,6 +34,10 @@
             this.ExitButton = new System.Windows.Forms.Button();
             this.AboutButton = new System.Windows.Forms.Button();
             this.ClockTimer = new System.Windows.Forms.Timer(this.components);
+            this.Labletimer = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.StopwatchTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // DisplayLabel
@@ -41,18 +45,20 @@
             this.DisplayLabel.BackColor = System.Drawing.Color.Black;
             this.DisplayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.DisplayLabel.ForeColor = System.Drawing.Color.GreenYellow;
-            this.DisplayLabel.Location = new System.Drawing.Point(12, 9);
+            this.DisplayLabel.Location = new System.Drawing.Point(18, 14);
+            this.DisplayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DisplayLabel.Name = "DisplayLabel";
-            this.DisplayLabel.Size = new System.Drawing.Size(323, 81);
+            this.DisplayLabel.Size = new System.Drawing.Size(484, 125);
             this.DisplayLabel.TabIndex = 0;
             this.DisplayLabel.Text = "00:00:00";
             this.DisplayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // SettingsButton
             // 
-            this.SettingsButton.Location = new System.Drawing.Point(341, 9);
+            this.SettingsButton.Location = new System.Drawing.Point(512, 14);
+            this.SettingsButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.SettingsButton.Name = "SettingsButton";
-            this.SettingsButton.Size = new System.Drawing.Size(126, 23);
+            this.SettingsButton.Size = new System.Drawing.Size(189, 35);
             this.SettingsButton.TabIndex = 1;
             this.SettingsButton.Text = "Настройки...";
             this.SettingsButton.UseVisualStyleBackColor = true;
@@ -60,9 +66,10 @@
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(341, 67);
+            this.ExitButton.Location = new System.Drawing.Point(512, 103);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(126, 23);
+            this.ExitButton.Size = new System.Drawing.Size(189, 35);
             this.ExitButton.TabIndex = 2;
             this.ExitButton.Text = "Выход";
             this.ExitButton.UseVisualStyleBackColor = true;
@@ -70,9 +77,10 @@
             // 
             // AboutButton
             // 
-            this.AboutButton.Location = new System.Drawing.Point(341, 38);
+            this.AboutButton.Location = new System.Drawing.Point(512, 58);
+            this.AboutButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.AboutButton.Name = "AboutButton";
-            this.AboutButton.Size = new System.Drawing.Size(126, 23);
+            this.AboutButton.Size = new System.Drawing.Size(189, 35);
             this.AboutButton.TabIndex = 3;
             this.AboutButton.Text = "О программе...";
             this.AboutButton.UseVisualStyleBackColor = true;
@@ -84,19 +92,62 @@
             this.ClockTimer.Interval = 1000;
             this.ClockTimer.Tick += new System.EventHandler(this.ClockTimer_Tick);
             // 
+            // Labletimer
+            // 
+            this.Labletimer.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Labletimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Labletimer.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Labletimer.Location = new System.Drawing.Point(24, 162);
+            this.Labletimer.Name = "Labletimer";
+            this.Labletimer.Size = new System.Drawing.Size(478, 117);
+            this.Labletimer.TabIndex = 4;
+            this.Labletimer.Text = "00:00.00";
+            this.Labletimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Labletimer.Click += new System.EventHandler(this.Labletimer_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(512, 162);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(189, 50);
+            this.StartButton.TabIndex = 5;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(512, 232);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(189, 47);
+            this.StopButton.TabIndex = 6;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // StopwatchTimer
+            // 
+            this.StopwatchTimer.Interval = 10;
+            this.StopwatchTimer.Tick += new System.EventHandler(this.StopwatchTimer_Tick);
+            // 
             // AlarmClockForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(479, 102);
+            this.ClientSize = new System.Drawing.Size(718, 304);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.Labletimer);
             this.Controls.Add(this.AboutButton);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.SettingsButton);
             this.Controls.Add(this.DisplayLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "AlarmClockForm";
             this.Text = "Будильник";
+            this.Load += new System.EventHandler(this.AlarmClockForm_Load);
             this.ResumeLayout(false);
 
         }
@@ -108,6 +159,10 @@
         private System.Windows.Forms.Button ExitButton;
         private System.Windows.Forms.Button AboutButton;
         private System.Windows.Forms.Timer ClockTimer;
+        private System.Windows.Forms.Label Labletimer;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Timer StopwatchTimer;
     }
 }
 
--- Projects.24.1.24.2.25.1/1. AlarmClock/AlarmClock/AlarmClock/AlarmClockForm.cs
+++ Projects.24.1.24.2.25.1/1. AlarmClock/AlarmClock/AlarmClock/AlarmClockForm.cs
@@ -17,30 +17,37 @@ namespace AlarmClock
     {
         private AlarmState _alarmState = new AlarmState();
 
+        private DateTime? _stopwatchStartTime;
+        private TimeSpan _stopwatchElapsed = TimeSpan.Zero;
+        private bool _isStopwatchRunning = false;
+
         public AlarmClockForm()
         {
             InitializeComponent();
+
+            ClockTimer.Interval = 1000;
+            ClockTimer.Start();
+
+            StopwatchTimer.Interval = 10;
+            // Добавьте это:
+            StartButton.Click += StartButton_Click;
+            StopButton.Click += StopButton_Click;
         }
+
 
         private void ClockTimer_Tick(object sender, EventArgs e)
         {
             DisplayLabel.Text = DateTime.Now.ToLongTimeString();
 
-            if (!_alarmState.IsAlarmActive)
-            {
-                return;
-            }
+            if (!_alarmState.IsAlarmActive) return;
 
-            if (!_alarmState.IsAwakeActivated && 
+            if (!_alarmState.IsAwakeActivated &&
                 DateTime.Now.Hour == _alarmState.AlarmTime.Hour &&
                 DateTime.Now.Minute == _alarmState.AlarmTime.Minute)
             {
                 _alarmState.IsAwakeActivated = true;
-
-                var awakeForm = new AwakeForm();
-                awakeForm.AlarmState = _alarmState;
+                var awakeForm = new AwakeForm { AlarmState = _alarmState };
                 awakeForm.FormClosed += AwakeForm_FormClosed;
-
                 awakeForm.ShowDialog();
             }
 
@@ -50,44 +57,75 @@ namespace AlarmClock
             }
         }
 
+        private void StopwatchTimer_Tick(object sender, EventArgs e)
+        {
+            if (_isStopwatchRunning && _stopwatchStartTime.HasValue)
+            {
+                // Считаем: сколько прошло с нажатия Start + то, что накопили до этого
+                TimeSpan current = (DateTime.Now - _stopwatchStartTime.Value) + _stopwatchElapsed;
+                Labletimer.Text = current.ToString(@"mm\:ss\.ff");
+            }
+        }
+
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (!_isStopwatchRunning)
+            {
+                _stopwatchStartTime = DateTime.Now;
+                _isStopwatchRunning = true;
+                StopwatchTimer.Start();
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if (_isStopwatchRunning)
+            {
+                StopwatchTimer.Stop();
+                if (_stopwatchStartTime.HasValue)
+                {
+                    _stopwatchElapsed += DateTime.Now - _stopwatchStartTime.Value;
+                }
+                _isStopwatchRunning = false;
+            }
+            else
+            {
+                _stopwatchElapsed = TimeSpan.Zero;
+                _stopwatchStartTime = null;
+                Labletimer.Text = "00:00.00";
+            }
+        }
+
+        private void UpdateControls()
+        {
+            Text = _alarmState.IsAlarmActive
+                ? $"Будильник ({_alarmState.AlarmTime.ToShortTimeString()})"
+                : "Будильник";
+        }
+
         private void AwakeForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             ((Form)sender).FormClosed -= AwakeForm_FormClosed;
             UpdateControls();
         }
 
-        private void ExitButton_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+        private void ExitButton_Click(object sender, EventArgs e) => Close();
 
-        private void AboutButton_Click(object sender, EventArgs e)
-        {
-            var form = new AboutForm();
-            form.ShowDialog();
-        }
+        private void AboutButton_Click(object sender, EventArgs e) => new AboutForm().ShowDialog();
 
         private void SettingsButton_Click(object sender, EventArgs e)
         {
-            var form = new SettingsForm();
-            form.AlarmState = _alarmState;
-
-            if (form.ShowDialog() == DialogResult.OK)
-            {
-                UpdateControls();
-            }
+            var form = new SettingsForm { AlarmState = _alarmState };
+            if (form.ShowDialog() == DialogResult.OK) UpdateControls();
         }
 
-        private void UpdateControls()
+        private void Labletimer_Click(object sender, EventArgs e) { }
+
+        private void AlarmClockForm_Load(object sender, EventArgs e)
         {
-            if (_alarmState.IsAlarmActive)
-            {
-                Text = $"Будильник (ожидает срабатывания в {_alarmState.AlarmTime.ToShortTimeString()})";
-            }
-            else
-            {
-                Text = "Будильник";
-            }
+
         }
     }
 }
+
