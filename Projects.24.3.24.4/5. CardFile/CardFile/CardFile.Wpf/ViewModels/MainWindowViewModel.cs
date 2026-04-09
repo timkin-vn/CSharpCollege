4b43be2d5aab7620787eade4cfedadf4e855d586
AlekseeyTrifonov22 <bobikbobik2022@mail.ru>
2026-04-09 22:00:41 +0300

    5 - фильтр по подразделению по статусу сотрудника.

--- Projects.24.3.24.4/5. CardFile/CardFile/CardFile.Wpf/ViewModels/MainWindowViewModel.cs
+++ Projects.24.3.24.4/5. CardFile/CardFile/CardFile.Wpf/ViewModels/MainWindowViewModel.cs
@@ -7,19 +7,50 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CardFile.Wpf.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private CardFileService _service = new CardFileService();
+        private readonly CardFileService _service = new CardFileService();
+        private readonly List<CardViewModel> _allCards = new List<CardViewModel>();
+        private string _selectedDepartment = "Все подразделения";
+        private string _selectedStatus = "Все";
 
         public ObservableCollection<CardViewModel> Cards { get; } = new ObservableCollection<CardViewModel>();
 
+        public ObservableCollection<string> Departments { get; } = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Statuses { get; } = new ObservableCollection<string>
+        {
+            "Все",
+            "Работают",
+            "Уволены"
+        };
+
         public CardViewModel SelectedCard { get; set; }
+
+        public string SelectedDepartment
+        {
+            get => _selectedDepartment;
+            set
+            {
+                _selectedDepartment = value ?? "Все подразделения";
+                OnPropertyChanged(nameof(SelectedDepartment));
+                ApplyFilter();
+            }
+        }
+
+        public string SelectedStatus
+        {
+            get => _selectedStatus;
+            set
+            {
+                _selectedStatus = value ?? "Все";
+                OnPropertyChanged(nameof(SelectedStatus));
+                ApplyFilter();
+            }
+        }
 
         public bool IsEditButtonEnabled => SelectedCard != null;
 
@@ -37,14 +68,10 @@ namespace CardFile.Wpf.ViewModels
         public void WindowLoaded()
         {
             Mapping.Initialize();
-
             LoadAllData();
         }
 
-        public CardViewModel GetSelectedCard()
-        {
-            return SelectedCard;
-        }
+        public CardViewModel GetSelectedCard() => SelectedCard;
 
         public CardViewModel GetNewCard()
         {
@@ -57,39 +84,37 @@ namespace CardFile.Wpf.ViewModels
 
         public void SaveEditedCard(CardViewModel card)
         {
-            var index = Cards.ToList().FindIndex(c => c.Id == card.Id);
-
-            if (index < 0)
-            {
-                throw new Exception("Карточка с таким Id не существует");
-            }
-
-            var id =_service.Save(ToBusiness(card));
-
-            if (id < 0)
-            {
-                Cards.RemoveAt(index);
-            }
-            else
-            {
-                Cards[index].LoadViewModel(card);
-            }
-        }
-
-        public void SaveNewCard(CardViewModel card)
-        {
-            var newCard = new CardViewModel();
-            newCard.LoadViewModel(card);
-
             var id = _service.Save(ToBusiness(card));
-
             if (id < 0)
             {
                 return;
             }
 
+            var item = _allCards.FirstOrDefault(c => c.Id == card.Id);
+            if (item != null)
+            {
+                item.LoadViewModel(card);
+            }
+
+            UpdateDepartments();
+            ApplyFilter();
+        }
+
+        public void SaveNewCard(CardViewModel card)
+        {
+            var id = _service.Save(ToBusiness(card));
+            if (id < 0)
+            {
+                return;
+            }
+
+            var newCard = new CardViewModel();
+            newCard.LoadViewModel(card);
             newCard.Id = id;
-            Cards.Add(newCard);
+
+            _allCards.Add(newCard);
+            UpdateDepartments();
+            ApplyFilter();
         }
 
         public void DeleteSelectedCard()
@@ -100,16 +125,10 @@ namespace CardFile.Wpf.ViewModels
             }
 
             _service.Delete(SelectedCard.Id);
-            var index = Cards.ToList().FindIndex(c => c.Id == SelectedCard.Id);
-
-            if (index < 0)
-            {
-                throw new Exception("Карточка с таким Id не существует");
-            }
-
-            Cards.RemoveAt(index);
+            _allCards.RemoveAll(c => c.Id == SelectedCard.Id);
             SelectedCard = null;
-
+            UpdateDepartments();
+            ApplyFilter();
             OnPropertyChanged(nameof(SelectedCard));
         }
 
@@ -122,78 +141,79 @@ namespace CardFile.Wpf.ViewModels
         public void SaveToFile(string fileName)
         {
             _service.SaveToFile(fileName);
-
             FileName = fileName;
             OnPropertyChanged(nameof(WindowTitle));
         }
 
         public void SaveToFile()
         {
-            try
-            {
-                _service.SaveToFile(FileName);
-            }
-            catch(Exception)
-            {
-                FileName = null;
-                OnPropertyChanged(nameof(WindowTitle));
-                throw;
-            }
+            _service.SaveToFile(FileName);
         }
 
         public void OpenFromFile(string fileName)
         {
             _service.OpenFromFile(fileName);
             LoadAllData();
-
             FileName = fileName;
             OnPropertyChanged(nameof(WindowTitle));
         }
 
         private void LoadAllData()
         {
-            var cards = _service.GetAll();
-            Cards.Clear();
-            foreach (var card in cards)
+            _allCards.Clear();
+            _allCards.AddRange(_service.GetAll().Select(ToViewModel));
+            UpdateDepartments();
+            ApplyFilter();
+        }
+
+        private void UpdateDepartments()
+        {
+            var previous = SelectedDepartment;
+            Departments.Clear();
+            Departments.Add("Все подразделения");
+
+            foreach (var department in _allCards.Select(c => c.Department).Distinct().OrderBy(d => d))
             {
-                Cards.Add(ToViewModel(card));
+                Departments.Add(department);
+            }
+
+            if (Departments.Contains(previous))
+            {
+                _selectedDepartment = previous;
+            }
+            else
+            {
+                _selectedDepartment = "Все подразделения";
+            }
+
+            OnPropertyChanged(nameof(SelectedDepartment));
+        }
+
+        private void ApplyFilter()
+        {
+            var filtered = _allCards
+                .Where(c => SelectedDepartment == "Все подразделения" || c.Department == SelectedDepartment)
+                .Where(c =>
+                    SelectedStatus == "Все" ||
+                    (SelectedStatus == "Работают" && !c.DismissalDate.HasValue) ||
+                    (SelectedStatus == "Уволены" && c.DismissalDate.HasValue))
+                .ToList();
+
+            Cards.Clear();
+            foreach (var card in filtered)
+            {
+                Cards.Add(card);
             }
         }
 
         private CardViewModel ToViewModel(Card card)
         {
             return Mapping.Mapper.Map<CardViewModel>(card);
-            //return new CardViewModel
-            //{
-            //    Id = card.Id,
-            //    FirstName = card.FirstName,
-            //    LastName = card.LastName,
-            //    MiddleName = card.MiddleName,
-            //    BirthDate = card.BirthDate,
-            //    Department = card.Department,
-            //    Position = card.Position,
-            //    EmploymentDate = card.EmploymentDate,
-            //    DismissalDate = card.DismissalDate,
-            //    Salary = card.Salary,
-            //};
         }
 
         private Card ToBusiness(CardViewModel card)
         {
             return Mapping.Mapper.Map<Card>(card);
-            //return new Card
-            //{
-            //    Id = card.Id,
-            //    FirstName = card.FirstName,
-            //    LastName = card.LastName,
-            //    MiddleName = card.MiddleName,
-            //    BirthDate = card.BirthDate,
-            //    Department = card.Department,
-            //    Position = card.Position,
-            //    EmploymentDate = card.EmploymentDate,
-            //    DismissalDate = card.DismissalDate,
-            //    Salary = card.Salary,
-            //};
         }
     }
 }
