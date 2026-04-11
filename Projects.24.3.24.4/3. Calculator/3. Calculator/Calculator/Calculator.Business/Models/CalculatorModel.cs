414ce45a1abec8e578b9493df0ec63589df794a2
exccde <maximerem999@gmail.com>
2026-04-11 11:27:27 +0300

    3 - Equal_Decimal_Sign_Sqrt

---
+++ Projects.24.3.24.4/3. Calculator/3. Calculator/Calculator/Calculator.Business/Models/CalculatorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculator.Business.Models
+{
+    public class CalculatorModel
+    {
+        public double RegisterX { get; internal set; }
+
+        internal double RegisterY { get; set; }
+
+        internal string OperationCode { get; set; }
+
+        internal bool IsLastDigitPressed { get; set; }
+
+        internal string CurrentInput { get; set; } = "0";
+
+        public string DisplayText { get; internal set; } = "0";
+    }
+}
