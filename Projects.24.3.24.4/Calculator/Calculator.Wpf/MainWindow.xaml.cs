9978bdda20f1064061450928368e5a03a7de2780
крутой ярик 1488 <luna98tic@gmail.com>
2026-04-11 11:22:55 +0300

    Add files via upload

---
+++ Projects.24.3.24.4/Calculator/Calculator.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿﻿using Calculator.Wpf.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Calculator.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitButton_Click(object sender, RoutedEventArgs e)
+        {
+            var digitString = ((Button)sender).Content as string;
+            ViewModel.PressDigit(digitString);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressClear();
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var operationCode = ((Button)sender).Content as string;
+            ViewModel.PressOperation(operationCode);
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PressEqual();
+        }
+
+private void DecimalButton_Click(object sender, RoutedEventArgs e)
+{
+    ViewModel.PressDecimalSeparator();
+}
+
+private void SignButton_Click(object sender, RoutedEventArgs e)
+{
+    ViewModel.PressToggleSign();
+}
+
+private void SqrtButton_Click(object sender, RoutedEventArgs e)
+{
+    ViewModel.PressSquareRoot();
+}
+
+
+    }
+}
---
+++ Projects.24.3.24.4/Calculator/Calculator.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System.ComponentModel;
+
+namespace Calculator.Wpf.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly CalculatorModel _calculatorModel = new CalculatorModel();
+        private readonly CalculatorService _calculatorService = new CalculatorService();
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string DisplayValue => _calculatorModel.DisplayText;
+
+        public void PressDigit(string digitString)
+        {
+            _calculatorService.PressDigit(_calculatorModel, digitString);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressClear()
+        {
+            _calculatorService.PressClear(_calculatorModel);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressOperation(string operationCode)
+        {
+            _calculatorService.PressOperation(_calculatorModel, operationCode);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+        public void PressEqual()
+        {
+            _calculatorService.PressEqual(_calculatorModel);
+            OnPropertyChanged(nameof(DisplayValue));
+        }
+
+public void PressDecimalSeparator()
+{
+    _calculatorService.PressDecimalSeparator(_calculatorModel);
+    OnPropertyChanged(nameof(DisplayValue));
+}
+
+public void PressToggleSign()
+{
+    _calculatorService.PressToggleSign(_calculatorModel);
+    OnPropertyChanged(nameof(DisplayValue));
+}
+
+public void PressSquareRoot()
+{
+    _calculatorService.PressSquareRoot(_calculatorModel);
+    OnPropertyChanged(nameof(DisplayValue));
+}
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
