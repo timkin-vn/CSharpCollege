5d2cd34f48795aeca0cbb1d1a3c95549d080a5f6
крутой ярик 1488 <luna98tic@gmail.com>
2026-04-11 11:25:36 +0300

    Add files via upload

---
+++ Projects.24.3.24.4/Calculator/Calculator.Business/Services/CalculatorService.cs
@@ -0,0 +1,173 @@
+﻿using Calculator.Business.Models;
+using System;
+using System.Globalization;
+
+namespace Calculator.Business.Services
+{
+    public class CalculatorService
+    {
+        public void PressDigit(CalculatorModel calculatorModel, string digitString)
+        {
+            if (!int.TryParse(digitString, out _))
+            {
+                return;
+            }
+
+            if (!calculatorModel.IsLastDigitPressed)
+            {
+                calculatorModel.CurrentInput = "0";
+                calculatorModel.RegisterX = 0;
+            }
+
+            if (calculatorModel.CurrentInput == "0")
+            {
+                calculatorModel.CurrentInput = digitString;
+            }
+            else
+            {
+                calculatorModel.CurrentInput += digitString;
+            }
+
+            calculatorModel.RegisterX = Parse(calculatorModel.CurrentInput);
+            calculatorModel.DisplayText = calculatorModel.CurrentInput;
+            calculatorModel.IsLastDigitPressed = true;
+        }
+
+        public void PressDecimalSeparator(CalculatorModel calculatorModel)
+        {
+            if (!calculatorModel.IsLastDigitPressed)
+            {
+                calculatorModel.CurrentInput = "0";
+                calculatorModel.RegisterX = 0;
+                calculatorModel.IsLastDigitPressed = true;
+            }
+
+            if (calculatorModel.CurrentInput.Contains("."))
+            {
+                return;
+            }
+
+            calculatorModel.CurrentInput += ".";
+            calculatorModel.DisplayText = calculatorModel.CurrentInput;
+        }
+
+        public void PressClear(CalculatorModel calculatorModel)
+        {
+            calculatorModel.RegisterX = 0;
+            calculatorModel.RegisterY = 0;
+            calculatorModel.OperationCode = null;
+            calculatorModel.IsLastDigitPressed = false;
+            calculatorModel.CurrentInput = "0";
+            calculatorModel.DisplayText = "0";
+        }
+
+        public void PressMoveXToY(CalculatorModel calculatorModel)
+        {
+            calculatorModel.RegisterY = calculatorModel.RegisterX;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        public void PressOperation(CalculatorModel calculatorModel, string operationCode)
+        {
+            CompleteOperation(calculatorModel);
+
+            PressMoveXToY(calculatorModel);
+            calculatorModel.OperationCode = operationCode;
+            calculatorModel.CurrentInput = Format(calculatorModel.RegisterX);
+            calculatorModel.DisplayText = calculatorModel.CurrentInput;
+        }
+
+        public void PressEqual(CalculatorModel calculatorModel)
+        {
+            CompleteOperation(calculatorModel);
+            calculatorModel.OperationCode = null;
+            calculatorModel.CurrentInput = Format(calculatorModel.RegisterX);
+            calculatorModel.DisplayText = calculatorModel.CurrentInput;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        public void PressPercent(CalculatorModel calculatorModel)
+        {
+            if (!string.IsNullOrEmpty(calculatorModel.OperationCode))
+            {
+                calculatorModel.RegisterX = calculatorModel.RegisterY * calculatorModel.RegisterX / 100;
+            }
+            else
+            {
+                calculatorModel.RegisterX = calculatorModel.RegisterX / 100;
+            }
+
+            calculatorModel.CurrentInput = Format(calculatorModel.RegisterX);
+            calculatorModel.DisplayText = calculatorModel.CurrentInput;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        public void PressSquare(CalculatorModel calculatorModel)
+        {
+            calculatorModel.RegisterX = calculatorModel.RegisterX * calculatorModel.RegisterX;
+            calculatorModel.CurrentInput = Format(calculatorModel.RegisterX);
+            calculatorModel.DisplayText = calculatorModel.CurrentInput;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        public void PressSquareRoot(CalculatorModel calculatorModel)
+        {
+            if (calculatorModel.RegisterX < 0)
+            {
+                return;
+            }
+
+            calculatorModel.RegisterX = Math.Sqrt(calculatorModel.RegisterX);
+            calculatorModel.CurrentInput = Format(calculatorModel.RegisterX);
+            calculatorModel.DisplayText = calculatorModel.CurrentInput;
+            calculatorModel.IsLastDigitPressed = false;
+        }
+
+        private void CompleteOperation(CalculatorModel calculatorModel)
+        {
+            switch (calculatorModel.OperationCode)
+            {
+                case "+":
+                    calculatorModel.RegisterX = calculatorModel.RegisterY + calculatorModel.RegisterX;
+                    break;
+
+                case "-":
+                    calculatorModel.RegisterX = calculatorModel.RegisterY - calculatorModel.RegisterX;
+                    break;
+
+                case "*":
+                    calculatorModel.RegisterX = calculatorModel.RegisterY * calculatorModel.RegisterX;
+                    break;
+
+                case "/":
+                    calculatorModel.RegisterX = calculatorModel.RegisterX == 0 ? 0 : calculatorModel.RegisterY / calculatorModel.RegisterX;
+                    break;
+            }
+
+            if (!string.IsNullOrEmpty(calculatorModel.OperationCode))
+            {
+                calculatorModel.DisplayText = Format(calculatorModel.RegisterX);
+            }
+        }
+
+        private double Parse(string text)
+        {
+            if (text.EndsWith("."))
+            {
+                text = text.TrimEnd('.');
+            }
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return 0;
+            }
+
+            return double.Parse(text, CultureInfo.InvariantCulture);
+        }
+
+        private string Format(double value)
+        {
+            return value.ToString("0.############", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Projects.24.3.24.4/Calculator/Calculator/CalculatorForm.cs
@@ -0,0 +1,131 @@
+﻿using Calculator.Business.Models;
+using Calculator.Business.Services;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private readonly CalculatorModel _calculatorModel = new CalculatorModel();
+
+        private readonly CalculatorService _calculatorService = new CalculatorService();
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+
+            EqualButton.Click -= OperationButton_Click;
+            EqualButton.Click += EqualButton_Click;
+
+            ClientSize = new Size(463, 455);
+            AddExtraButtons();
+            DisplayResult();
+        }
+
+        private void AddExtraButtons()
+        {
+            var decimalButton = new Button
+            {
+                Text = ".",
+                Font = new Font("Microsoft Sans Serif", 27.75F),
+                Size = new Size(75, 59),
+                Location = new Point(93, 313),
+            };
+            decimalButton.Click += DecimalButton_Click;
+            Controls.Add(decimalButton);
+
+            var percentButton = new Button
+            {
+                Text = "%",
+                Font = new Font("Microsoft Sans Serif", 27.75F),
+                Size = new Size(75, 59),
+                Location = new Point(174, 313),
+            };
+            percentButton.Click += PercentButton_Click;
+            Controls.Add(percentButton);
+
+            var squareButton = new Button
+            {
+                Text = "x²",
+                Font = new Font("Microsoft Sans Serif", 20F),
+                Size = new Size(75, 59),
+                Location = new Point(12, 378),
+            };
+            squareButton.Click += SquareButton_Click;
+            Controls.Add(squareButton);
+
+            var squareRootButton = new Button
+            {
+                Text = "√",
+                Font = new Font("Microsoft Sans Serif", 24F),
+                Size = new Size(75, 59),
+                Location = new Point(93, 378),
+            };
+            squareRootButton.Click += SquareRootButton_Click;
+            Controls.Add(squareRootButton);
+        }
+
+        private void DigitButton_Click(object sender, EventArgs e)
+        {
+            var digitString = ((Button)sender).Text;
+            _calculatorService.PressDigit(_calculatorModel, digitString);
+            DisplayResult();
+        }
+
+        private void DisplayResult()
+        {
+            DisplayLabel.Text = _calculatorModel.DisplayText;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            _calculatorService.PressClear(_calculatorModel);
+            DisplayResult();
+        }
+
+        private void MoveXToYButton_Click(object sender, EventArgs e)
+        {
+            _calculatorService.PressMoveXToY(_calculatorModel);
+            DisplayResult();
+        }
+
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            var operationCode = ((Button)sender).Text;
+            _calculatorService.PressOperation(_calculatorModel, operationCode);
+            DisplayResult();
+        }
+
+        private void EqualButton_Click(object sender, EventArgs e)
+        {
+            _calculatorService.PressEqual(_calculatorModel);
+            DisplayResult();
+        }
+
+        private void DecimalButton_Click(object sender, EventArgs e)
+        {
+            _calculatorService.PressDecimalSeparator(_calculatorModel);
+            DisplayResult();
+        }
+
+        private void PercentButton_Click(object sender, EventArgs e)
+        {
+            _calculatorService.PressPercent(_calculatorModel);
+            DisplayResult();
+        }
+
+        private void SquareButton_Click(object sender, EventArgs e)
+        {
+            _calculatorService.PressSquare(_calculatorModel);
+            DisplayResult();
+        }
+
+        private void SquareRootButton_Click(object sender, EventArgs e)
+        {
+            _calculatorService.PressSquareRoot(_calculatorModel);
+            DisplayResult();
+        }
+    }
+}
