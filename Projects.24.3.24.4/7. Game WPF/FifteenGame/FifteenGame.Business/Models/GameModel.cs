9a8395cdbeecea59aa32917bbe3b50c8ce9aca33
Тимкин Владимир <timkin_vn@mail.ru>
2026-04-27 16:17:26 +0700

    Проект Игра Wpf для групп 24/3, 24/4

---
+++ Projects.24.3.24.4/7. Game WPF/FifteenGame/FifteenGame.Business/Models/GameModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Business.Models
+{
+    public class GameModel
+    {
+        public const int RowCount = 4;
+
+        public const int ColumnCount = 4;
+
+        public const int FreeCellValue = -1;
+
+        private int[,] _cells = new int[RowCount, ColumnCount];
+
+        public int this[int row, int column]
+        {
+            get => _cells[row, column];
+            internal set => _cells[row, column] = value;
+        }
+
+        public int FreeCellRow { get; internal set; }
+
+        public int FreeCellColumn { get; internal set; }
+    }
+}
