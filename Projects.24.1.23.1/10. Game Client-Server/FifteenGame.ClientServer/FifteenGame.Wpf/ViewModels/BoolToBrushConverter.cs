14f5c20dd18851a9193081e403ce3a33af80dfe9
misa47 <hayatotima@gmail.com>
2026-05-20 01:48:55 +0300

    .

---
+++ Projects.24.1.23.1/10. Game Client-Server/FifteenGame.ClientServer/FifteenGame.Wpf/ViewModels/BoolToBrushConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FifteenGame.Wpf.ViewModels
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = (bool)value;
+            return isSelected ? Brushes.Orange : Brushes.LightBlue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
