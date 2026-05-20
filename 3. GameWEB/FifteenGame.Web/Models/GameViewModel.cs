f6e53383f3eabcade432c2957f3aaf35ef1fb1e7
Maksim <maksimmiliskov2@gmail.com>
2026-05-20 20:39:01 +1200

    Add files via upload

---
+++ 3. GameWEB/FifteenGame.Web/Models/GameViewModel.cs
@@ -0,0 +1,30 @@
+﻿using FifteenGame.Business.Models;
+
+namespace FifteenGame.Web.Models
+{
+    public class GameViewModel
+    {
+        public int RowCount => GameField.RowCount;
+        public int ColumnCount => GameField.ColumnCount;
+
+        public CellViewModel[,] Cells { get; set; } = new CellViewModel[GameField.RowCount, GameField.ColumnCount];
+
+        public int Score { get; set; }
+        public int MovesLeft { get; set; }
+        public GameState GameState { get; set; }
+        public string GameOverMessage { get; set; }
+
+        public string GameStatus
+        {
+            get
+            {
+                if (GameState == GameState.Playing)
+                    return $"Очки: {Score} | Ходов осталось: {MovesLeft}";
+                else if (GameState == GameState.Won)
+                    return $"Победа! Набрано {Score} очков!";
+                else
+                    return $"Игра окончена. Очки: {Score}";
+            }
+        }
+    }
+}
