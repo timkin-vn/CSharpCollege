215acf86d0961fd5b2f22bfe438416a7ef816737
Maksim <maksimmiliskov2@gmail.com>
2026-05-20 20:46:01 +1200

    Add files via upload

---
+++ 5. GameClient-Server/FifteenGame.Wpf/Infrastructure/NinjectKernel.cs
@@ -0,0 +1,35 @@
+﻿using Ninject;
+using System;
+using System.Windows;
+
+namespace FifteenGame.Wpf.Infrastructure
+{
+    public static class NinjectKernel
+    {
+        private static IKernel _instance;
+
+        public static IKernel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    try
+                    {
+                        _instance = new StandardKernel(new FifteenGameModule());
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("CRITICAL NINJECT ERROR: " + ex.ToString());
+                        throw;
+                    }
+                }
+                return _instance;
+            }
+            set
+            {
+                _instance = value;
+            }
+        }
+    }
+}
---
+++ 5. GameClient-Server/FifteenGame.Wpf/ViewModels/LoginViewModel.cs
@@ -0,0 +1,86 @@
+﻿using FifteenGame.Common.BusinessDtos;
+using FifteenGame.Common.Services;
+using FifteenGame.Wpf.Infrastructure;
+using Ninject;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FifteenGame.Wpf.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private readonly IUserService _userService;
+        public event Action<UserDto> OnLoginSuccess;
+
+        public string Username { get; set; }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RegisterCommand { get; }
+
+        public LoginViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+                return;
+
+            _userService = NinjectKernel.Instance.Get<IUserService>();
+
+            LoginCommand = new RelayCommand(Login);
+            RegisterCommand = new RelayCommand(Register);
+        }
+
+        private void Login(object parameter)
+        {
+            var passwordBox = parameter as PasswordBox;
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(passwordBox?.Password))
+            {
+                MessageBox.Show("Введите логин и пароль");
+                return;
+            }
+
+            try
+            {
+                var user = _userService.Login(Username, passwordBox.Password);
+                if (user != null)
+                {
+                    OnLoginSuccess?.Invoke(user);
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка соединения с сервером. Запущен ли FifteenGame.Server?");
+            }
+        }
+
+        private void Register(object parameter)
+        {
+            var passwordBox = parameter as PasswordBox;
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(passwordBox?.Password)) return;
+
+            try
+            {
+                var user = _userService.Register(Username, passwordBox.Password);
+                if (user != null)
+                {
+                    MessageBox.Show("Регистрация успешна! Теперь войдите.");
+                }
+                else
+                {
+                    MessageBox.Show("Такой пользователь уже существует.");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка соединения с сервером.");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
