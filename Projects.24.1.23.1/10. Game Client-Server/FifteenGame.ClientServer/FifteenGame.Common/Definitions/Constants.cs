fdeabd6922dee890525e7a8794239f8a305075ca
misa47 <hayatotima@gmail.com>
2026-05-20 03:58:20 +0300

    .

--- Projects.24.1.23.1/10. Game Client-Server/FifteenGame.ClientServer/FifteenGame.Common/Definitions/Constants.cs
+++ Projects.24.1.23.1/10. Game Client-Server/FifteenGame.ClientServer/FifteenGame.Common/Definitions/Constants.cs
@@ -8,10 +8,7 @@ namespace FifteenGame.Common.Definitions
 {
     public static class Constants
     {
-        public const int RowCount = 4;
-
-        public const int ColumnCount = 4;
-
-        public const int FreeCellValue = -1;
+        public const int RowCount = 8;
+        public const int ColumnCount = 8;
     }
 }
---
+++ Projects.24.1.23.1/10. Game Client-Server/FifteenGame.ClientServer/FifteenGame.DataAcces.EF/Migrations/202605200053242_Match3Game.cs
@@ -0,0 +1,22 @@
+﻿namespace FifteenGame.DataAcces.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Match3Game : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("public.Games", "MatchesCount", c => c.Int(nullable: false));
+            AddColumn("public.Games", "IsFinished", c => c.Boolean(nullable: false));
+            DropColumn("public.Games", "MoveCount");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("public.Games", "MoveCount", c => c.Int(nullable: false));
+            DropColumn("public.Games", "IsFinished");
+            DropColumn("public.Games", "MatchesCount");
+        }
+    }
+}
