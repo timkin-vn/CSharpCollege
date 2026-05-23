08b2bb682b0276b02678d71d3d2ca828376dd339
Ishakao <illiyastar25@gmail.com>
2026-05-23 05:59:08 +0300

    Add files via upload

---
+++ Projects.24.1.24.2.25.1/1. AlarmClock/Forms/Seconds.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace AlarmClock.Forms
+{
+    partial class X
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.laiblSec = new System.Windows.Forms.Label();
+            this.buttonStar = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.timerSec = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ScrollBarCircles = new System.Windows.Forms.ListBox();
+            this.buttonSbros = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // laiblSec
+            // 
+            this.laiblSec.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.laiblSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.laiblSec.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.laiblSec.Location = new System.Drawing.Point(12, 9);
+            this.laiblSec.Name = "laiblSec";
+            this.laiblSec.Size = new System.Drawing.Size(214, 60);
+            this.laiblSec.TabIndex = 0;
+            this.laiblSec.Text = "00:00:00";
+            this.laiblSec.Click += new System.EventHandler(this.laiblSec_Click);
+            // 
+            // buttonStar
+            // 
+            this.buttonStar.BackColor = System.Drawing.Color.Silver;
+            this.buttonStar.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStar.Location = new System.Drawing.Point(12, 88);
+            this.buttonStar.Name = "buttonStar";
+            this.buttonStar.Size = new System.Drawing.Size(214, 34);
+            this.buttonStar.TabIndex = 1;
+            this.buttonStar.Text = "Старт";
+            this.buttonStar.UseVisualStyleBackColor = false;
+            this.buttonStar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.BackColor = System.Drawing.Color.Silver;
+            this.buttonStop.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStop.Location = new System.Drawing.Point(12, 128);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(214, 34);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Стоп";
+            this.buttonStop.UseVisualStyleBackColor = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // timerSec
+            // 
+            this.timerSec.Interval = 1000;
+            this.timerSec.Tick += new System.EventHandler(this.timerSec_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(395, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Круг";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ScrollBarCircles
+            // 
+            this.ScrollBarCircles.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ScrollBarCircles.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ScrollBarCircles.FormattingEnabled = true;
+            this.ScrollBarCircles.ItemHeight = 21;
+            this.ScrollBarCircles.Location = new System.Drawing.Point(235, 9);
+            this.ScrollBarCircles.Name = "ScrollBarCircles";
+            this.ScrollBarCircles.Size = new System.Drawing.Size(265, 151);
+            this.ScrollBarCircles.TabIndex = 5;
+            this.ScrollBarCircles.SelectedIndexChanged += new System.EventHandler(this.listKrugi_SelectedIndexChanged);
+            // 
+            // buttonSbros
+            // 
+            this.buttonSbros.BackColor = System.Drawing.Color.Silver;
+            this.buttonSbros.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSbros.Location = new System.Drawing.Point(203, 184);
+            this.buttonSbros.Name = "buttonSbros";
+            this.buttonSbros.Size = new System.Drawing.Size(154, 31);
+            this.buttonSbros.TabIndex = 6;
+            this.buttonSbros.Text = "Сброс";
+            this.buttonSbros.UseVisualStyleBackColor = false;
+            this.buttonSbros.Click += new System.EventHandler(this.buttonSbros_Click);
+            // 
+            // X
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(514, 219);
+            this.Controls.Add(this.buttonSbros);
+            this.Controls.Add(this.ScrollBarCircles);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStar);
+            this.Controls.Add(this.laiblSec);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "X";
+            this.Text = "Seconds";
+            this.Load += new System.EventHandler(this.X_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label laiblSec;
+        private System.Windows.Forms.Button buttonStar;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Timer timerSec;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox ScrollBarCircles;
+        private System.Windows.Forms.Button buttonSbros;
+    }
+}
---
+++ Projects.24.1.24.2.25.1/1. AlarmClock/Forms/SettingsForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AlarmClock.Forms
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AlarmTimeTextBox = new System.Windows.Forms.TextBox();
+            this.AlarmMessageTextBox = new System.Windows.Forms.TextBox();
+            this.IsSoundActiveCheckBox = new System.Windows.Forms.CheckBox();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Время срабатывания:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Сообщение при срабатывании:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // AlarmTimeTextBox
+            // 
+            this.AlarmTimeTextBox.BackColor = System.Drawing.Color.Silver;
+            this.AlarmTimeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AlarmTimeTextBox.Location = new System.Drawing.Point(193, 25);
+            this.AlarmTimeTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.AlarmTimeTextBox.Multiline = true;
+            this.AlarmTimeTextBox.Name = "AlarmTimeTextBox";
+            this.AlarmTimeTextBox.Size = new System.Drawing.Size(81, 22);
+            this.AlarmTimeTextBox.TabIndex = 2;
+            this.AlarmTimeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AlarmMessageTextBox
+            // 
+            this.AlarmMessageTextBox.BackColor = System.Drawing.Color.Silver;
+            this.AlarmMessageTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AlarmMessageTextBox.Location = new System.Drawing.Point(264, 67);
+            this.AlarmMessageTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.AlarmMessageTextBox.Multiline = true;
+            this.AlarmMessageTextBox.Name = "AlarmMessageTextBox";
+            this.AlarmMessageTextBox.Size = new System.Drawing.Size(172, 22);
+            this.AlarmMessageTextBox.TabIndex = 3;
+            this.AlarmMessageTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // IsSoundActiveCheckBox
+            // 
+            this.IsSoundActiveCheckBox.AutoSize = true;
+            this.IsSoundActiveCheckBox.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsSoundActiveCheckBox.Location = new System.Drawing.Point(14, 123);
+            this.IsSoundActiveCheckBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.IsSoundActiveCheckBox.Name = "IsSoundActiveCheckBox";
+            this.IsSoundActiveCheckBox.Size = new System.Drawing.Size(158, 26);
+            this.IsSoundActiveCheckBox.TabIndex = 5;
+            this.IsSoundActiveCheckBox.Text = "Звуковой сигнал";
+            this.IsSoundActiveCheckBox.UseVisualStyleBackColor = true;
+            this.IsSoundActiveCheckBox.CheckedChanged += new System.EventHandler(this.IsSoundActiveCheckBox_CheckedChanged);
+            // 
+            // OkButton
+            // 
+            this.OkButton.BackColor = System.Drawing.Color.Silver;
+            this.OkButton.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OkButton.Location = new System.Drawing.Point(14, 216);
+            this.OkButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(154, 54);
+            this.OkButton.TabIndex = 6;
+            this.OkButton.Text = "Применить";
+            this.OkButton.UseVisualStyleBackColor = false;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.BackColor = System.Drawing.Color.RosyBrown;
+            this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelBtn.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.Location = new System.Drawing.Point(326, 216);
+            this.CancelBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(119, 54);
+            this.CancelBtn.TabIndex = 7;
+            this.CancelBtn.Text = "Отмена";
+            this.CancelBtn.UseVisualStyleBackColor = false;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // SettingsForm
+            // 
+            this.AcceptButton = this.OkButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.CancelButton = this.CancelBtn;
+            this.ClientSize = new System.Drawing.Size(459, 283);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.IsSoundActiveCheckBox);
+            this.Controls.Add(this.AlarmMessageTextBox);
+            this.Controls.Add(this.AlarmTimeTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SettingsForm";
+            this.Text = "Настройки будильника";
+            this.Load += new System.EventHandler(this.SettingsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AlarmTimeTextBox;
+        private System.Windows.Forms.TextBox AlarmMessageTextBox;
+        private System.Windows.Forms.CheckBox IsSoundActiveCheckBox;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button CancelBtn;
+    }
+}
