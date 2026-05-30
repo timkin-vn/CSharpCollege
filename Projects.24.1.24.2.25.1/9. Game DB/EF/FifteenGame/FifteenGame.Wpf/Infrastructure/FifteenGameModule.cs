bfe193943811012f2eb13ca87031ef5dbcb3c366
Тимкин Владимир <timkin_vn@mail.ru>
2026-05-30 12:22:18 +0700

    Добавлен проект Игра с использованием БД на платформе EF

---
+++ Projects.24.1.24.2.25.1/9. Game DB/EF/FifteenGame/FifteenGame.Wpf/Infrastructure/FifteenGameModule.cs
@@ -0,0 +1,26 @@
+﻿using FifteenGame.Business.Services;
+using FifteenGame.Common.Contracts.Repositories;
+using FifteenGame.Common.Contracts.Services;
+using FifteenGame.DataAccess.EF.Repositories;
+using FifteenGame.DataAccess.Repositories;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Wpf.Infrastructure
+{
+    internal class FifteenGameModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUserRepository>().To<UserRepositoryEF>().InSingletonScope();
+            Bind<IGameRepository>().To<GameRepositoryEF>().InSingletonScope();
+
+            Bind<IUserService>().To<UserService>().InSingletonScope();
+            Bind<IGameService>().To<GameService>().InSingletonScope();
+        }
+    }
+}
---
+++ Projects.24.3.24.4/9. Game DB/EF/FifteenGame/FifteenGame.DataAccess.EF/Repositories/GameRepositoryEF.cs
@@ -0,0 +1,172 @@
+﻿using FifteenGame.Common.Contracts.Repositories;
+using FifteenGame.Common.Definitions;
+using FifteenGame.Common.Dtos;
+using FifteenGame.DataAccess.EF.DataContext;
+using FifteenGame.DataAccess.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.DataAccess.EF.Repositories
+{
+    public class GameRepositoryEF : IGameRepository
+    {
+        public GameDto GetByGameId(int gameId)
+        {
+            using (var context = new FifteenGameDataContext())
+            {
+                return ToDto(context.Games.Include("Cells").FirstOrDefault(g => g.Id == gameId));
+            }
+        }
+
+        public IEnumerable<GameDto> GetByUserId(int userId)
+        {
+            using (var context = new FifteenGameDataContext())
+            {
+                var games = context.Games.Include("Cells").Where(g => g.UserId == userId);
+                return games.Select(ToDto).ToList();
+            }
+        }
+
+        public void Remove(int gameId)
+        {
+            using (var context = new FifteenGameDataContext())
+            {
+                var game = context.Games.Include("Cells").FirstOrDefault(g => g.Id == gameId);
+                if (game == null)
+                {
+                    return;
+                }
+
+                context.Games.Remove(game);
+                context.SaveChanges();
+            }
+        }
+
+        public int Save(GameDto gameDto)
+        {
+            if (gameDto.Id == 0)
+            {
+                return Create(gameDto);
+            }
+
+            return Update(gameDto);
+        }
+
+        private int Create(GameDto gameDto)
+        {
+            using (var context = new FifteenGameDataContext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Id == gameDto.UserId);
+                if (user == null)
+                {
+                    throw new Exception("Нет такого пользователя");
+                }
+
+                var newGame = new Game
+                {
+                    User = user,
+                    MoveCount = gameDto.MoveCount,
+                    Cells = new List<Cell>(),
+                };
+
+                context.Games.Add(newGame);
+
+                for (int row = 0; row < Constants.RowCount; row++)
+                {
+                    for (int column = 0; column < Constants.ColumnCount; column++)
+                    {
+                        if (gameDto.Cells[row, column] == Constants.FreeCellValue)
+                        {
+                            continue;
+                        }
+
+                        newGame.Cells.Add(new Cell
+                        {
+                            Row = row,
+                            Column = column,
+                            Value = gameDto.Cells[row, column],
+                        });
+                    }
+                }
+
+                context.SaveChanges();
+                return newGame.Id;
+            }
+        }
+
+        private int Update(GameDto gameDto)
+        {
+            using (var context = new FifteenGameDataContext())
+            {
+                var game = context.Games.Include("Cells").FirstOrDefault(g => g.Id == gameDto.Id);
+                if (game == null)
+                {
+                    throw new Exception("Нет такой игры");
+                }
+
+                game.MoveCount = gameDto.MoveCount;
+                context.SaveChanges();
+
+                bool updated = false;
+                for (int row = 0; row < Constants.RowCount; row++)
+                {
+                    for (int column = 0; column < Constants.ColumnCount; column++)
+                    {
+                        if (gameDto.Cells[row, column] == Constants.FreeCellValue)
+                        {
+                            continue;
+                        }
+
+                        var selectedCell = game.Cells.First(c => c.Value == gameDto.Cells[row, column]);
+                        if (selectedCell.Row != row || selectedCell.Column != column)
+                        {
+                            selectedCell.Row = row;
+                            selectedCell.Column = column;
+                            updated = true;
+                        }
+                    }
+                }
+
+                if (updated)
+                {
+                    context.SaveChanges();
+                }
+
+                return gameDto.Id;
+            }
+        }
+
+        private GameDto ToDto(Game game)
+        {
+            if (game == null)
+            {
+                return null;
+            }
+
+            var result = new GameDto
+            {
+                Id = game.Id,
+                UserId = game.UserId,
+                MoveCount = game.MoveCount,
+            };
+
+            for (int row = 0; row < Constants.RowCount; row++)
+            {
+                for (int column = 0; column < Constants.ColumnCount; column++)
+                {
+                    result.Cells[row, column] = Constants.FreeCellValue;
+                }
+            }
+
+            foreach (var cell in game.Cells)
+            {
+                result.Cells[cell.Row, cell.Column] = cell.Value;
+            }
+
+            return result;
+        }
+    }
+}
