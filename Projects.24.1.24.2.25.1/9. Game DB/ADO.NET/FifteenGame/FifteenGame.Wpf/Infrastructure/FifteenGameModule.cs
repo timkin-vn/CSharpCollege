0f151be9400743f4c9d1dc0bf9767330707c53c1
tsaplinalexey <tsaplan699@gmail.com>
2026-06-23 05:15:38 +0300

    add

---
+++ Projects.24.1.24.2.25.1/9. Game DB/ADO.NET/FifteenGame/FifteenGame.Wpf/Infrastructure/FifteenGameModule.cs
@@ -0,0 +1,25 @@
+﻿using FifteenGame.Business.Services;
+using FifteenGame.Common.Contracts.Repositories;
+using FifteenGame.Common.Contracts.Services;
+using FifteenGame.DataAccess.Repositories;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenGame.Wpf.Infrastructure
+{
+    internal class FifteenGameModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUserRepository>().To<UserRepository>().InSingletonScope();
+            Bind<IGameRepository>().To<GameRepository>().InSingletonScope();
+
+            Bind<IUserService>().To<UserService>().InSingletonScope();
+            Bind<IGameService>().To<GameService>().InSingletonScope();
+        }
+    }
+}
